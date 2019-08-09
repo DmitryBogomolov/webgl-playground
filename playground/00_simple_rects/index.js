844d5618a896a1c847af0b00e466e94058b73ce9
Dmitry Bogomolov <Dmitry.Bogomolov.mail@gmail.com>
2019-08-10 01:08:09 +0300

    Interleaved vertex data (#11)
    * Use DataView
    
    * Add colors (#6)
    
    * Use byte instead of float for color
    
    * Use index buffer
    
    * Use vertex array object (#7)

--- playground/00_simple_rects/index.js
+++ playground/00_simple_rects/index.js
@@ -4,9 +4,10 @@ import fragShaderSource from './simple.frag';
 const {
     VERTEX_SHADER, FRAGMENT_SHADER,
     COMPILE_STATUS, LINK_STATUS,
-    ARRAY_BUFFER, STATIC_DRAW,
+    ARRAY_BUFFER, ELEMENT_ARRAY_BUFFER,
+    STATIC_DRAW,
     COLOR_BUFFER_BIT,
-    FLOAT,
+    UNSIGNED_BYTE, UNSIGNED_SHORT, FLOAT,
     TRIANGLES,
 } = WebGLRenderingContext.prototype;
 
@@ -24,6 +25,11 @@ function createContext(container) {
     const gl = canvas.getContext('webgl');
     if (!gl) {
         const err = 'Context is not created';
+        console.error(err);
+        throw new Error(err);
+    }
+    if (!gl.getExtension('OES_vertex_array_object')) {
+        const err = 'No OES_vertex_array_object extension';
         console.error(err);
         throw new Error(err);
     }
@@ -49,6 +55,17 @@ function createProgram(gl, vertShader, fragShader) {
     const program = gl.createProgram();
     gl.attachShader(program, vertShader);
     gl.attachShader(program, fragShader);
+
+    // TODO: Inspect sources for names.
+    const attributes = {
+        a_position: 0,
+        a_color: 1,
+    };
+    const uniforms = {};
+    Object.keys(attributes).forEach((name) => {
+        gl.bindAttribLocation(program, attributes[name], name);
+    })
+    
     gl.linkProgram(program);
     if (!gl.getProgramParameter(program, LINK_STATUS)) {
         gl.deleteShader(vertShader);
@@ -59,73 +76,108 @@ function createProgram(gl, vertShader, fragShader) {
         throw new Error(err);
     }
 
-    // TODO: Inspect sources for names.
-    // TODO: Use `bindAttribLocation`.
-    const attributes = {
-        a_position: gl.getAttribLocation(program, 'a_position'),
-    };
-    const uniforms = {};
-
     return { program, attributes, uniforms };
 }
 
-// TODO: Add index buffer.
-// TODO: Use VertexAttributeArray.
-function createBuffer(gl) {
-    const buffer = gl.createBuffer();
-    gl.bindBuffer(ARRAY_BUFFER, buffer);
+function createBuffer(gl, program, vaoExt) {
     const vertices = [
-        -1,  0,
-        -1, -1,
-         0, -1,
+        { position: [-1,  0], color: [1, 0, 0], },
+        { position: [-1, -1], color: [1, 0, 0], },
+        { position: [ 0, -1], color: [1, 0, 0], },
 
-         0, -1,
-         1, -1,
-         1,  0,
+        { position: [ 0, -1], color: [1, 1, 0], },
+        { position: [ 1, -1], color: [1, 1, 0], },
+        { position: [ 1,  0], color: [1, 1, 0], },
 
-         1,  0,
-         1,  1,
-         0,  1,
+        { position: [ 1,  0], color: [0, 1, 0], },
+        { position: [ 1,  1], color: [0, 1, 0], },
+        { position: [ 0,  1], color: [0, 1, 0], },
 
-         0,  1,
-        -1,  1,
-        -1,  0,
+        { position: [ 0,  1], color: [0, 1, 1], },
+        { position: [-1,  1], color: [0, 1, 1], },
+        { position: [-1,  0], color: [0, 1, 1], },
     ];
-    gl.bufferData(ARRAY_BUFFER, new Float32Array(vertices), STATIC_DRAW);
+    const indexes = [
+        0, 1, 2,
+        3, 4, 5,
+        6, 7, 8,
+        9, 10, 11,
+    ];
+    const vertexSize = (2 + 1) * 4;
+    const data = new ArrayBuffer(vertices.length * vertexSize);
+    const dv = new DataView(data);
+    let offset = 0;
+    vertices.forEach((vertex) => {
+        dv.setFloat32(offset, vertex.position[0], true);
+        offset += 4;
+        dv.setFloat32(offset, vertex.position[1], true);
+        offset += 4;
+        dv.setUint8(offset, Math.round(vertex.color[0] * 255));
+        offset++;
+        dv.setUint8(offset, Math.round(vertex.color[1] * 255));
+        offset++;
+        dv.setUint8(offset, Math.round(vertex.color[2] * 255));
+        offset++;
+        dv.setUint8(offset, 0);
+        offset++;
+    });
+
+    const vertexBuffer = gl.createBuffer();
+    gl.bindBuffer(ARRAY_BUFFER, vertexBuffer);
+    gl.bufferData(ARRAY_BUFFER, data, STATIC_DRAW);
+
+    const indexBuffer = gl.createBuffer();
+    gl.bindBuffer(ELEMENT_ARRAY_BUFFER, indexBuffer);
+    gl.bufferData(ELEMENT_ARRAY_BUFFER, new Uint16Array(indexes), STATIC_DRAW);
+
+    const vao = vaoExt.createVertexArrayOES();
+    vaoExt.bindVertexArrayOES(vao);
+    gl.bindBuffer(ARRAY_BUFFER, vertexBuffer);
+    gl.vertexAttribPointer(program.attributes.a_position, 2, FLOAT, false, vertexSize, 0);
+    gl.enableVertexAttribArray(program.attributes.a_position);
+    gl.vertexAttribPointer(program.attributes.a_color, 3, UNSIGNED_BYTE, true, vertexSize, 8);
+    gl.enableVertexAttribArray(program.attributes.a_color);
+    gl.bindBuffer(ELEMENT_ARRAY_BUFFER, indexBuffer);
+    vaoExt.bindVertexArrayOES(null);
+
     return {
-        buffer,
-        vertexCount: 12,
-        vertexSize: 2,
+        vao,
+        vertexBuffer,
+        indexBuffer,
+        vertexCount: vertices.length,
+        vertexSize,
+        indexCount: vertices.length,
     };
 }
 
 function init() {
     const container = document.querySelector('.container');
     const { gl, size } = createContext(container);
+    const vaoExt = gl.getExtension('OES_vertex_array_object');
 
     const vertShader = createShader(gl, VERTEX_SHADER, vertShaderSource);
     const fragShader = createShader(gl, FRAGMENT_SHADER, fragShaderSource);
     const program = createProgram(gl, vertShader, fragShader);
 
-    const buffer = createBuffer(gl);
+    const buffer = createBuffer(gl, program, vaoExt);
 
     gl.viewport(0, 0, size[0], size[1]);
     gl.clearColor(0, 0, 0, 1);
 
     return {
         gl,
+        vaoExt,
         program,
         buffer,
     };
 }
 
-function render({ gl, program, buffer }) {
+function render({ gl, vaoExt, program, buffer }) {
     gl.clear(COLOR_BUFFER_BIT);
     gl.useProgram(program.program);
-    gl.bindBuffer(ARRAY_BUFFER, buffer.buffer);
-    gl.enableVertexAttribArray(program.attributes.a_position);
-    gl.vertexAttribPointer(program.attributes.a_position, buffer.vertexSize, FLOAT, false, 0, 0);
-    gl.drawArrays(TRIANGLES, 0, buffer.vertexCount);
+    vaoExt.bindVertexArrayOES(buffer.vao);
+    gl.drawElements(TRIANGLES, buffer.indexCount, UNSIGNED_SHORT, 0);
+    vaoExt.bindVertexArrayOES(null);
 }
 
 function runLoop(state) {
