1ba9dbf27698b450734496b8413869f03e3cced7
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2019-08-21 14:01:31 +0300

    Add jest

---
+++ server/utils.test.js
@@ -0,0 +1,22 @@
+const { getOutputName, getBundleRoute, prettifyName } = require('./utils');
+
+describe('utils', () => {
+    describe('getOutputName', () => {
+        it('build output name', () => {
+            expect(getOutputName('test-module')).toEqual('test-module.bundle.js');
+        });
+    });
+
+    describe('getBundleRoute', () => {
+        it('build bundle route', () => {
+            expect(getBundleRoute('test-module')).toEqual('/static/test-module.bundle.js');
+        });
+    });
+
+    describe('prettifyName', () => {
+        it('prettify name', () => {
+            expect(prettifyName('1_test-module')).toEqual('test-module');
+            expect(prettifyName('2_test_module')).toEqual('test module');
+        });
+    });
+});
