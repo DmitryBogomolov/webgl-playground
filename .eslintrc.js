fa11280cf33804d7d691d8793df51dc52eae39b6
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2019-08-21 09:28:34 +0300

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+    'env': {
+        'browser': true,
+        'es6': true
+    },
+    'extends': 'eslint:recommended',
+    'globals': {
+        'Atomics': 'readonly',
+        'SharedArrayBuffer': 'readonly'
+    },
+    'parserOptions': {
+        'ecmaVersion': 2018,
+        'sourceType': 'module'
+    },
+    'rules': {
+        'indent': ['error', 4],
+        'linebreak-style': ['error', 'unix'],
+        'quotes': ['error', 'single'],
+        'semi': ['error', 'always'],
+        'no-multi-spaces': 'error',
+        'comma-dangle': ['error', 'always-multiline'],
+        'space-before-function-paren': [
+            'error',
+            { 'anonymous': 'always', 'named': 'never', 'asyncArrow': 'always' },
+        ],
+        'key-spacing': 'error',
+        'keyword-spacing': 'error',
+        'func-call-spacing': 'error',
+    },
+};
