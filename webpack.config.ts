aae63e253a13bf036d79df12e70b462edb58f18c
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2021-04-28 11:15:50 +0300

    Replace array with object

--- webpack.config.ts
+++ webpack.config.ts
@@ -5,7 +5,6 @@ import { Compiler, Configuration, EntryObject } from 'webpack';
 import Mustache from 'mustache';
 
 interface Playground {
-    readonly name: string;
     readonly title: string;
     readonly hasWorker: boolean;
     readonly hasMarkup: boolean;
@@ -19,9 +18,12 @@ interface Template {
 const TEMPLATES_DIR = path.join(__dirname, 'templates');
 const PLAYGROUND_DIR = path.join(__dirname, 'playground');
 
-const templates = new Map<string, Template>();
-templates.set('index', { path: path.join(TEMPLATES_DIR, 'index.html'), content: '' });
-templates.set('playground', { path: path.join(TEMPLATES_DIR, 'playground.html'), content: '' });
+const ROOT_TEMPLATE_NAME = 'index';
+const PLAYGROUND_TEMPLATE_NAME = 'playground';
+
+const templates: Record<string, Template> = {};
+templates[ROOT_TEMPLATE_NAME] = { path: path.join(TEMPLATES_DIR, 'index.html'), content: '' };
+templates[PLAYGROUND_TEMPLATE_NAME] = { path: path.join(TEMPLATES_DIR, 'playground.html'), content: '' };
 
 function capitalizeWord(word: string): string {
     return word[0].toUpperCase() + word.slice(1);
@@ -31,20 +33,19 @@ function capitalizeName(name: string): string {
     return name.replace(/_/g, ' ').replace(/\w\S*/g, capitalizeWord);
 }
 
-const playgrounds: Playground[] = [];
+const playgrounds: Record<string, Playground> = {};
 fs.readdirSync(PLAYGROUND_DIR).forEach((dirName) => {
     const currentDir = path.join(PLAYGROUND_DIR, dirName);
     if (fs.existsSync(path.join(currentDir, 'index.ts'))) {
         const markupPath = path.join(currentDir, 'markup.html');
         const hasMarkup = fs.existsSync(markupPath);
-        playgrounds.push({
-            name: dirName,
+        playgrounds[dirName] = {
             title: capitalizeName(dirName),
             hasWorker: fs.existsSync(path.join(currentDir, 'worker.ts')),
             hasMarkup,
-        });
+        };
         if (hasMarkup) {
-            templates.set(dirName, { path: markupPath, content: '' });
+            templates[dirName] = { path: markupPath, content: '' };
         }
     }
 });
@@ -53,10 +54,10 @@ const entry: EntryObject = {
     'index': path.join(TEMPLATES_DIR, 'index.ts'),
 };
 
-playgrounds.forEach((playground) => {
-    entry[playground.name] = path.join(PLAYGROUND_DIR, playground.name, 'index.ts');
+Object.entries(playgrounds).forEach(([name, playground]) => {
+    entry[name] = path.join(PLAYGROUND_DIR, name, 'index.ts');
     if (playground.hasWorker) {
-        entry[playground.name + '_worker'] = path.join(PLAYGROUND_DIR, playground.name, 'worker.ts');
+        entry[name + '_worker'] = path.join(PLAYGROUND_DIR, name, 'worker.ts');
     }
 });
 
@@ -88,14 +89,14 @@ const config: Configuration = {
             let rootPage = '';
             const playgroundPages = new Map<string, string>();
 
-            templates.forEach(({ path }, name) => {
+            Object.entries(templates).forEach(([name, { path }]) => {
                 function readTemplate(): void {
                     fs.readFile(path, 'utf8', (_err, data) => {
-                        templates.get(name)!.content = data;
+                        templates[name]!.content = data;
                         // Simple depedencies.
-                        if (name === 'index') {
+                        if (name === ROOT_TEMPLATE_NAME) {
                             rootPage = '';
-                        } else if (name === 'playground') {
+                        } else if (name === PLAYGROUND_TEMPLATE_NAME) {
                             playgroundPages.clear();
                         } else {
                             playgroundPages.delete(name);
@@ -108,11 +109,11 @@ const config: Configuration = {
 
             function getRootPage(): string {
                 if (!rootPage) {
-                    rootPage = Mustache.render(templates.get('index')!.content, {
+                    rootPage = Mustache.render(templates[ROOT_TEMPLATE_NAME]!.content, {
                         title: 'WebGL Playground',
                         bootstrap_url: '/static/bootstrap.min.css',
                         bundle_url: '/assets/index.js',
-                        playgrounds: playgrounds.map(({ name, title }) => ({ url: `/playground/${name}/`, title })),
+                        playgrounds: Object.entries(playgrounds).map(([name, { title }]) => ({ url: `/playground/${name}/`, title })),
                     });
                 }
                 return rootPage;
@@ -121,13 +122,13 @@ const config: Configuration = {
             function getPlaygroundPage(name: string): string {
                 let content = playgroundPages.get(name);
                 if (!content) {
-                    const playground = playgrounds.find((playground) => playground.name === name)!;
-                    content = Mustache.render(templates.get('playground')!.content, {
+                    const playground = playgrounds[name]!;
+                    content = Mustache.render(templates[PLAYGROUND_TEMPLATE_NAME]!.content, {
                         title: playground.title,
                         bootstrap_url: '/static/bootstrap.min.css',
                         bundle_url: `/assets/${name}.js`,
                         worker_url: playground.hasWorker ? `/assets/${name}_worker.js` : null,
-                        custom_markup: playground.hasMarkup ? templates.get(name)!.content : null,
+                        custom_markup: playground.hasMarkup ? templates[name]!.content : null,
                     });
                     playgroundPages.set(name, content);
                 }
@@ -137,7 +138,7 @@ const config: Configuration = {
             app.get('/', (_req, res) => {
                 res.send(getRootPage());
             });
-            playgrounds.forEach(({ name }) => {
+            Object.keys(playgrounds).forEach((name) => {
                 app.get(`/playground/${name}/`, (_req, res) => {
                     res.send(getPlaygroundPage(name));
                 });
@@ -170,7 +171,7 @@ const config: Configuration = {
         {
             apply(compiler: Compiler): void {
                 compiler.hooks.afterCompile.tap('watch-templates', (compilation) => {
-                    templates.forEach(({ path }) => {
+                    Object.values(templates).forEach(({ path }) => {
                         compilation.fileDependencies.add(path);
                     });
                 });
