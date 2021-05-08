2edee8e8f8b6bf1c4591a36c1b87a762cef7fed7
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2021-05-08 13:38:12 +0300

    Add string format util

--- lib/utils.ts
+++ lib/utils.ts
@@ -10,6 +10,20 @@ export function logSilenced(state: boolean): void {
     isLogSilenced = !!state;
 }
 
+export function formatStr(format: string, ...params: unknown[]): string {
+    return format.replace(/{(\d+)}/g, (_match, i: number) => {
+        const param = params[i];
+        const type = typeof param;
+        if (type === 'object') {
+            return JSON.stringify(param);
+        } else if (type === 'symbol') {
+            return (param as symbol).toString();
+        } else {
+            return param as string;
+        }
+    });
+}
+
 export class Logger {
     private readonly _prefix: string;
 
