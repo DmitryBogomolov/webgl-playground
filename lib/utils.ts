16f9a9a5ba368949281cef66bebbb856a9de2ec5
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2021-05-16 23:24:49 +0300

    Add 'throttle' util

--- lib/utils.ts
+++ lib/utils.ts
@@ -63,9 +63,32 @@ export function generateDefaultIndexes(vertexCount: number): number[] {
     return data;
 }
 
-type Action = () => void;
+export function throttle<T extends (...args: any[]) => void>(func: T, duration: number): T {
+    let timeout = 0;
+    let lastTime = 0;
+    let pendingArgs: Parameters<T> | null = null;
+    function handler(): void {
+        timeout = 0;
+        lastTime = Date.now();
+        func(...pendingArgs!);
+        pendingArgs = null;
+    }
+    function throttledFunc(...args: Parameters<T>): void {
+        const time = Date.now();
+        const timespan = time - lastTime;
+        if (timespan > duration) {
+            lastTime = time;
+            func(...args);
+            return;
+        }
+        // eslint-disable-next-line @typescript-eslint/no-implied-eval
+        timeout = timeout || setTimeout(handler as TimerHandler, duration - timespan);
+        pendingArgs = args;
+    }
+    return throttledFunc as T;
+}
 
-export function handleWindowResize(callback: Action): Action {
+export function handleWindowResize(callback: () => void): () => void {
     window.addEventListener('resize', callback);
     return () => {
         window.removeEventListener('resize', callback);
