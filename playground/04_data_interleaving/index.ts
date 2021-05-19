69f40ae8dd7534daf7f8ea9256107c0ce2a01369
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2021-05-20 00:48:25 +0300

    Add description

--- playground/04_data_interleaving/index.ts
+++ playground/04_data_interleaving/index.ts
@@ -7,21 +7,26 @@ import {
     Runtime,
     Primitive,
     Program,
+    VertexSchema,
 } from 'lib';
 import vertexShaderSource from './shaders/shader.vert';
 import fragmentShaderSource from './shaders/shader.frag';
 
 /**
+ * Vertex data interleaving.
+ *
+ * Shows two ways of vertex layout - Array of Structure (AoS) and Structure of Arrays (SoA).
+ * https://en.wikipedia.org/wiki/AoS_and_SoA
  */
 export type DESCRIPTION = never;
 
 const container = document.querySelector<HTMLElement>(PLAYGROUND_ROOT)!;
 
 const vertices = [
-    { position: [-0.5, -0.5], color: [1, 0, 0], factor: 0.5 },
-    { position: [+0.5, -0.5], color: [0, 0, 1], factor: 0.6 },
-    { position: [+0.5, +0.5], color: [1, 0, 0], factor: 0.7 },
-    { position: [-0.5, +0.5], color: [0, 0, 1], factor: 0.8 },
+    { position: [-0.5, -0.5], color: [0, 1, 0], factor: 0.3 },
+    { position: [+0.5, -0.5], color: [0, 0, 1], factor: 0.5 },
+    { position: [+0.5, +0.5], color: [0, 1, 0], factor: 0.7 },
+    { position: [-0.5, +0.5], color: [0, 0, 1], factor: 0.9 },
 ];
 const indices = [0, 1, 2, 2, 3, 0];
 
@@ -31,26 +36,7 @@ function makeAoSPrimitive(runtime: Runtime): Primitive {
         { name: 'a_color', type: 'ubyte3', normalized: true },
         { name: 'a_factor', type: 'ubyte1', normalized: true },
     ]);
-    const program = new Program(runtime, {
-        vertexShader: vertexShaderSource,
-        fragmentShader: fragmentShaderSource,
-        schema,
-    });
-    const primitive = new Primitive(runtime);
-
-    const vertexData = new ArrayBuffer(vertices.length * schema.totalSize);
-    const writer = new VertexWriter(schema, vertexData);
-    for (let i = 0; i < vertices.length; ++i) {
-        const { position, color, factor } = vertices[i];
-        writer.writeAttribute(i, 'a_position', position);
-        writer.writeAttribute(i, 'a_color', color);
-        writer.writeAttribute(i, 'a_factor', [factor]);
-    }
-
-    primitive.setData(vertexData, new Uint16Array(indices));
-    primitive.setProgram(program);
-
-    return primitive;
+    return makePrimitive(runtime, schema, vertices.length * schema.totalSize, [-0.5, +0.5]);
 }
 
 function makeSoAPrimitive(runtime: Runtime): Primitive {
@@ -59,14 +45,20 @@ function makeSoAPrimitive(runtime: Runtime): Primitive {
         { name: 'a_position', type: 'float2', offset: 16, stride: 8 },
         { name: 'a_factor', type: 'ubyte1', normalized: true, offset: 48, stride: 4 },
     ]);
+    return makePrimitive(runtime, schema, 64, [+0.5, -0.5]);
+}
+
+function makePrimitive(
+    runtime: Runtime, schema: VertexSchema, arrayBufferSize: number, [dx, dy]: [number, number],
+): Primitive {
     const program = new Program(runtime, {
-        vertexShader: vertexShaderSource,
+        vertexShader: vertexShaderSource.replace(/%OFFSET%/g, `vec2(${dx}, ${dy})`),
         fragmentShader: fragmentShaderSource,
         schema,
     });
     const primitive = new Primitive(runtime);
 
-    const vertexData = new ArrayBuffer(vertices.length * schema.totalSize);
+    const vertexData = new ArrayBuffer(arrayBufferSize);
     const writer = new VertexWriter(schema, vertexData);
     for (let i = 0; i < vertices.length; ++i) {
         const { position, color, factor } = vertices[i];
@@ -86,12 +78,8 @@ const aosPrimitive = makeAoSPrimitive(runtime);
 const soaPrimitive = makeSoAPrimitive(runtime);
 
 const loop = new RenderLoop(() => {
-    aosPrimitive.draw({
-        'u_offset': [-0.5, -0.5],
-    });
-    soaPrimitive.draw({
-        'u_offset': [+0.5, +0.5],
-    });
+    aosPrimitive.draw();
+    soaPrimitive.draw();
 });
 loop.start();
 logSilenced(true);
--- playground/04_data_interleaving/shaders/shader.vert
+++ playground/04_data_interleaving/shaders/shader.vert
@@ -2,11 +2,11 @@ attribute vec2 a_position;
 attribute vec3 a_color;
 attribute float a_factor;
 
-uniform vec2 u_offset;
+const vec2 offset = %OFFSET%;
 
 varying vec4 v_color;
 
 void main() {
-    gl_Position = vec4(a_position + u_offset, 0.0, 1.0);
+    gl_Position = vec4(a_position + offset, 0.0, 1.0);
     v_color = vec4(a_color * a_factor, 1.0);
 }
