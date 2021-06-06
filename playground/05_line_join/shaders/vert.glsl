6d36201c8da90888c062bc2677606d98a1126bd1
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2021-06-06 12:39:21 +0300

    Refactor shader

--- playground/05_line_join/shaders/vert.glsl
+++ playground/05_line_join/shaders/vert.glsl
@@ -8,10 +8,6 @@ uniform float u_thickness;
 
 const float EPS = 1.E-7;
 
-vec2 rotate_right(vec2 v) {
-    return vec2(v.y, -v.x);
-}
-
 /*
 a_position.z - point location on segment end = -1 (left), +1 (right)
 
@@ -30,19 +26,37 @@ right point (+1) right turn (+1) : inner
 
 */
 
+vec2 get_tangent(vec2 segment_dir, vec2 outer_dir) {
+    return dot(segment_dir, outer_dir) > -1.0 + EPS ? normalize(segment_dir + outer_dir) : segment_dir;
+}
+
+vec2 rotate_right(vec2 v) {
+    return vec2(v.y, -v.x);
+}
+
+float get_rotation_sign(vec2 outer_dir, vec2 bitangent) {
+    return 2.0 * step(0.0, dot(outer_dir, bitangent)) - 1.0;
+}
+
+vec2 get_inner_offset(vec2 normal, vec2 bitangent) {
+    return bitangent / dot(normal, bitangent);
+}
+
+vec2 get_outer_offset(vec2 normal, vec2 bitangent) {
+    vec2 slope = normalize(normal + bitangent);
+    return slope / dot(normal, slope);
+}
+
 void main() {
-    vec2 curr_dir = normalize((a_position.xy - a_other.xy) * u_canvas_size);
-    vec2 next_dir = normalize((a_other.zw - a_position.xy) * u_canvas_size);
-    vec2 tangent = dot(curr_dir, next_dir) <= -1.0 + EPS ? curr_dir : normalize(curr_dir + next_dir);
-    vec2 curr_norm = rotate_right(curr_dir);
+    vec2 segment_dir = normalize((a_position.xy - a_other.xy) * u_canvas_size);
+    vec2 outer_dir = normalize((a_other.zw - a_position.xy) * u_canvas_size);
+    vec2 tangent = get_tangent(segment_dir, outer_dir);
+    vec2 normal = rotate_right(segment_dir);
     vec2 bitangent = rotate_right(tangent);
-    vec2 slope = normalize(curr_norm + bitangent);
-
-    float rotation_dir = 2.0 * step(0.0, dot(next_dir, bitangent)) - 1.0;
-    vec2 inner_offset = +rotation_dir * bitangent / dot(curr_norm, bitangent);
-    vec2 outer_offset = -rotation_dir * slope / dot(curr_norm, slope);
-    vec2 offset = a_position.z * rotation_dir > 0.0 ? inner_offset : outer_offset;
-
+    float rotation_sign = get_rotation_sign(outer_dir, bitangent);
+    vec2 offset = a_position.z * rotation_sign > 0.0
+        ? +rotation_sign * get_inner_offset(normal, bitangent)
+        : -rotation_sign * get_outer_offset(normal, bitangent);
     vec2 bioffset = offset * u_thickness / u_canvas_size;
     gl_Position = vec4(a_position.xy + bioffset, 0.0, 1.0);
 }
