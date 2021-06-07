a0801133d4ecbdca5a28cfcbaa713fbcbf42ef69
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2021-06-07 14:06:13 +0300

    Refactor

--- playground/05_line_join/index.ts
+++ playground/05_line_join/index.ts
@@ -4,7 +4,7 @@ import {
     Program,
     Runtime,
     Color, colors, color2array,
-    VertexWriter, AttrValue,
+    VertexWriter, AttrValue, VertexSchema,
 } from 'lib';
 import vertexShaderSource from './shaders/vert.glsl';
 import fragmentShaderSource from './shaders/frag.glsl';
@@ -25,6 +25,13 @@ const enum Location {
     L = -1,
     R = +1,
 }
+
+const vertices: Vertex[] = [
+    { position: { x: -0.7, y: -0.8 }, color: colors.BLUE },
+    { position: { x: -0.1, y: +0.5 }, color: colors.GREEN },
+    { position: { x: +0.4, y: -0.5 }, color: colors.RED },
+    { position: { x: +0.8, y: +0.6 }, color: colors.GREEN },
+];
 
 function makePositionAttr(position: Position, location: Location): AttrValue {
     return [position.x, position.y, location];
@@ -47,58 +54,62 @@ function makePrimitive(runtime: Runtime): Primitive {
         schema,
     });
 
-    const vertices: Vertex[] = [
-        { position: { x: -0.7, y: -0.8 }, color: colors.BLUE },
-        { position: { x: -0.1, y: +0.5 }, color: colors.GREEN },
-        { position: { x: +0.4, y: -0.5 }, color: colors.RED },
-        { position: { x: +0.8, y: +0.6 }, color: colors.GREEN },
-    ];
-    const segmentCount = vertices.length - 1;
-    const vertexData = new ArrayBuffer(schema.totalSize * segmentCount * 4);
-    const writer = new VertexWriter(schema, vertexData);
-    const indexData = new Uint16Array(6 * (2 * segmentCount - 1));
-    let indexBase = 0;
-    for (let i = 0; i < segmentCount; ++i) {
-        const vertexBase = i * 4;
-        const start = vertices[i];
-        const end = vertices[i + 1];
-        const before = i > 0 ? vertices[i - 1] : end;
-        const after = i < segmentCount - 1 ? vertices[i + 2] : start;
-
-        writer.writeAttribute(vertexBase + 0, 'a_position', makePositionAttr(start.position, Location.R));
-        writer.writeAttribute(vertexBase + 1, 'a_position', makePositionAttr(start.position, Location.L));
-        writer.writeAttribute(vertexBase + 2, 'a_position', makePositionAttr(end.position, Location.L));
-        writer.writeAttribute(vertexBase + 3, 'a_position', makePositionAttr(end.position, Location.R));
-        writer.writeAttribute(vertexBase + 0, 'a_other', makeOtherAttr(end.position, before.position));
-        writer.writeAttribute(vertexBase + 1, 'a_other', makeOtherAttr(end.position, before.position));
-        writer.writeAttribute(vertexBase + 2, 'a_other', makeOtherAttr(start.position, after.position));
-        writer.writeAttribute(vertexBase + 3, 'a_other', makeOtherAttr(start.position, after.position));
-        writer.writeAttribute(vertexBase + 0, 'a_color', color2array(start.color));
-        writer.writeAttribute(vertexBase + 1, 'a_color', color2array(start.color));
-        writer.writeAttribute(vertexBase + 2, 'a_color', color2array(end.color));
-        writer.writeAttribute(vertexBase + 3, 'a_color', color2array(end.color));
-
-        indexData[indexBase++] = vertexBase + 0;
-        indexData[indexBase++] = vertexBase + 1;
-        indexData[indexBase++] = vertexBase + 3;
-        indexData[indexBase++] = vertexBase + 3;
-        indexData[indexBase++] = vertexBase + 2;
-        indexData[indexBase++] = vertexBase + 0;
-
-        if (i < segmentCount - 1) {
-            indexData[indexBase++] = vertexBase + 2;
-            indexData[indexBase++] = vertexBase + 3;
-            indexData[indexBase++] = vertexBase + 5;
-            indexData[indexBase++] = vertexBase + 5;
-            indexData[indexBase++] = vertexBase + 4;
-            indexData[indexBase++] = vertexBase + 2;
-        }
-    }
+    const vertexData = new ArrayBuffer(schema.totalSize * (vertices.length - 1) * 4);
+    const indexData = new Uint16Array(6 * (2 * (vertices.length - 1) - 1));
+    writeVertices(vertexData, schema, vertices);
+    writeIndexes(indexData, vertices.length);
 
     primitive.setProgram(program);
     primitive.setData(vertexData, indexData);
 
     return primitive;
+}
+
+function writeVertices(vertexData: ArrayBuffer, schema: VertexSchema, vertices: ReadonlyArray<Vertex>): void {
+    const writer = new VertexWriter(schema, vertexData);
+    for (let i = 0; i < vertices.length - 1; ++i) {
+        const vertexBase = i * 4;
+        const start = vertices[i];
+        const end = vertices[i + 1];
+        const before = vertices[i - 1] || end;
+        const after = vertices[i + 2] || start;
+
+        const startOther = makeOtherAttr(end.position, before.position);
+        const endOther = makeOtherAttr(start.position, after.position);
+        const startColor = color2array(start.color);
+        const endColor = color2array(end.color);
+
+        writer.writeAttribute(vertexBase + 0, 'a_position', makePositionAttr(start.position, Location.R));
+        writer.writeAttribute(vertexBase + 1, 'a_position', makePositionAttr(start.position, Location.L));
+        writer.writeAttribute(vertexBase + 2, 'a_position', makePositionAttr(end.position, Location.L));
+        writer.writeAttribute(vertexBase + 3, 'a_position', makePositionAttr(end.position, Location.R));
+        writer.writeAttribute(vertexBase + 0, 'a_other', startOther);
+        writer.writeAttribute(vertexBase + 1, 'a_other', startOther);
+        writer.writeAttribute(vertexBase + 2, 'a_other', endOther);
+        writer.writeAttribute(vertexBase + 3, 'a_other', endOther);
+        writer.writeAttribute(vertexBase + 0, 'a_color', startColor);
+        writer.writeAttribute(vertexBase + 1, 'a_color', startColor);
+        writer.writeAttribute(vertexBase + 2, 'a_color', endColor);
+        writer.writeAttribute(vertexBase + 3, 'a_color', endColor);
+    }
+}
+
+function writeIndexes(indexData: Uint16Array, vertexCount: number): void {
+    for (let i = 0; i < vertexCount - 1; ++i) {
+        writeSegmentIndexes(indexData, i * 12, i * 4);
+    }
+    for (let i = 0; i < vertexCount - 2; ++i) {
+        writeSegmentIndexes(indexData, i * 12 + 6, i * 4 + 2);
+    }
+}
+
+function writeSegmentIndexes(indexData: Uint16Array, offset: number, vertexIndex: number): void {
+    indexData[offset + 0] = vertexIndex + 0;
+    indexData[offset + 1] = vertexIndex + 1;
+    indexData[offset + 2] = vertexIndex + 3;
+    indexData[offset + 3] = vertexIndex + 3;
+    indexData[offset + 4] = vertexIndex + 2;
+    indexData[offset + 5] = vertexIndex + 0;
 }
 
 const runtime = new Runtime(container);
