c80de3725fe5203dc0c5048293cf34c77f798e8e
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2021-06-25 11:09:23 +0300

    Slightly refactor tree

--- lib/alg/kd-tree.test.ts
+++ lib/alg/kd-tree.test.ts
@@ -6,6 +6,9 @@ describe('kd-tree', () => {
             readonly x: number;
             readonly y: number;
         }
+        const getX = (point: Point): number => point.x;
+        const getY = (point: Point): number => point.y;
+        const getDist = (diff: readonly number[]): number => diff[0] * diff[0] + diff[1] * diff[1];
 
         it('find nearest point', () => {
             const points: Point[] = [
@@ -19,9 +22,6 @@ describe('kd-tree', () => {
                 { x: 55, y: 1 },
                 { x: 70, y: 70 },
             ];
-            const getX = (point: Point): number => point.x;
-            const getY = (point: Point): number => point.y;
-            const getDist = (diff: readonly number[]): number => diff[0] * diff[0] + diff[1] * diff[1];
 
             const tree = new KDTree(points, [getX, getY], getDist);
 
@@ -45,6 +45,13 @@ describe('kd-tree', () => {
             check({ x: 1, y: 1 }, points[0]);
             check({ x: 99, y: 1 }, points[7]);
             check({ x: 99, y: 99 }, points[8]);
+        });
+
+        it('find nothing in empty tree', () => {
+            const tree = new KDTree([], [getX, getY], getDist);
+
+            expect(tree.findNearest({ x: 0, y: 0 })).toEqual(null);
+            expect(tree.findNearest({ x: 99, y: 99 })).toEqual(null);
         });
     });
 });
--- lib/alg/kd-tree.ts
+++ lib/alg/kd-tree.ts
@@ -1,3 +1,21 @@
+export type AxisFunc<T> = (element: T) => number;
+export type DistFunc = (axes: ReadonlyArray<number>) => number;
+type AxisFuncs<T> = ReadonlyArray<AxisFunc<T>>;
+
+interface KDNode<T> {
+    readonly element: T;
+    readonly lChild: KDNode<T> | null;
+    readonly rChild: KDNode<T> | null;
+}
+
+interface SearchContext<T> {
+    readonly axisFuncs: AxisFuncs<T>;
+    readonly distFunc: DistFunc;
+    readonly target: T;
+    bestDistance: number;
+    bestElement: T;
+}
+
 // TODO: Find k nearest.
 // TODO: Find in radius.
 // https://www.cs.cmu.edu/~ckingsf/bioinfo-lectures/kdtrees.pdf
@@ -13,25 +31,21 @@ export class KDTree<T> {
     }
 
     findNearest(target: T): T | null {
+        if (this._root === null) {
+            return null;
+        }
         const distance = new Array(this._axisFuncs.length);
         distance.fill(0);
-        const info: NearestInfo<T> = {
+        const context: SearchContext<T> = {
+            target,
+            axisFuncs: this._axisFuncs,
+            distFunc: this._distFunc,
             bestDistance: Number.MAX_VALUE,
-            bestElement: null,
+            bestElement: null as unknown as T,
         };
-        findNearest(target, this._root, this._axisFuncs, this._distFunc, 0, distance, info);
-        return info.bestElement;
+        findNearest(this._root, 0, distance, context);
+        return context.bestElement;
     }
-}
-
-type DistFunc = (axes: ReadonlyArray<number>) => number;
-type AxisFunc<T> = (element: T) => number;
-type AxisFuncs<T> = ReadonlyArray<AxisFunc<T>>;
-
-interface KDNode<T> {
-    readonly element: T;
-    readonly lChild: KDNode<T> | null;
-    readonly rChild: KDNode<T> | null;
 }
 
 function makeNode<T>(elements: T[], axisFuncs: AxisFuncs<T>, axis: number): KDNode<T> | null {
@@ -61,31 +75,30 @@ function makeNode<T>(elements: T[], axisFuncs: AxisFuncs<T>, axis: number): KDNo
     };
 }
 
-interface NearestInfo<T> {
-    bestDistance: number;
-    bestElement: T | null;
+function getDistance<T>(p1: T, p2: T, axisFuncs: AxisFuncs<T>, distFunc: DistFunc): number {
+    const dist: number[] = [];
+    for (let i = 0; i < axisFuncs.length; ++i) {
+        const axisFunc = axisFuncs[i];
+        dist[i] = axisFunc(p1) - axisFunc(p2);
+    }
+    return distFunc(dist);
 }
 
 function findNearest<T>(
-    target: T, node: KDNode<T> | null,
-    axisFuncs: AxisFuncs<T>, distFunc: DistFunc, axis: number,
-    distance: ReadonlyArray<number>, info: NearestInfo<T>,
+    node: KDNode<T> | null, axis: number, distance: ReadonlyArray<number>, context: SearchContext<T>,
 ): void {
     if (node === null) {
         return;
     }
+    const { target, axisFuncs, distFunc } = context;
     const targetToBoxDistance = distFunc(distance);
-    if (targetToBoxDistance > info.bestDistance) {
+    if (targetToBoxDistance > context.bestDistance) {
         return;
     }
-    const dist: number[] = [];
-    for (let i = 0; i < axisFuncs.length; ++i) {
-        dist[i] = axisFuncs[i](target) - axisFuncs[i](node.element);
-    }
-    const targetToNodeDistance = distFunc(dist);
-    if (targetToNodeDistance < info.bestDistance) {
-        info.bestDistance = targetToNodeDistance;
-        info.bestElement = node.element;
+    const targetToNodeDistance = getDistance(target, node.element, axisFuncs, distFunc);
+    if (targetToNodeDistance < context.bestDistance) {
+        context.bestDistance = targetToNodeDistance;
+        context.bestElement = node.element;
     }
     const nextAxis = (axis + 1) % axisFuncs.length;
     const axisDist = axisFuncs[axis](target) - axisFuncs[axis](node.element);
@@ -93,11 +106,11 @@ function findNearest<T>(
     const rDist = distance.slice();
     if (axisDist < 0) {
         rDist[axis] -= axisDist;
-        findNearest(target, node.lChild, axisFuncs, distFunc, nextAxis, lDist, info);
-        findNearest(target, node.rChild, axisFuncs, distFunc, nextAxis, rDist, info);
+        findNearest(node.lChild, nextAxis, lDist, context);
+        findNearest(node.rChild, nextAxis, rDist, context);
     } else {
         lDist[axis] += axisDist;
-        findNearest(target, node.rChild, axisFuncs, distFunc, nextAxis, rDist, info);
-        findNearest(target, node.lChild, axisFuncs, distFunc, nextAxis, lDist, info);
+        findNearest(node.rChild, nextAxis, rDist, context);
+        findNearest(node.lChild, nextAxis, lDist, context);
     }
 }
