786e557ddca2fce8f3c922daa5cce7a7406c1bc4
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2021-06-30 10:15:15 +0300

    Allow to pass non array values to VertexWriter

--- lib/vertex-writer.ts
+++ lib/vertex-writer.ts
@@ -1,5 +1,10 @@
+
 import { Logger } from './utils/logger';
 import { Attribute, AttributeType, VertexSchema, AttributeTypeMap } from './vertex-schema';
+import { isVec2, Vec2, vec2arr } from './geometry/vec2';
+import { isVec3, Vec3, vec3arr } from './geometry/vec3';
+import { isVec4, Vec4, vec4arr } from './geometry/vec4';
+import { Color, color2arr, isColor } from './color';
 
 type Normalizer = (value: number) => number;
 
@@ -19,7 +24,11 @@ interface AttributesMap {
     readonly [key: string]: Attribute;
 }
 
-export type AttrValue = ReadonlyArray<number>;
+type v1 = readonly [number];
+type v2 = readonly [number, number];
+type v3 = readonly [number, number, number];
+type v4 = readonly [number, number, number, number];
+export type AttrValue = number | v1 | v2 | v3 | v4 | Vec2 | Vec3 | Vec4 | Color;
 type TypedArray = Int8Array | Uint8Array | Int16Array | Uint16Array | Float32Array;
 type ArrayMaker = (buffer: ArrayBuffer, offset: number, length: number) => TypedArray;
 
@@ -52,6 +61,18 @@ function wrapBuffer(buffer: ArrayBufferView | ArrayBuffer): ArrayBufferView {
     return ArrayBuffer.isView(buffer) ? buffer : { buffer, byteOffset: 0, byteLength: buffer.byteLength };
 }
 
+type Unwrapper = (value: AttrValue) => number[] | null;
+interface UnwrappersMap {
+    readonly [key: number]: Unwrapper;
+}
+
+const unwrappers: UnwrappersMap = {
+    [1]: unwrap1,
+    [2]: unwrap2,
+    [3]: unwrap3,
+    [4]: unwrap4,
+};
+
 const logger = new Logger('VertexWriter');
 
 export class VertexWriter {
@@ -68,7 +89,8 @@ export class VertexWriter {
         if (!attr) {
             throw logger.error('attribute "{0}" is unknown', attrName);
         }
-        if (attr.size > attrValue.length) {
+        const values = unwrappers[attr.size](attrValue);
+        if (values === null) {
             throw logger.error(
                 'attribute "{0}" size is {1} but value is {2}', attrName, attr.size, attrValue,
             );
@@ -77,7 +99,57 @@ export class VertexWriter {
         const base = (attr.offset + attr.stride * vertexIndex) / view.BYTES_PER_ELEMENT | 0;
         const normalize = attr.normalized ? normalizers[attr.type] : eigen;
         for (let i = 0; i < attr.size; ++i) {
-            view[base + i] = normalize(attrValue[i]);
+            view[base + i] = normalize(values[i]);
         }
     }
+}
+
+function isNumArray(arg: unknown, length: number): arg is number[] {
+    return Array.isArray(arg) && arg.length >= length;
+}
+
+function unwrap1(value: AttrValue): number[] | null {
+    if (typeof value === 'number') {
+        return [value];
+    }
+    if (isNumArray(value, 1)) {
+        return value;
+    }
+    return null;
+}
+
+function unwrap2(value: AttrValue): number[] | null {
+    if (isVec2(value)) {
+        return vec2arr(value);
+    }
+    if (isNumArray(value, 2)) {
+        return value;
+    }
+    return null;
+}
+
+function unwrap3(value: AttrValue): number[] | null {
+    if (isVec3(value)) {
+        return vec3arr(value);
+    }
+    if (isColor(value)) {
+        return color2arr(value);
+    }
+    if (isNumArray(value, 3)) {
+        return value;
+    }
+    return null;
+}
+
+function unwrap4(value: AttrValue): number[] | null {
+    if (isVec4(value)) {
+        return vec4arr(value);
+    }
+    if (isColor(value)) {
+        return color2arr(value);
+    }
+    if (isNumArray(value, 4)) {
+        return value;
+    }
+    return null;
 }
