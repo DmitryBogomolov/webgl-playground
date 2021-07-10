97bed00b8e0db701dea81c12c439c3f363d5e885
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2021-07-11 00:32:51 +0300

    Add second runtime

--- playground/05_line_join/utils.ts
+++ playground/05_line_join/utils.ts
@@ -1,17 +1,16 @@
-import { Runtime, Vec2, KDTree } from 'lib';
+import { Vec2, KDTree } from 'lib';
 import { Vertex } from './vertex';
 
-export type Ndc2PxFunc = (ndc: Vec2) => Vec2;
-
 export type SearchTree = KDTree<Vec2>;
+export type SizeFunc = () => Vec2;
 
-export function makeSearchTree(vertices: ReadonlyArray<Vertex>, runtime: Runtime): SearchTree {
+export function makeSearchTree(vertices: ReadonlyArray<Vertex>, getSize: SizeFunc): SearchTree {
     return new KDTree<Vec2>(
         vertices.map((v) => v.position),
         [(v) => v.x, (v) => v.y],
         ([x, y]) => {
             // screen_x ~ W / 2 * ndc_x, screen_y ~ H / 2 * ndc_y
-            const { x: w, y: h } = runtime.canvasSize();
+            const { x: w, y: h } = getSize();
             return (x * w / 2) ** 2 + (y * h / 2) ** 2;
         },
     );
