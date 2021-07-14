3a23412f7facbd1a2e37ed663d4819e785bdaa30
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2021-07-14 23:47:21 +0300

    Use EventEmitter in demo

--- playground/05_line_join/index.ts
+++ playground/05_line_join/index.ts
@@ -1,9 +1,9 @@
 import {
     Runtime,
-    Color, color,
-    Vec2, vec2, dist2, Tracker,
+    Vec2, dist2,
+    Tracker,
 } from 'lib';
-import { Vertex } from './vertex';
+import { State } from './state';
 import { Line } from './line/line';
 import { BevelLine } from './line/bevel';
 import { RoundLine } from './line/round';
@@ -21,34 +21,7 @@ export type DESCRIPTION = never;
 const containerBevel = document.querySelector<HTMLDivElement>(PLAYGROUND_ROOT + '-bevel')!;
 const containerRound = document.querySelector<HTMLDivElement>(PLAYGROUND_ROOT + '-round')!;
 
-const pickColor = (function () {
-    const colorPool: ReadonlyArray<Color> = [
-        color(0, 0, 1),
-        color(0, 1, 0),
-        color(0, 1, 1),
-        color(1, 0, 0),
-        color(1, 0, 1),
-        color(1, 1, 0),
-    ];
-    let next = 0;
-
-    function pick(): Color {
-        const idx = next;
-        next = (next + 1) % colorPool.length;
-        return colorPool[idx];
-    }
-
-    return pick;
-}());
-
-const vertices: Vertex[] = [
-    { position: vec2(-0.7, -0.8), color: pickColor() },
-    { position: vec2(-0.1, +0.5), color: pickColor() },
-    { position: vec2(+0.4, -0.5), color: pickColor() },
-    { position: vec2(+0.8, +0.6), color: pickColor() },
-];
-
-let thickness = 50;
+const state = new State();
 
 const runtimeBevel = new Runtime(containerBevel);
 const runtimeRound = new Runtime(containerRound);
@@ -63,37 +36,25 @@ function setupLine<T extends Line>(runtime: Runtime, ctor: LineConstructor<T>): 
         runtime.clearColorBuffer();
         line.render();
     });
+    state.thicknessChanged.on(() => {
+        line.setThickness(state.thickness);
+        runtime.requestRender();
+    });
+    state.verticesChanged.on(() => {
+        line.setVertices(state.vertices);
+        runtime.requestRender();
+    });
+    state.vertexUpdated.on((idx) => {
+        line.updateVertex(state.vertices, idx);
+        runtime.requestRender();
+    });
+    line.setVertices(state.vertices);
+    line.setThickness(state.thickness);
     return line;
 }
 
-const lineBevel = setupLine(runtimeBevel, BevelLine);
-const lineRound = setupLine(runtimeRound, RoundLine);
-
-function requestRender(): void {
-    runtimeBevel.requestRender();
-    runtimeRound.requestRender();
-}
-
-function setThickness(): void {
-    lineBevel.setThickness(thickness);
-    lineRound.setThickness(thickness);
-    requestRender();
-}
-
-function setVertices(): void {
-    lineBevel.setVertices(vertices);
-    lineRound.setVertices(vertices);
-    requestRender();
-}
-
-function updateVertex(idx: number): void {
-    lineBevel.updateVertex(vertices, idx);
-    lineRound.updateVertex(vertices, idx);
-    requestRender();
-}
-
-setThickness();
-setVertices();
+setupLine(runtimeBevel, BevelLine);
+setupLine(runtimeRound, RoundLine);
 
 function ndc2px(ndc: Vec2): Vec2 {
     return runtimeBevel.ndc2px(ndc);
@@ -104,11 +65,14 @@ function px2ndc(px: Vec2): Vec2 {
 }
 
 const tree = new SearchTree(() => runtimeBevel.size());
-updateTree();
+tree.update(state.vertices);
 
-function updateTree(): void {
-    tree.update(vertices);
-}
+state.verticesChanged.on(() => {
+    tree.update(state.vertices);
+});
+state.vertexUpdated.on(() => {
+    tree.update(state.vertices);
+});
 
 const VERTEX_THRESHOLD = 16;
 const BORDER_THRESHOLD = 8;
@@ -123,43 +87,36 @@ function setupTracker(container: HTMLElement): void {
     new Tracker(container, {
         onDblClick({ coords }) {
             const vertexIdx = tree.findNearest(px2ndc(coords))!;
-            const vertexCoords = ndc2px(vertices[vertexIdx].position);
+            const vertexCoords = ndc2px(state.vertices[vertexIdx].position);
             const dist = dist2(vertexCoords, coords);
             if (dist <= VERTEX_THRESHOLD) {
-                if (vertices.length <= 2) {
+                if (state.vertices.length <= 2) {
                     return;
                 }
-                vertices.splice(vertexIdx, 1);
-                updateTree();
-                setVertices();
+                state.removeVertex(vertexIdx);
             } else {
-                vertices.splice(vertices.length, 0, { position: px2ndc(coords), color: pickColor() });
-                updateTree();
-                setVertices();
+                state.addVertex(state.vertices.length, px2ndc(coords));
             }
         },
         onStart({ coords }) {
             const vertexIdx = tree.findNearest(px2ndc(coords))!;
-            const vertexCoords = ndc2px(vertices[vertexIdx].position);
+            const vertexCoords = ndc2px(state.vertices[vertexIdx].position);
             const dist = dist2(vertexCoords, coords);
             if (dist <= VERTEX_THRESHOLD) {
                 motionVertexIdx = vertexIdx;
-            } else if (Math.abs(dist - thickness / 2) <= BORDER_THRESHOLD) {
+            } else if (Math.abs(dist - state.thickness / 2) <= BORDER_THRESHOLD) {
                 thicknessVertexIdx = vertexIdx;
             }
         },
         onMove({ coords }) {
             if (motionVertexIdx >= 0) {
-                vertices[motionVertexIdx].position = px2ndc({
+                state.updateVertex(motionVertexIdx, px2ndc({
                     x: clamp(coords.x, 0, containerBevel.clientWidth),
                     y: clamp(coords.y, 0, containerBevel.clientHeight),
-                });
-                updateTree();
-                updateVertex(motionVertexIdx);
+                }));
             } else if (thicknessVertexIdx >= 0) {
-                const dist = dist2(coords, ndc2px(vertices[thicknessVertexIdx].position));
-                thickness = dist * 2 | 0;
-                setThickness();
+                const dist = dist2(coords, ndc2px(state.vertices[thicknessVertexIdx].position));
+                state.setThickness(dist * 2 | 0);
             }
         },
         onEnd() {
