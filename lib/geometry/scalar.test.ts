eaed644ee7138cea022d28fb10d88ae5bf8c4936
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2022-07-19 11:43:46 +0300

    Add util function

--- lib/geometry/scalar.test.ts
+++ lib/geometry/scalar.test.ts
@@ -1,6 +1,31 @@
-import { linearMapping } from './scalar';
+import {
+    deg2rad, rad2deg,
+    linearMapping,
+} from './scalar';
 
 describe('scalar', () => {
+    it('deg2rad', () => {
+        expect(deg2rad(0)).toBeCloseTo(0);
+        expect(deg2rad(30)).toBeCloseTo(Math.PI / 6);
+        expect(deg2rad(45)).toBeCloseTo(Math.PI / 4);
+        expect(deg2rad(60)).toBeCloseTo(Math.PI / 3);
+        expect(deg2rad(90)).toBeCloseTo(Math.PI / 2);
+        expect(deg2rad(180)).toBeCloseTo(Math.PI);
+        expect(deg2rad(270)).toBeCloseTo(Math.PI * 1.5);
+        expect(deg2rad(360)).toBeCloseTo(Math.PI * 2);
+    });
+
+    it('rad2deg', () => {
+        expect(rad2deg(0)).toBeCloseTo(0);
+        expect(rad2deg(Math.PI / 6)).toBeCloseTo(30);
+        expect(rad2deg(Math.PI / 4)).toBeCloseTo(45);
+        expect(rad2deg(Math.PI / 3)).toBeCloseTo(60);
+        expect(rad2deg(Math.PI / 2)).toBeCloseTo(90);
+        expect(rad2deg(Math.PI)).toBeCloseTo(180);
+        expect(rad2deg(Math.PI * 1.5)).toBeCloseTo(270);
+        expect(rad2deg(Math.PI * 2)).toBeCloseTo(360);
+    });
+
     it('linear mapping', () => {
         const func = linearMapping(0, 10, -1, +1);
         expect(func(0)).toEqual(-1);
--- lib/geometry/scalar.ts
+++ lib/geometry/scalar.ts
@@ -1,3 +1,13 @@
+const DEG_RAD = 180 / Math.PI;
+
+export function deg2rad(deg: number): number {
+    return deg / DEG_RAD;
+}
+
+export function rad2deg(rad: number): number {
+    return rad * DEG_RAD;
+}
+
 export function linearMapping(from1: number, from2: number, to1: number, to2: number): (arg: number) => number {
     const a = (to2 - to1) / (from2 - from1);
     const b = (to1 * from2 - to2 * from1) / (from2 - from1);
