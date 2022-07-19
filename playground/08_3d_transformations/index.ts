051f54183952773f595f1e8d0bffa7fb5393dcc3
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2022-07-19 11:16:18 +0300

    Add figure renderer

--- playground/08_3d_transformations/index.ts
+++ playground/08_3d_transformations/index.ts
@@ -1,15 +1,14 @@
 import {
     Runtime,
     BUFFER_MASK,
-    color,
     memoize,
+    color,
     Vec2,
     ZERO3, XUNIT3, YUNIT3, ZUNIT3, vec3,
-    mat4,
-    mul4x4, identity4x4, scaling4x4, rotation4x4,
-    perspective4x4, lookAt4x4,
+    mat4, mul4x4, identity4x4, scaling4x4, rotation4x4, perspective4x4, lookAt4x4, targetTo4x4, apply4x4,
 } from 'lib';
 import { makePrimitive } from './primitive';
+import { makeFigureRenderer } from './figure';
 
 /**
  * 3D Transformations.
@@ -18,20 +17,30 @@ import { makePrimitive } from './primitive';
  */
 export type DESCRIPTION = never;
 
-const CAMERA_DISTANCE = 3;
-const MODEL_SCALE = 1.2;
+const CAMERA_DISTANCE = 13;
 const PI2 = Math.PI * 2;
 
 const container = document.querySelector<HTMLElement>(PLAYGROUND_ROOT)!;
 const runtime = new Runtime(container);
 runtime.setClearColor(color(0.4, 0.4, 0.4));
 runtime.setDepthTest(true);
-runtime.setCulling(true);
-const primitive = makePrimitive(runtime);
+const figure1 = makeFigureRenderer(
+    makePrimitive(runtime),
+    2, vec3(1, 8, 0), 3, +0.11 * PI2 / 1000,
+);
+const figure2 = makeFigureRenderer(
+    makePrimitive(runtime),
+    0.9, vec3(1, 0, 0), 4, -0.15 * PI2 / 1000,
+);
+const figure3 = makeFigureRenderer(
+    makePrimitive(runtime),
+    0.7, vec3(0, 0, 1), 5, +0.19 * PI2 / 1000,
+);
 
-const projection = mat4();
+const proj = mat4();
 const view = mat4();
-const transform = mat4();
+const viewProj = mat4();
+const unit = identity4x4();
 
 let cameraAngle = 0;
 const cameraSpeed = 0.1 * PI2 / 1000;
@@ -43,14 +52,18 @@ runtime.onRender((delta) => {
     worldAngle = (worldAngle + delta * worldSpeed) % PI2;
 
     updateProjection(runtime.canvasSize());
-    updateView(cameraAngle);
-    updateWorld(worldAngle);
-    mul4x4(view, transform, transform);
-    mul4x4(projection, transform, transform);
+    updateView(0);
+    identity4x4(viewProj);
+    mul4x4(view, viewProj, viewProj);
+    mul4x4(proj, viewProj, viewProj);
+    figure1.update(viewProj, unit, delta);
+    figure2.update(viewProj, figure1.model(), delta);
+    figure3.update(viewProj, figure1.model(), delta);
 
     runtime.clearBuffer(BUFFER_MASK.COLOR | BUFFER_MASK.DEPTH);
-    primitive.program().setUniform('u_transform', transform, true);
-    primitive.render();
+    figure1.render();
+    figure2.render();
+    figure3.render();
     runtime.requestRender();
 });
 
@@ -60,22 +73,15 @@ const updateProjection = memoize(({ x, y }: Vec2): void => {
         yFov: Math.PI / 4,
         zNear: 0.001,
         zFar: 100,
-    }, projection);
+    }, proj);
 });
 
 function updateView(angle: number): void {
     const x = CAMERA_DISTANCE * Math.sin(angle);
     const z = CAMERA_DISTANCE * Math.cos(angle);
     lookAt4x4({
-        eye: { x, y: 0, z },
+        eye: { x, y: 3, z },
         center: ZERO3,
         up: YUNIT3,
     }, view);
-}
-
-function updateWorld(angle: number): void {
-    identity4x4(transform);
-    scaling4x4(vec3(MODEL_SCALE, MODEL_SCALE, MODEL_SCALE), transform);
-    rotation4x4(ZUNIT3, angle, transform);
-    rotation4x4(XUNIT3, angle, transform);
 }
