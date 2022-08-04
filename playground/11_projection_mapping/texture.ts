3c9440f69cdc00d1f999066949dc9d01884688d2
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2022-08-04 16:39:17 +0300

    Add texture

--- playground/11_projection_mapping/texture.ts
+++ playground/11_projection_mapping/texture.ts
@@ -7,7 +7,7 @@ export function makeTexture(runtime: Runtime, onReady: () => void): Texture {
     const texture = new Texture(runtime);
 
     const image = new Image();
-    image.src = '/static/leaves.jpg';
+    image.src = '/static/f-letter.png';
     image.onload = () => {
         image.onload = null;
         texture.setImageData(image, { unpackFlipY: true, generateMipmap: true });
