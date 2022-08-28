5d0f947f07cf3c41a1841d12272cca786c3cda27
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2022-08-28 23:17:56 +0300

    Add camera tests

---
+++ lib/gl/camera.test.ts
@@ -0,0 +1,9 @@
+import { Camera } from './camera';
+
+describe('camera', () => {
+    describe('Camera', () => {
+        it('initial state', () => {
+            const camera = new Camera();
+        });
+    });
+});
