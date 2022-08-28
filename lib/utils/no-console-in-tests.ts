a6d7cbd8a89bc6a423d5bb61640907b94c80297f
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2022-08-29 00:57:30 +0300

    Remove duplication

--- lib/utils/no-console-in-tests.ts
+++ lib/utils/no-console-in-tests.ts
@@ -2,7 +2,6 @@ import { logSilenced } from './logger';
 import { Vec2 } from '../geometry/vec2';
 import { Vec3 } from '../geometry/vec3';
 import { Vec4 } from '../geometry/vec4';
-logSilenced(true);
 
 declare global {
     // eslint-disable-next-line @typescript-eslint/no-namespace
@@ -21,173 +20,91 @@ declare global {
 }
 
 const EPS = 1E-4;
-const RANK2 = 2;
-const RANK3 = 3;
-const RANK4 = 4;
+function equal(lhs: number, rhs: number): boolean {
+    return Math.abs(lhs - rhs) < EPS;
+}
+
+function checkVec<T>(actual: T, expected: T, keys: string[]): jest.CustomMatcherResult {
+    // @ts-ignore Vector field.
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+    const checks = keys.map((key) => equal(actual[key], expected[key]));
+    if (checks.every(Boolean)) {
+        return {
+            pass: true,
+            message: () => 'OK',
+        };
+    }
+    return {
+        pass: false,
+        message: () => {
+            const lines: string[] = [];
+            checks.forEach((check, i) => {
+                if (!check) {
+                    const key = keys[i];
+                    // @ts-ignore Vector field.
+                    lines.push(`${key}: ${expected[key]} != ${actual[key]}`);
+                }
+            });
+            return lines.join('\n');
+        },
+    };
+}
+
+function checkMat(
+    actual: ReadonlyArray<number>, expected: ReadonlyArray<number>, rank: number,
+): jest.CustomMatcherResult {
+    const list: [number, number][] = [];
+    for (let i = 0; i < rank; ++i) {
+        for (let j = 0; j < rank; ++j) {
+            const act = actual[j * rank + i];
+            const exp = expected[i * rank + j];
+            if (!equal(act, exp)) {
+                list.push([i, j]);
+            }
+        }
+    }
+    if (list.length === 0) {
+        return {
+            pass: true,
+            message: () => 'OK',
+        };
+    }
+    return {
+        pass: false,
+        message: () => {
+            const lines: string[] = [];
+            for (const [i, j] of list) {
+                lines.push(`${i},${j}: ${expected[i * rank + j]} != ${actual[j * rank + i]}`);
+            }
+            return lines.join('\n');
+        },
+    };
+}
 
 expect.extend({
     toBeVec2(actual: Vec2, expected: Vec2) {
-        const keys = ['x', 'y'];
-        const checks = keys.map((key) => {
-            return Math.abs(actual[key as keyof Vec2] - expected[key as keyof Vec2]) < EPS;
-        });
-        if (checks.every(Boolean)) {
-            return {
-                pass: true,
-                message: () => 'OK',
-            };
-        }
-        return {
-            pass: false,
-            message: () => {
-                const lines: string[] = [];
-                checks.forEach((check, i) => {
-                    if (!check) {
-                        const key = keys[i] as keyof Vec2;
-                        lines.push(`${key}: ${expected[key]} != ${actual[key]}`);
-                    }
-                });
-                return lines.join('\n');
-            },
-        };
+        return checkVec(actual, expected, ['x', 'y']);
     },
 
     toBeVec3(actual: Vec3, expected: Vec3) {
-        const keys = ['x', 'y', 'z'];
-        const checks = keys.map((key) => {
-            return Math.abs(actual[key as keyof Vec3] - expected[key as keyof Vec3]) < EPS;
-        });
-        if (checks.every(Boolean)) {
-            return {
-                pass: true,
-                message: () => 'OK',
-            };
-        }
-        return {
-            pass: false,
-            message: () => {
-                const lines: string[] = [];
-                checks.forEach((check, i) => {
-                    if (!check) {
-                        const key = keys[i] as keyof Vec3;
-                        lines.push(`${key}: ${expected[key]} != ${actual[key]}`);
-                    }
-                });
-                return lines.join('\n');
-            },
-        };
+        return checkVec(actual, expected, ['x', 'y', 'z']);
     },
 
     toBeVec4(actual: Vec4, expected: Vec4) {
-        const keys = ['x', 'y', 'z', 'w'];
-        const checks = keys.map((key) => {
-            return Math.abs(actual[key as keyof Vec4] - expected[key as keyof Vec4]) < EPS;
-        });
-        if (checks.every(Boolean)) {
-            return {
-                pass: true,
-                message: () => 'OK',
-            };
-        }
-        return {
-            pass: false,
-            message: () => {
-                const lines: string[] = [];
-                checks.forEach((check, i) => {
-                    if (!check) {
-                        const key = keys[i] as keyof Vec4;
-                        lines.push(`${key}: ${expected[key]} != ${actual[key]}`);
-                    }
-                });
-                return lines.join('\n');
-            },
-        };
+        return checkVec(actual, expected, ['x', 'y', 'z', 'w']);
     },
 
     toBeMat2(actual: ReadonlyArray<number>, expected: ReadonlyArray<number>) {
-        const list: [number, number][] = [];
-        for (let i = 0; i < RANK2; ++i) {
-            for (let j = 0; j < RANK2; ++j) {
-                const act = actual[j * RANK2 + i];
-                const exp = expected[i * RANK2 + j];
-                if (Math.abs(act - exp) >= EPS) {
-                    list.push([i, j]);
-                }
-            }
-        }
-        if (list.length === 0) {
-            return {
-                pass: true,
-                message: () => 'OK',
-            };
-        }
-        return {
-            pass: false,
-            message: () => {
-                const lines: string[] = [];
-                for (const [i, j] of list) {
-                    lines.push(`${i},${j}: ${expected[i * RANK2 + j]} != ${actual[j * RANK2 + i]}`);
-                }
-                return lines.join('\n');
-            },
-        };
+        return checkMat(actual, expected, 2);
     },
 
     toBeMat3(actual: ReadonlyArray<number>, expected: ReadonlyArray<number>) {
-        const list: [number, number][] = [];
-        for (let i = 0; i < RANK3; ++i) {
-            for (let j = 0; j < RANK3; ++j) {
-                const act = actual[j * RANK3 + i];
-                const exp = expected[i * RANK3 + j];
-                if (Math.abs(act - exp) >= EPS) {
-                    list.push([i, j]);
-                }
-            }
-        }
-        if (list.length === 0) {
-            return {
-                pass: true,
-                message: () => 'OK',
-            };
-        }
-        return {
-            pass: false,
-            message: () => {
-                const lines: string[] = [];
-                for (const [i, j] of list) {
-                    lines.push(`${i},${j}: ${expected[i * RANK3 + j]} != ${actual[j * RANK3 + i]}`);
-                }
-                return lines.join('\n');
-            },
-        };
+        return checkMat(actual, expected, 3);
     },
 
     toBeMat4(actual: ReadonlyArray<number>, expected: ReadonlyArray<number>) {
-        const list: [number, number][] = [];
-        for (let i = 0; i < RANK4; ++i) {
-            for (let j = 0; j < RANK4; ++j) {
-                const act = actual[j * RANK4 + i];
-                const exp = expected[i * RANK4 + j];
-                if (Math.abs(act - exp) >= EPS) {
-                    list.push([i, j]);
-                }
-            }
-        }
-        if (list.length === 0) {
-            return {
-                pass: true,
-                message: () => 'OK',
-            };
-        }
-        return {
-            pass: false,
-            message: () => {
-                const lines: string[] = [];
-                for (const [i, j] of list) {
-                    lines.push(`${i},${j}: ${expected[i * RANK4 + j]} != ${actual[j * RANK4 + i]}`);
-                }
-                return lines.join('\n');
-            },
-        };
+        return checkMat(actual, expected, 4);
     },
 });
+
+logSilenced(true);
