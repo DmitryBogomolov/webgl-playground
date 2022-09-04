660451559bd6c7eda78195698b20cdf27ab2d50f
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2022-09-04 12:46:49 +0300

    Add controls

--- playground/12_render_to_texture/index.ts
+++ playground/12_render_to_texture/index.ts
@@ -4,9 +4,12 @@ import {
     Framebuffer,
     Camera,
     vec3, YUNIT3, norm3, rotate3,
-    Mat4, translation4x4, rotation4x4,
+    Mat4, mat4, identity4x4, translation4x4, apply4x4, xrotation4x4, yrotation4x4,
     Color, color,
+    deg2rad,
 } from 'lib';
+import { observable, computed } from 'util/observable';
+import { createControls } from 'util/controls';
 import { makeCube, makePlane } from './primitive';
 
 /**
@@ -50,6 +53,10 @@ const objects: ReadonlyArray<ObjectInfo> = [
 ];
 const lightDir = norm3(vec3(1, 3, 2));
 
+const animationFlag = observable(true);
+const xRotation = observable(0);
+const yRotation = observable(30);
+
 let textureCameraPos = vec3(0, 2, 5);
 const CAMERA_ROTATION_SPEED = (2 * Math.PI) * 0.1;
 const textureCamera = new Camera();
@@ -57,7 +64,15 @@ textureCamera.setEyePos(textureCameraPos);
 
 const targetCamera = new Camera();
 targetCamera.setEyePos(vec3(0, 0, 2));
-const targetModel = rotation4x4(YUNIT3, Math.PI / 6);
+
+const _targetModel = mat4();
+const targetModel = computed(([xRotation, yRotation]) => {
+    const mat = _targetModel;
+    identity4x4(mat);
+    apply4x4(mat, xrotation4x4, deg2rad(xRotation));
+    apply4x4(mat, yrotation4x4, deg2rad(yRotation));
+    return mat;
+}, [xRotation, yRotation]);
 
 const texture = new Texture(runtime);
 texture.setParameters({
@@ -101,17 +116,30 @@ function renderTarget(): void {
     texture.setUnit(2);
     const program = plane.program();
     program.setUniform('u_view_proj', targetCamera.getTransformMat());
-    program.setUniform('u_model', targetModel);
+    program.setUniform('u_model', targetModel());
     program.setUniform('u_texture', 2);
     plane.render();
 }
 
+[animationFlag, targetModel]
+    .forEach((item) => item.on(() => runtime.requestFrameRender()));
+
 runtime.frameRendered().on((delta) => {
-    textureCameraPos = rotate3(textureCameraPos, YUNIT3, CAMERA_ROTATION_SPEED * delta / 1000);
-    textureCamera.setEyePos(textureCameraPos);
+    if (animationFlag()) {
+        textureCameraPos = rotate3(textureCameraPos, YUNIT3, CAMERA_ROTATION_SPEED * delta / 1000);
+        textureCamera.setEyePos(textureCameraPos);
+    }
 
     renderTexture();
     renderTarget();
 
-    runtime.requestFrameRender();
+    if (animationFlag()) {
+        runtime.requestFrameRender();
+    }
 });
+
+createControls(container, [
+    { label: 'animation', checked: animationFlag },
+    { label: 'x rotation', value: xRotation, min: -45, max: +45 },
+    { label: 'y rotation', value: yRotation, min: -45, max: +45 },
+]);
