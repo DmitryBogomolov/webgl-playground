ff9c3efa304888a3696fecb3ebda1d0defee3a11
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2022-09-15 13:28:09 +0300

    Add controls

--- playground/13_shadows_and_texture/index.ts
+++ playground/13_shadows_and_texture/index.ts
@@ -3,10 +3,12 @@ import {
     Primitive,
     Framebuffer,
     Camera,
-    Vec3, vec3,
+    Vec3, vec3, mul3,
     Mat4, translation4x4, inversetranspose4x4,
     Color, colors, color, Program,
+    deg2rad,
 } from 'lib';
+import { observable, computed } from 'util/observable';
 import { createControls } from 'util/controls';
 import { makeProgram, makeDepthProgram, makeCube, makeSphere } from './primitive';
 
@@ -24,14 +26,48 @@ runtime.setDepthTest(true);
 const backgroundColor = color(0.7, 0.7, 0.7);
 const depthDataBackgroundColor = color(1, 1, 1);
 
-const depthCamera = new Camera();
-const camera = new Camera();
-depthCamera.setEyePos(vec3(-5, 0, 2));
+const viewLon = observable(0);
+const lightLon = observable(-60);
+const lightLat = observable(0);
+const lightDist = observable(5);
 // Z range should be quite small. Z precision goes down with distance from camera
 // and total Z range further affects it. So Z range should be as small as possible.
-depthCamera.setZNear(2);
-depthCamera.setZFar(10);
+const zNear = observable(0.5);
+const zFar = observable(10);
+
+const depthCamera = new Camera();
+const camera = new Camera();
 camera.setEyePos(vec3(-2, 3, 5));
+
+viewLon.on((viewLon) => {
+    const lon = deg2rad(viewLon);
+    const pos = vec3(
+        5 * Math.sin(lon),
+        3,
+        5 * Math.cos(lon),
+    );
+    camera.setEyePos(pos);
+});
+
+const lightPos = computed(([lightLon, lightLat, lightDist]) => {
+    const lon = deg2rad(lightLon);
+    const lat = deg2rad(lightLat);
+    return mul3(vec3(
+        Math.cos(lat) * Math.sin(lon),
+        Math.sin(lat),
+        Math.cos(lat) * Math.cos(lon),
+    ), lightDist);
+}, [lightLon, lightLat, lightDist]);
+
+lightPos.on((lightPos) => {
+    depthCamera.setEyePos(lightPos);
+});
+zNear.on((zNear) => {
+    depthCamera.setZNear(zNear);
+});
+zFar.on((zFar) => {
+    depthCamera.setZFar(zFar);
+});
 
 const framebuffer = new Framebuffer(runtime);
 framebuffer.setup('color|depth', { x: 512, y: 512 }, true);
@@ -116,5 +152,15 @@ runtime.frameRendered().on(() => {
     renderScene(program, camera, depthCamera);
 });
 
+[camera.changed(), depthCamera.changed()].forEach((item) => {
+    item.on(() => runtime.requestFrameRender());
+});
+
 createControls(container, [
+    { label: 'view lon', value: viewLon, min: -180, max: +180 },
+    { label: 'light lon', value: lightLon, min: -180, max: +180 },
+    { label: 'light lat', value: lightLat, min: -60, max: +60 },
+    { label: 'light dist', value: lightDist, min: 2, max: 10, step: 0.5 },
+    { label: 'z near', value: zNear, min: 0.1, max: 2, step: 0.1 },
+    { label: 'z far', value: zFar, min: 7, max: 20, step: 1 },
 ]);
