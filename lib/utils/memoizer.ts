dd38309975d01f96974321f7e719277f33f537f6
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2022-09-28 13:15:45 +0300

    Add comparator to memoize

--- lib/utils/memoizer.ts
+++ lib/utils/memoizer.ts
@@ -1,24 +1,19 @@
-export function memoize<T extends (...args: readonly unknown[]) => unknown>(func: T): T {
-    let memoizedArgs: Parameters<T> = [] as unknown as Parameters<T>;
+import { compareArrays } from './compare';
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function memoize<T extends (...args: any[]) => any>(
+    func: T,
+    compareArgs: (lhs: Parameters<T>, rhs: Parameters<T>) => boolean = compareArrays,
+): T {
+    let memoizedArgs: Parameters<T> = null as unknown as Parameters<T>;
     let memoizedValue: ReturnType<T>;
     function memoizedFunc(...args: Parameters<T>): ReturnType<T> {
-        if (!compareArgs(memoizedArgs, args)) {
+        if (!memoizedArgs || !compareArgs(memoizedArgs, args)) {
             memoizedArgs = args;
             memoizedValue = func(...memoizedArgs) as ReturnType<T>;
         }
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-return
         return memoizedValue;
     }
     return memoizedFunc as T;
-}
-
-function compareArgs<TArgs extends readonly unknown[]>(args1: TArgs, args2: TArgs): boolean {
-    if (args1.length !== args2.length) {
-        return false;
-    }
-    for (let i = 0; i < args1.length; ++i) {
-        if (args1[i] !== args2[i]) {
-            return false;
-        }
-    }
-    return true;
 }
