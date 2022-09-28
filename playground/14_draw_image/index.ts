4c9e58d04ffbdbcdb190cd7cac02cd09ffc76f45
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2022-09-28 16:38:01 +0300

    Update demo

--- playground/14_draw_image/index.ts
+++ playground/14_draw_image/index.ts
@@ -1,6 +1,6 @@
 import {
     Runtime,
-    ImageRenderer,
+    ImageRenderer, ImageRendererRawImageData,
     color, colors, color2uint,
 } from 'lib';
 
@@ -16,94 +16,143 @@ const runtime = new Runtime(container);
 runtime.setDepthTest(true);
 runtime.setClearColor(color(0.7, 0.7, 0.7));
 
-const image1 = new ImageRenderer(runtime, 'image/cells');
-image1.setTextureUnit(3);
-const image2 = new ImageRenderer(runtime, 'image/f-letter');
-image2.setTextureUnit(4);
-const image3 = new ImageRenderer(runtime, 'image/f-letter/part');
-image2.setTextureUnit(5);
-const image4 = new ImageRenderer(runtime, 'image/leaves');
-image4.setTextureUnit(6);
+const imageCells = new ImageRenderer(runtime, 'image/cells');
+imageCells.setTextureUnit(3);
+const imageLetter = new ImageRenderer(runtime, 'image/f-letter');
+imageLetter.setTextureUnit(4);
+const imageLeaves = new ImageRenderer(runtime, 'image/leaves');
+imageLeaves.setTextureUnit(5);
 
-{
-    const arr = new Uint32Array(8 * 8);
-    for (let i = 0; i < 8; ++i) {
-        for (let j = 0; j < 8; ++j) {
+void imageCells.setImageData(generateTextureData());
+void imageLetter.setImageData({ url: '/static/f-letter.png' }).then(() => {
+    runtime.requestFrameRender();
+});
+void imageLeaves.setImageData({ url: '/static/leaves.jpg' }).then(() => {
+    runtime.requestFrameRender();
+});
+
+let step = 0;
+const SPEED = 0.1;
+
+runtime.frameRendered().on((delta) => {
+    runtime.clearBuffer('color');
+    step = (step + SPEED * delta / 1000) % 1;
+
+    render1(imageLeaves, step);
+    render2(imageCells, step);
+    render3(imageLetter, step);
+
+    runtime.requestFrameRender();
+});
+
+function render1(image: ImageRenderer, step: number): void {
+    const { y: height } = runtime.viewportSize();
+    const size = image.imageSize();
+
+    image.setLocation({
+        x1: 10,
+        y2: 10,
+    });
+    image.render();
+
+    image.setLocation({
+        x1: 10,
+        y1: 10,
+        width: size.x * 0.9,
+        height: size.y * 0.9,
+    });
+    image.render();
+
+    const w = size.x * 0.5;
+    const h = size.y * 0.5;
+    const range = height - (10 + size.y) - (10 + size.y * 0.9) - h;
+    image.setLocation({
+        x1: 10,
+        y2: 10 + size.y + range * (step > 0.5 ? 1 - step : step) * 2,
+        width: w,
+        height: h,
+    });
+    image.render();
+}
+
+function render2(image: ImageRenderer, step: number): void {
+    const { x: width } = runtime.viewportSize();
+    const size = image.imageSize();
+
+    image.setLocation({
+        x2: 10,
+        y2: 10,
+        width: size.x,
+        height: size.y,
+    });
+    image.render();
+
+    image.setLocation({
+        y2: 10,
+        x1: 300,
+        width: size.x * 0.9,
+        height: size.y * 0.9,
+    });
+    image.render();
+
+    const w = size.x * 0.75;
+    const h = size.y * 0.75;
+    const range = width - (10 + size.x) - (300 + size.x * 0.9) - w;
+    image.setLocation({
+        y2: 10,
+        x1: 300 + size.x * 0.9 + range * (step > 0.5 ? 1 - step : step) * 2,
+        width: w,
+        height: h,
+    });
+    image.render();
+}
+
+function render3(image: ImageRenderer, step: number): void {
+    const size = image.imageSize();
+
+    image.setRegion({});
+    image.setLocation({
+        x2: 10,
+        y1: 10,
+    });
+    image.render();
+
+    image.setLocation({
+        x2: 10 + size.x + 60,
+        y1: 10 + size.y * 0.1,
+        width: size.x * 0.8,
+        height: size.y * 0.8,
+        rotation: step * Math.PI * 2,
+    });
+    image.render();
+
+    image.setLocation({
+        x2: 10 + size.x + 60 + size.x * 0.8 + 60,
+        y1: 10 + 35,
+    });
+    image.setRegion({
+        y1: 35,
+        y2: 45,
+        rotation: step * Math.PI * 2,
+    });
+    image.render();
+}
+
+function generateTextureData(): ImageRendererRawImageData {
+    const SIZE = 8 * 24;
+    const arr = new Uint32Array(SIZE * SIZE);
+    for (let i = 0; i < SIZE; ++i) {
+        const ti = (i / SIZE * 8) | 0;
+        for (let j = 0; j < SIZE; ++j) {
+            const tj = (j / SIZE * 8) | 0;
             let c;
-            if ((i + j) % 2) {
+            if ((ti + tj) % 2) {
                 c = colors.CYAN;
             } else {
                 c = colors.MAGENTA;
             }
-            arr[i * 8 + j] = color2uint(c);
+            arr[i * SIZE + j] = color2uint(c);
         }
     }
-    void image1.setImageData({ data: new Uint8Array(arr.buffer), size: { x: 8, y: 8 } });
+    return { data: new Uint8Array(arr.buffer), size: { x: SIZE, y: SIZE } };
 }
-void image2.setImageData({ url: '/static/f-letter.png' }).then(() => {
-    runtime.requestFrameRender();
-});
-void image3.setImageData({ url: '/static/f-letter.png' }).then(() => {
-    runtime.requestFrameRender();
-});
-void image4.setImageData({ url: '/static/leaves.jpg' }).then(() => {
-    runtime.requestFrameRender();
-});
-
-image1.setLocation({
-    y2: 100,
-    x2: 30,
-    width: 200,
-    height: 200,
-});
-image2.setRegion({
-    // x1: 256-40,
-    // x2: 256-40,
-    x1: 40,
-    // x2: 40,
-    y1: 35,
-    y2: 45,
-    // rotation: Math.PI * 0.1,
-});
-image2.setLocation({
-    x1: 400,
-    // x2: -256,
-    y2: 10,
-    // rotation: Math.PI / 6,
-    // y2: +10,
-});
-image3.setLocation({
-    x1: 400,
-    y2: 310,
-});
-// image.setRotation(Math.PI / 4);
-// image.setSize({ x: 256, y: 256 });
-image4.setLocation({
-    y1: 10,
-    x1: 10,
-});
-
-const ROTATION_SPEED = (2 * Math.PI) * 0.1;
-
-let angle = 0;
-
-runtime.frameRendered().on((delta) => {
-    runtime.clearBuffer('color');
-
-    // angle += ROTATION_SPEED * delta / 1000;
-    // // image.setRegion({
-    // //     ...image.getRegion(),
-    // //     rotation: angle,
-    // // });
-    // image2.setLocation({
-    //     ...image2.getLocation(),
-    //     rotation: angle,
-    // });
-
-    image1.render();
-    image2.render();
-    image3.render();
-    image4.render();
-
-    // runtime.requestFrameRender();
-});
