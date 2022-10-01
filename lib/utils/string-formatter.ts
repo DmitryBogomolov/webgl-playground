d465b5dde4b68cc12918f80720aadd6321d73a25
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2022-10-01 22:54:09 +0300

    Update string format

--- lib/utils/string-formatter.ts
+++ lib/utils/string-formatter.ts
@@ -1,13 +1,27 @@
+const objectToString = Object.prototype.toString;
+
 export function formatStr(format: string, ...params: unknown[]): string {
     return format.replace(/{(\d+)}/g, (_match, i: number) => {
         const param = params[i];
-        const type = typeof param;
-        if (type === 'object') {
-            return JSON.stringify(param);
-        } else if (type === 'symbol') {
-            return (param as symbol).toString();
-        } else {
-            return param as string;
+        if (param === null || param === undefined) {
+            return String(param);
         }
+        const type = typeof param;
+        if (type === 'symbol') {
+            return String(param);
+        }
+        if (Array.isArray(param)) {
+            return JSON.stringify(param);
+        }
+        if (type === 'object') {
+            const obj = param as Object;
+            if (obj.toString !== objectToString) {
+                return obj.toString();
+            }
+            if (objectToString.call(obj) === '[object Object]') {
+                return JSON.stringify(obj);
+            }
+        }
+        return String(param);
     });
 }
