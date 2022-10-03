9d1610df591d638ff31b1d2ceb97e245e84ccc4d
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2022-10-03 13:35:24 +0300

    Add cube rotation

--- playground/16_skybox/index.ts
+++ playground/16_skybox/index.ts
@@ -4,9 +4,10 @@ import {
     TextureCube,
     Camera,
     mul3,
+    Mat4, mat4, identity4x4, apply4x4, yrotation4x4, xrotation4x4, inversetranspose4x4,
     deg2rad, spherical2zxy,
 } from 'lib';
-import { observable, computed } from 'util/observable';
+import { Observable, observable, computed } from 'util/observable';
 import { createControls } from 'util/controls';
 import { makeQuad, makeCube } from './primitive';
 import { makeTexture } from './texture';
@@ -42,22 +43,46 @@ function main(): void {
         camera.setEyePos(cameraPos);
     });
 
-    camera.changed().on(() => runtime.requestFrameRender());
+    const modelLon = observable(0);
+    const modelLat = observable(0);
+    const _modelMat = mat4();
+    const modelMat = computed(([modelLon, modelLat]) => {
+        const mat = _modelMat;
+        identity4x4(mat);
+        apply4x4(mat, yrotation4x4, deg2rad(modelLon));
+        apply4x4(mat, xrotation4x4, deg2rad(modelLat));
+        return mat;
+    }, [modelLon, modelLat]);
+
+    const _normalMat = mat4();
+    const normalMat = computed(([modelMat]) => {
+        return inversetranspose4x4(modelMat, _normalMat);
+    }, [modelMat]);
+
+    [modelMat, normalMat, camera.changed()].forEach(
+        (proxy) => proxy.on(() => runtime.requestFrameRender())
+    );
     runtime.sizeChanged().on(() => {
         camera.setViewportSize(runtime.canvasSize());
     });
     runtime.frameRendered().on(() => {
-        renderFrame(runtime, camera, quad, cube, texture);
+        renderFrame(runtime, camera, modelMat, normalMat, quad, cube, texture);
     });
 
     createControls(container, [
         { label: 'camera lon', value: cameraLon, min: -180, max: +180 },
         { label: 'camera lat', value: cameraLat, min: -80, max: +80 },
         { label: 'camera dist', value: cameraDist, min: 1, max: 5, step: 0.2 },
+        { label: 'model lon', value: modelLon, min: -90, max: +90 },
+        { label: 'model lat', value: modelLat, min: -90, max: +90 },
     ]);
 }
 
-function renderFrame(runtime: Runtime, camera: Camera, quad: Primitive, cube: Primitive, texture: TextureCube): void {
+function renderFrame(
+    runtime: Runtime, camera: Camera,
+    modelMat: Observable<Mat4>, normalMat: Observable<Mat4>,
+    quad: Primitive, cube: Primitive, texture: TextureCube,
+): void {
     runtime.clearBuffer('color|depth');
 
     runtime.setCubeTextureUnit(4, texture);
@@ -66,6 +91,8 @@ function renderFrame(runtime: Runtime, camera: Camera, quad: Primitive, cube: Pr
     runtime.setDepthFunc('less');
     cube.program().setUniform('u_texture', 4);
     cube.program().setUniform('u_view_proj', camera.getTransformMat());
+    cube.program().setUniform('u_model', modelMat());
+    cube.program().setUniform('u_model_invtrs', normalMat());
     cube.program().setUniform('u_camera_position', camera.getEyePos());
     cube.render();
 
--- playground/16_skybox/shaders/reflect.vert
+++ playground/16_skybox/shaders/reflect.vert
@@ -2,12 +2,15 @@ attribute vec3 a_position;
 attribute vec3 a_normal;
 
 uniform mat4 u_view_proj;
+uniform mat4 u_model;
+uniform mat4 u_model_invtrs;
 
 varying vec3 v_position;
 varying vec3 v_normal;
 
 void main() {
-    gl_Position = u_view_proj * vec4(a_position, 1.0);
-    v_position = a_position;
-    v_normal = a_normal;
+    vec4 position = u_model * vec4(a_position, 1.0);
+    gl_Position = u_view_proj * position;
+    v_position = position.xyz;
+    v_normal = mat3(u_model_invtrs) * a_normal;
 }
