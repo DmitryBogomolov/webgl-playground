214324e8ddbb6c3d8442d34f58b5f249bd4f8d5d
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2022-10-16 14:58:01 +0300

    Add main function

--- playground/13_shadows_and_texture/index.ts
+++ playground/13_shadows_and_texture/index.ts
@@ -29,57 +29,7 @@ import { makeProgram, makeDepthProgram, makeCube, makeSphere, makeWireframe } fr
  */
 export type DESCRIPTION = never;
 
-const container = document.querySelector<HTMLElement>(PLAYGROUND_ROOT)!;
-const runtime = new Runtime(container, { extensions: ['depth_texture'] });
-runtime.setDepthTest(true);
-
-const backgroundColor = color(0.7, 0.7, 0.7);
-const depthDataBackgroundColor = color(1, 1, 1);
-
-const viewLon = observable(0);
-const lightLon = observable(-60);
-const lightLat = observable(0);
-const lightDist = observable(5);
-// Z range should be quite small. Z precision goes down with distance from camera
-// and total Z range further affects it. So Z range should be as small as possible.
-const zNear = observable(0.5);
-const zFar = observable(10);
-
-const depthCamera = new Camera();
-const camera = new Camera();
-camera.setEyePos(vec3(-2, 3, 5));
-
-viewLon.on((viewLon) => {
-    const lon = deg2rad(viewLon);
-    const pos = vec3(
-        5 * Math.sin(lon),
-        3,
-        5 * Math.cos(lon),
-    );
-    camera.setEyePos(pos);
-});
-
-const lightPos = computed(([lightLon, lightLat, lightDist]) => {
-    const dir = spherical2zxy({ azimuth: deg2rad(lightLon), elevation: deg2rad(lightLat) });
-    return mul3(dir, lightDist);
-}, [lightLon, lightLat, lightDist]);
-
-lightPos.on((lightPos) => {
-    depthCamera.setEyePos(lightPos);
-});
-zNear.on((zNear) => {
-    depthCamera.setZNear(zNear);
-});
-zFar.on((zFar) => {
-    depthCamera.setZFar(zFar);
-});
-
-const framebuffer = new Framebuffer(runtime, {
-    attachment: 'color|depth',
-    useDepthTexture: true,
-    size: { x: 512, y: 512 },
-});
-depthCamera.setViewportSize(framebuffer.size());
+main();
 
 interface ObjectInfo {
     readonly primitive: Primitive;
@@ -87,6 +37,122 @@ interface ObjectInfo {
     readonly modelInvtrs: Mat4;
     readonly color: Color;
 }
+
+interface State {
+    readonly runtime: Runtime;
+    readonly program: Program;
+    readonly depthProgram: Program;
+    readonly framebuffer: Framebuffer;
+    readonly camera: Camera;
+    readonly depthCamera: Camera;
+    readonly backgroundColor: Color;
+    readonly depthDataBackgroundColor: Color;
+    readonly objects: ReadonlyArray<ObjectInfo>;
+    readonly wireframe: Primitive;
+}
+
+function main(): void {
+    const container = document.querySelector<HTMLElement>(PLAYGROUND_ROOT)!;
+    const runtime = new Runtime(container, { extensions: ['depth_texture'] });
+    runtime.setDepthTest(true);
+
+    const viewLon = observable(0);
+    const lightLon = observable(-60);
+    const lightLat = observable(0);
+    const lightDist = observable(5);
+    // Z range should be quite small. Z precision goes down with distance from camera
+    // and total Z range further affects it. So Z range should be as small as possible.
+    const zNear = observable(0.5);
+    const zFar = observable(10);
+
+    const depthCamera = new Camera();
+    const camera = new Camera();
+    camera.setEyePos(vec3(-2, 3, 5));
+
+    viewLon.on((viewLon) => {
+        const lon = deg2rad(viewLon);
+        const pos = vec3(
+            5 * Math.sin(lon),
+            3,
+            5 * Math.cos(lon),
+        );
+        camera.setEyePos(pos);
+    });
+
+    const lightPos = computed(([lightLon, lightLat, lightDist]) => {
+        const dir = spherical2zxy({ azimuth: deg2rad(lightLon), elevation: deg2rad(lightLat) });
+        return mul3(dir, lightDist);
+    }, [lightLon, lightLat, lightDist]);
+
+    lightPos.on((lightPos) => {
+        depthCamera.setEyePos(lightPos);
+    });
+    zNear.on((zNear) => {
+        depthCamera.setZNear(zNear);
+    });
+    zFar.on((zFar) => {
+        depthCamera.setZFar(zFar);
+    });
+
+    const framebuffer = new Framebuffer(runtime, {
+        attachment: 'color|depth',
+        useDepthTexture: true,
+        size: { x: 512, y: 512 },
+    });
+    depthCamera.setViewportSize(framebuffer.size());
+
+    runtime.sizeChanged().on(() => {
+        camera.setViewportSize(runtime.canvasSize());
+    });
+
+    const state: State = {
+        runtime,
+        program: makeProgram(runtime),
+        depthProgram: makeDepthProgram(runtime),
+        framebuffer,
+        camera,
+        depthCamera,
+        backgroundColor: color(0.7, 0.7, 0.7),
+        depthDataBackgroundColor: colors.WHITE,
+        objects: [
+            makeObject(
+                makeCube(runtime, 1.8),
+                vec3(+2, 0, 1),
+                colors.CYAN,
+            ),
+            makeObject(
+                makeSphere(runtime, 1.5),
+                vec3(-1, 0, 1),
+                colors.MAGENTA,
+            ),
+            makeObject(
+                makeSphere(runtime, 1.2),
+                vec3(0, 0, -2),
+                colors.BLUE,
+            ),
+        ],
+        wireframe: makeWireframe(runtime),
+    };
+
+    runtime.frameRendered().on(() => {
+        renderDepthData(state);
+        renderScene(state);
+    });
+
+    [camera.changed(), depthCamera.changed()].forEach((item) => {
+        item.on(() => runtime.requestFrameRender());
+    });
+
+    createControls(container, [
+        { label: 'view lon', value: viewLon, min: -180, max: +180 },
+        { label: 'light lon', value: lightLon, min: -180, max: +180 },
+        { label: 'light lat', value: lightLat, min: -60, max: +60 },
+        { label: 'light dist', value: lightDist, min: 2, max: 10, step: 0.5 },
+        { label: 'z near', value: zNear, min: 0.1, max: 2, step: 0.1 },
+        { label: 'z far', value: zFar, min: 7, max: 20, step: 1 },
+    ]);
+}
+
 function makeObject(primitive: Primitive, offset: Vec3, clr: Color): ObjectInfo {
     const model = translation4x4(offset);
     const modelInvtrs = inversetranspose4x4(model);
@@ -97,31 +163,6 @@ function makeObject(primitive: Primitive, offset: Vec3, clr: Color): ObjectInfo 
         color: clr,
     };
 }
-const objects = [
-    makeObject(
-        makeCube(runtime, 1.8),
-        vec3(+2, 0, 1),
-        colors.CYAN,
-    ),
-    makeObject(
-        makeSphere(runtime, 1.5),
-        vec3(-1, 0, 1),
-        colors.MAGENTA,
-    ),
-    makeObject(
-        makeSphere(runtime, 1.2),
-        vec3(0, 0, -2),
-        colors.BLUE,
-    ),
-] as const;
-
-const program = makeProgram(runtime);
-const depthProgram = makeDepthProgram(runtime);
-const wireframe = makeWireframe(runtime);
-
-runtime.sizeChanged().on(() => {
-    camera.setViewportSize(runtime.canvasSize());
-});
 
 function renderObjects(
     list: ReadonlyArray<ObjectInfo>, program: Program, setUniforms: (obj: ObjectInfo) => void,
@@ -133,23 +174,27 @@ function renderObjects(
     }
 }
 
-function renderDepthData(program: Program, camera: Camera): void {
+function renderDepthData({
+    runtime, framebuffer, depthDataBackgroundColor, depthProgram, depthCamera, objects,
+}: State): void {
     runtime.setRenderTarget(framebuffer);
     runtime.setClearColor(depthDataBackgroundColor);
     runtime.clearBuffer('color|depth');
-    program.setUniform('u_view_proj', camera.getTransformMat());
-    renderObjects(objects, program, (obj) => {
-        program.setUniform('u_model', obj.model);
+    depthProgram.setUniform('u_view_proj', depthCamera.getTransformMat());
+    renderObjects(objects, depthProgram, (obj) => {
+        depthProgram.setUniform('u_model', obj.model);
     });
 }
 
-function renderWireframe(): void {
+function renderWireframe(wireframe: Primitive, camera: Camera, depthCamera: Camera): void {
     wireframe.program().setUniform('u_view_proj', camera.getTransformMat());
     wireframe.program().setUniform('u_model', depthCamera.getInvtransformMat());
     wireframe.render();
 }
 
-function renderScene(program: Program, camera: Camera, depthCamera: Camera): void {
+function renderScene({
+    runtime, backgroundColor, program, framebuffer, camera, depthCamera, objects, wireframe,
+}: State): void {
     runtime.setRenderTarget(null);
     runtime.setClearColor(backgroundColor);
     runtime.clearBuffer('color|depth');
@@ -165,23 +210,5 @@ function renderScene(program: Program, camera: Camera, depthCamera: Camera): voi
         program.setUniform('u_model_invtrs', obj.modelInvtrs);
         program.setUniform('u_color', obj.color);
     });
-    renderWireframe();
+    renderWireframe(wireframe, camera, depthCamera);
 }
-
-runtime.frameRendered().on(() => {
-    renderDepthData(depthProgram, depthCamera);
-    renderScene(program, camera, depthCamera);
-});
-
-[camera.changed(), depthCamera.changed()].forEach((item) => {
-    item.on(() => runtime.requestFrameRender());
-});
-
-createControls(container, [
-    { label: 'view lon', value: viewLon, min: -180, max: +180 },
-    { label: 'light lon', value: lightLon, min: -180, max: +180 },
-    { label: 'light lat', value: lightLat, min: -60, max: +60 },
-    { label: 'light dist', value: lightDist, min: 2, max: 10, step: 0.5 },
-    { label: 'z near', value: zNear, min: 0.1, max: 2, step: 0.1 },
-    { label: 'z far', value: zFar, min: 7, max: 20, step: 1 },
-]);
