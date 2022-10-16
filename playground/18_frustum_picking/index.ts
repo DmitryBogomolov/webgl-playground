ad4bbcb00883ae56bcaa43f364b989b386a19c2f
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2022-10-16 13:49:43 +0300

    Refactor

--- playground/18_frustum_picking/index.ts
+++ playground/18_frustum_picking/index.ts
@@ -5,10 +5,9 @@ import {
     Camera,
     Vec2, vec2, ZERO2,
     vec3, mul3,
-    mul4x4, frustum4x4,
+    Mat4, mat4, apply4x4, clone4x4, frustum4x4,
     Color, color, colors,
     uint2bytes, makeEventCoordsGetter, spherical2zxy, deg2rad,
-    linearMapping,
 } from 'lib';
 import { observable, computed } from 'util/observable';
 import { createControls } from 'util/controls';
@@ -107,23 +106,7 @@ function findCurrentPixel({ runtime, framebuffer, camera, objects, idProgram, pi
     runtime.setClearColor(colors.NONE);
     runtime.clearBuffer('color|depth');
 
-    const dy = camera.getZNear() * Math.tan(camera.getYFov() / 2);
-    const dx = camera.getAspect() * dy;
-
-    const mapX = linearMapping(0, runtime.canvasSize().x, -dx, +dx);
-    const mapY = linearMapping(0, runtime.canvasSize().y, -dy, +dy);
-
-    const x1 = mapX(pixelCoord.x);
-    const x2 = mapX(pixelCoord.x + 1);
-    const y1 = mapY(pixelCoord.y);
-    const y2 = mapY(pixelCoord.y + 1);
-
-    const projMat = frustum4x4({
-        left: x1, right: x2, bottom: y1, top: y2,
-        zNear: camera.getZNear(), zFar: camera.getZFar(),
-    });
-    const transformMat = mul4x4(projMat, camera.getViewMat());
-
+    const transformMat = makeIdViewProjMat(camera, pixelCoord);
     for (const { primitive, modelMat, id } of objects) {
         primitive.setProgram(idProgram);
         idProgram.setUniform('u_view_proj', transformMat);
@@ -176,19 +159,21 @@ function makeObjects(runtime: Runtime): {
     return { objects, program, idProgram };
 }
 
-// In perspective projection higher aspect means extra horizontal space on left and right sides.
-// I.e. if some pixel has coordinate x1 in a1 aspect then in a2 > a1 aspect that pixel would have
-// coordinate x2 > x1 if x1 < x_center and x2 < x1 of x1 > x_center. Y coordinate stays the same.
-function mapPixelCoodinates(coords: Vec2, size1: Vec2, size2: Vec2): Vec2 {
-    const aspect1 = size1.x / size1.y;
-    const aspect2 = size2.x / size2.y;
-    // Convert coordinates from [0, w1] * [0, h1] space to [-1, +1] * [-1, +1] space.
-    // Adjust [-1, +1] coordinates assuming that aspect1 * x1 == aspect2 * x2, y1 == y2.
-    const x = (2 * coords.x / size1.x - 1) * (aspect1 / aspect2);
-    const y = 2 * coords.y / size1.y - 1;
-    // Convert coodinates from [-1, +1] * [-1, +1] to [0, w2] * [0, h2].
-    return vec2(
-        Math.round(size2.x * (x + 1) / 2),
-        Math.round(size2.y * (y + 1) / 2),
-    );
+const _idViewProjMat = mat4();
+function makeIdViewProjMat(camera: Camera, { x, y }: Vec2): Mat4 {
+    const dy = camera.getZNear() * Math.tan(camera.getYFov() / 2);
+    const dx = camera.getAspect() * dy;
+    const { x: xViewport, y: yViewport } = camera.getViewportSize();
+    const x1 = dx * (2 * x / xViewport - 1);
+    const x2 = dx * (2 * (x + 1) / xViewport - 1);
+    const y1 = dy * (2 * y / yViewport - 1);
+    const y2 = dy * (2 * (y + 1) / yViewport - 1);
+
+    const mat = _idViewProjMat;
+    clone4x4(camera.getViewMat(), mat);
+    apply4x4(mat, frustum4x4, {
+        left: x1, right: x2, bottom: y1, top: y2,
+        zNear: camera.getZNear(), zFar: camera.getZFar(),
+    });
+    return mat;
 }
