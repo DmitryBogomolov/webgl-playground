ba9324c374a0c83956fae74fdcbb5cedffd9a651
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2022-10-16 15:08:57 +0300

    Add main function

--- playground/12_render_to_texture/index.ts
+++ playground/12_render_to_texture/index.ts
@@ -1,13 +1,14 @@
 import {
     Runtime,
     Framebuffer,
+    Primitive,
     Camera,
-    vec3, YUNIT3, norm3, rotate3,
+    Vec3, vec3, YUNIT3, norm3, rotate3,
     Mat4, mat4, identity4x4, translation4x4, apply4x4, xrotation4x4, yrotation4x4,
     Color, color,
     deg2rad,
 } from 'lib';
-import { observable, computed } from 'util/observable';
+import { observable, computed, Observable } from 'util/observable';
 import { createControls } from 'util/controls';
 import { makeObject, makeTexturePlane } from './primitive';
 
@@ -20,72 +21,121 @@ import { makeObject, makeTexturePlane } from './primitive';
  */
 export type DESCRIPTION = never;
 
-const container = document.querySelector<HTMLElement>(PLAYGROUND_ROOT)!;
-const runtime = new Runtime(container);
-runtime.setDepthTest(true);
-
-const textureBackgroundColor = color(0.7, 0.7, 0.7);
-const backgroundColor = color(0.4, 0.4, 0.4);
-const lightDir = norm3(vec3(1, 3, 2));
-
-const object = makeObject(runtime);
-const texturePlane = makeTexturePlane(runtime);
+main();
 
 interface ObjectInfo {
     readonly clr: Color;
     readonly model: Mat4;
 }
-const objects: ReadonlyArray<ObjectInfo> = [
-    {
-        clr: color(0.8, 0.2, 0.1),
-        model: translation4x4(vec3(+1.4, 0, 0)),
-    },
-    {
-        clr: color(0.3, 0.5, 0.9),
-        model: translation4x4(vec3(0, 0, +1.2)),
-    },
-    {
-        clr: color(0.8, 0.1, 0.4),
-        model: translation4x4(vec3(-1.5, 0, 0)),
-    },
-    {
-        clr: color(0.3, 0.9, 0.2),
-        model: translation4x4(vec3(0, 0, -1.6)),
-    },
-];
 
-const animationFlag = observable(true);
-const xRotation = observable(0);
-const yRotation = observable(30);
+interface State {
+    readonly runtime: Runtime;
+    readonly textureBackgroundColor: Color;
+    readonly backgroundColor: Color;
+    readonly lightDir: Vec3;
+    readonly camera: Camera;
+    readonly textureCamera: Camera;
+    readonly framebuffer: Framebuffer;
+    readonly texturePlane: Primitive;
+    readonly object: Primitive;
+    readonly objects: ReadonlyArray<ObjectInfo>;
+    readonly targetModel: Observable<Mat4>;
+}
 
-let textureCameraPos = vec3(0, 2, 5);
-const CAMERA_ROTATION_SPEED = (2 * Math.PI) * 0.1;
-const textureCamera = new Camera();
-textureCamera.setEyePos(textureCameraPos);
+function main(): void {
+    const container = document.querySelector<HTMLElement>(PLAYGROUND_ROOT)!;
+    const runtime = new Runtime(container);
+    runtime.setDepthTest(true);
 
-const camera = new Camera();
-camera.setEyePos(vec3(0, 0, 2));
+    const animationFlag = observable(true);
+    const xRotation = observable(0);
+    const yRotation = observable(30);
 
-const _targetModel = mat4();
-const targetModel = computed(([xRotation, yRotation]) => {
-    const mat = _targetModel;
-    identity4x4(mat);
-    apply4x4(mat, xrotation4x4, deg2rad(xRotation));
-    apply4x4(mat, yrotation4x4, deg2rad(yRotation));
-    return mat;
-}, [xRotation, yRotation]);
+    let textureCameraPos = vec3(0, 2, 5);
+    const CAMERA_ROTATION_SPEED = (2 * Math.PI) * 0.1;
+    const textureCamera = new Camera();
+    textureCamera.setEyePos(textureCameraPos);
 
-const framebuffer = new Framebuffer(runtime, {
-    attachment: 'color|depth',
-    size: { x: 256, y: 256 },
-});
-textureCamera.setViewportSize(framebuffer.size());
+    const camera = new Camera();
+    camera.setEyePos(vec3(0, 0, 2));
 
-runtime.sizeChanged().on(() => {
-    camera.setViewportSize(runtime.canvasSize());
-});
+    const _targetModel = mat4();
+    const targetModel = computed(([xRotation, yRotation]) => {
+        const mat = _targetModel;
+        identity4x4(mat);
+        apply4x4(mat, xrotation4x4, deg2rad(xRotation));
+        apply4x4(mat, yrotation4x4, deg2rad(yRotation));
+        return mat;
+    }, [xRotation, yRotation]);
 
-function renderToTexture(): void {
+    const framebuffer = new Framebuffer(runtime, {
+        attachment: 'color|depth',
+        size: { x: 256, y: 256 },
+    });
+    textureCamera.setViewportSize(framebuffer.size());
+
+    const state = {
+        runtime,
+        backgroundColor: color(0.4, 0.4, 0.4),
+        textureBackgroundColor: color(0.7, 0.7, 0.7),
+        lightDir: norm3(vec3(1, 3, 2)),
+        camera,
+        textureCamera,
+        framebuffer,
+        texturePlane: makeTexturePlane(runtime),
+        object: makeObject(runtime),
+        objects: [
+            {
+                clr: color(0.8, 0.2, 0.1),
+                model: translation4x4(vec3(+1.4, 0, 0)),
+            },
+            {
+                clr: color(0.3, 0.5, 0.9),
+                model: translation4x4(vec3(0, 0, +1.2)),
+            },
+            {
+                clr: color(0.8, 0.1, 0.4),
+                model: translation4x4(vec3(-1.5, 0, 0)),
+            },
+            {
+                clr: color(0.3, 0.9, 0.2),
+                model: translation4x4(vec3(0, 0, -1.6)),
+            },
+        ],
+        targetModel,
+    };
+
+    runtime.sizeChanged().on(() => {
+        camera.setViewportSize(runtime.canvasSize());
+    });
+
+    [animationFlag, targetModel]
+        .forEach((item) => item.on(() => runtime.requestFrameRender()));
+
+    runtime.frameRendered().on((delta) => {
+        if (animationFlag()) {
+            textureCameraPos = rotate3(textureCameraPos, YUNIT3, CAMERA_ROTATION_SPEED * delta / 1000);
+            textureCamera.setEyePos(textureCameraPos);
+        }
+
+        renderToTexture(state);
+        renderScene(state);
+
+        if (animationFlag()) {
+            runtime.requestFrameRender();
+        }
+    });
+
+    createControls(container, [
+        { label: 'animation', checked: animationFlag },
+        { label: 'x rotation', value: xRotation, min: -45, max: +45 },
+        { label: 'y rotation', value: yRotation, min: -45, max: +45 },
+    ]);
+}
+
+function renderToTexture({
+    runtime, framebuffer, textureBackgroundColor, object, objects, textureCamera, lightDir,
+}: State): void {
     runtime.setRenderTarget(framebuffer);
 
     runtime.setClearColor(textureBackgroundColor);
@@ -101,7 +151,9 @@ function renderToTexture(): void {
     }
 }
 
-function renderScene(): void {
+function renderScene({
+    runtime, backgroundColor, framebuffer, texturePlane, camera, targetModel,
+}: State): void {
     runtime.setRenderTarget(null);
 
     runtime.setClearColor(backgroundColor);
@@ -114,26 +166,3 @@ function renderScene(): void {
     program.setUniform('u_texture', 2);
     texturePlane.render();
 }
-
-[animationFlag, targetModel]
-    .forEach((item) => item.on(() => runtime.requestFrameRender()));
-
-runtime.frameRendered().on((delta) => {
-    if (animationFlag()) {
-        textureCameraPos = rotate3(textureCameraPos, YUNIT3, CAMERA_ROTATION_SPEED * delta / 1000);
-        textureCamera.setEyePos(textureCameraPos);
-    }
-
-    renderToTexture();
-    renderScene();
-
-    if (animationFlag()) {
-        runtime.requestFrameRender();
-    }
-});
-
-createControls(container, [
-    { label: 'animation', checked: animationFlag },
-    { label: 'x rotation', value: xRotation, min: -45, max: +45 },
-    { label: 'y rotation', value: yRotation, min: -45, max: +45 },
-]);
