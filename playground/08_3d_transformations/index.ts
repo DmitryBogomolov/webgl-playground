4cbcc5780de485a81b692dc394d1c3e6e2e68790
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2022-10-16 15:12:42 +0300

    Add main function

--- playground/08_3d_transformations/index.ts
+++ playground/08_3d_transformations/index.ts
@@ -14,57 +14,61 @@ import { makeFigureRenderer } from './figure';
  */
 export type DESCRIPTION = never;
 
-const CAMERA_HEIGHT = 3;
-const CAMERA_DISTANCE = 13;
-const PI2 = Math.PI * 2;
+main();
 
-const container = document.querySelector<HTMLElement>(PLAYGROUND_ROOT)!;
-const runtime = new Runtime(container);
-runtime.setClearColor(color(0.4, 0.4, 0.4));
-runtime.setDepthTest(true);
-const figure1 = makeFigureRenderer(
-    makePrimitive(runtime),
-    2, vec3(1, 8, 0), 3, +0.11 * PI2 / 1000,
-);
-const figure2 = makeFigureRenderer(
-    makePrimitive(runtime),
-    0.9, vec3(1, 0, 0), 4, -0.15 * PI2 / 1000,
-);
-const figure3 = makeFigureRenderer(
-    makePrimitive(runtime),
-    0.7, vec3(0, 0, 1), 5, +0.19 * PI2 / 1000,
-);
+function main(): void {
+    const CAMERA_HEIGHT = 3;
+    const CAMERA_DISTANCE = 13;
+    const PI2 = Math.PI * 2;
 
-const proj = mat4();
-const view = lookAt4x4({
-    eye: vec3(0, CAMERA_HEIGHT, CAMERA_DISTANCE),
-    center: ZERO3,
-    up: YUNIT3,
-});
-const viewProj = mat4();
-const unit = identity4x4();
+    const container = document.querySelector<HTMLElement>(PLAYGROUND_ROOT)!;
+    const runtime = new Runtime(container);
+    runtime.setClearColor(color(0.4, 0.4, 0.4));
+    runtime.setDepthTest(true);
+    const figure1 = makeFigureRenderer(
+        makePrimitive(runtime),
+        2, vec3(1, 8, 0), 3, +0.11 * PI2 / 1000,
+    );
+    const figure2 = makeFigureRenderer(
+        makePrimitive(runtime),
+        0.9, vec3(1, 0, 0), 4, -0.15 * PI2 / 1000,
+    );
+    const figure3 = makeFigureRenderer(
+        makePrimitive(runtime),
+        0.7, vec3(0, 0, 1), 5, +0.19 * PI2 / 1000,
+    );
 
-runtime.frameRendered().on((delta) => {
-    identity4x4(viewProj);
-    mul4x4(view, viewProj, viewProj);
-    mul4x4(proj, viewProj, viewProj);
-    figure1.update(viewProj, unit, delta);
-    figure2.update(viewProj, figure1.model(), delta);
-    figure3.update(viewProj, figure1.model(), delta);
+    const proj = mat4();
+    const view = lookAt4x4({
+        eye: vec3(0, CAMERA_HEIGHT, CAMERA_DISTANCE),
+        center: ZERO3,
+        up: YUNIT3,
+    });
+    const viewProj = mat4();
+    const unit = identity4x4();
 
-    runtime.clearBuffer('color|depth');
-    figure1.render();
-    figure2.render();
-    figure3.render();
-    runtime.requestFrameRender();
-});
+    runtime.frameRendered().on((delta) => {
+        identity4x4(viewProj);
+        mul4x4(view, viewProj, viewProj);
+        mul4x4(proj, viewProj, viewProj);
+        figure1.update(viewProj, unit, delta);
+        figure2.update(viewProj, figure1.model(), delta);
+        figure3.update(viewProj, figure1.model(), delta);
 
-runtime.sizeChanged().on(() => {
-    const { x, y } = runtime.canvasSize();
-    perspective4x4({
-        aspect: x / y,
-        yFov: Math.PI / 3,
-        zNear: 0.001,
-        zFar: 100,
-    }, proj);
-});
+        runtime.clearBuffer('color|depth');
+        figure1.render();
+        figure2.render();
+        figure3.render();
+        runtime.requestFrameRender();
+    });
+
+    runtime.sizeChanged().on(() => {
+        const { x, y } = runtime.canvasSize();
+        perspective4x4({
+            aspect: x / y,
+            yFov: Math.PI / 3,
+            zNear: 0.001,
+            zFar: 100,
+        }, proj);
+    });
+}
