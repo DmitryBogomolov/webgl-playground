7f165d06e3970018cb7b80cecea8557ce65b232b
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2022-10-20 01:15:15 +0300

    Prepare stub

---
+++ playground/20_glyph_texts/label.ts
@@ -0,0 +1,64 @@
+import {
+    Runtime,
+    Primitive,
+    Program,
+    Texture,
+    parseVertexSchema,
+} from 'lib';
+import vertShader from './shaders/label.vert';
+import fragShader from './shaders/label.frag';
+
+export function makeLabelPrimitive(runtime: Runtime): Primitive {
+    const vertexData = new Float32Array([
+        -1, -1,
+        +1, -1,
+        +1, +1,
+        -1, +1,
+    ]);
+    const indexData = new Uint16Array([
+        0, 1, 2,
+        2, 3, 0,
+    ]);
+    const schema = parseVertexSchema([
+        { name: 'a_position', type: 'float2' },
+    ]);
+
+    const primitive = new Primitive(runtime);
+    primitive.allocateVertexBuffer(vertexData.byteLength);
+    primitive.updateVertexData(vertexData);
+    primitive.allocateIndexBuffer(indexData.byteLength);
+    primitive.updateIndexData(indexData);
+    primitive.setVertexSchema(schema);
+    primitive.setIndexData({ indexCount: indexData.length });
+
+    const program = new Program(runtime, {
+        vertShader,
+        fragShader,
+        schema,
+    });
+    primitive.setProgram(program);
+
+    return primitive;
+}
+
+export function makeLabelTexture(runtime: Runtime, text: string, fontSize: number): Texture {
+    const texture = new Texture(runtime);
+
+    const canvas = document.createElement('canvas');
+    const ctx = canvas.getContext('2d')!;
+    const font = `${fontSize}px Verdana`;
+    ctx.font = font;
+    const width = Math.ceil(ctx.measureText(text).width) + 2;
+    canvas.width = width;
+    canvas.height = fontSize;
+    ctx.textAlign = 'center';
+    ctx.textBaseline = 'middle';
+    ctx.fillStyle = '#FFF';
+    ctx.font = font;
+    ctx.fillText(text, width / 2, fontSize / 2);
+
+    texture.setImageData(canvas, { unpackFlipY: true, unpackPremultiplyAlpha: true });
+    // runtime.canvas().parentElement!.parentElement!.appendChild(canvas);
+
+    return texture;
+}
---
+++ playground/20_glyph_texts/shaders/label.vert
@@ -0,0 +1,25 @@
+attribute vec2 a_position;
+
+uniform mat4 u_view_proj;
+uniform vec3 u_position;
+uniform vec2 u_size_coeff;
+uniform float u_base_distance;
+uniform vec3 u_view_position;
+
+varying vec2 v_texcoord;
+
+void main() {
+    vec4 position = u_view_proj * vec4(u_position, 1.0);
+    // Adjust quad size so that it match texture pixel size.
+    // u_size_coeff is texture to canvas size ratio.
+    // [-1,+1] -> canvas_size ~~ [-t,+t] -> texture_size => t = texture_size / canvas_size
+    // depth_coeff is base distance (like camera view distance) to point distance ratio.
+    // If object has visual size "h1" on distance "d1" then it will have visual size "h2" on distance "d2".
+    // (h1, d2) and (h2, d1) make similar triangles. h1 / d2 == h2 / d1 => h2 = h1 * d1 / d2.
+    // That gives perspective size diminishing for quad.
+    // Multiply by "w" to compensate further "w" division.
+    float depth_coeff = u_base_distance / distance(u_position, u_view_position);
+    position.xy += a_position * u_size_coeff * depth_coeff * position.w;
+    gl_Position = position;
+    v_texcoord = (a_position + vec2(1.0)) / 2.0;
+}
