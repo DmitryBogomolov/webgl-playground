30e93990355378b8229c1da2f8887ad73b9a6ab8
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2022-10-28 23:31:13 +0300

    Update plane equality

--- lib/geometry/plane3.test.ts
+++ lib/geometry/plane3.test.ts
@@ -22,6 +22,10 @@ describe('plane3', () => {
             { normal: { x: 1, y: 2, z: 3 }, distance: 1 },
             { normal: { x: 1, y: 2, z: 3 }, distance: 1 },
         )).toEqual(true);
+        expect(plane3eq(
+            { normal: { x: 1, y: 2, z: 3 }, distance: 1 },
+            { normal: { x: 2, y: 4, z: 6 }, distance: 1 },
+        )).toEqual(true);
     });
 
 });
