f56c01c9169d11a20a5e231589cfae864d129937
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2022-10-29 21:39:28 +0300

    Make plane equality simple

--- lib/geometry/plane3.test.ts
+++ lib/geometry/plane3.test.ts
@@ -22,10 +22,6 @@ describe('plane3', () => {
             { normal: { x: 1, y: 2, z: 3 }, distance: 1 },
             { normal: { x: 1, y: 2, z: 3 }, distance: 1 },
         )).toEqual(true);
-        expect(plane3eq(
-            { normal: { x: 1, y: 2, z: 3 }, distance: 1 },
-            { normal: { x: 2, y: 4, z: 6 }, distance: 1 },
-        )).toEqual(true);
     });
 
 });
--- lib/geometry/plane3.ts
+++ lib/geometry/plane3.ts
@@ -1,6 +1,6 @@
 import type { Plane3 } from './types/plane3';
 import type { Vec3 } from './types/vec3';
-import { XUNIT3, YUNIT3, ZUNIT3, vec3, isVec3, collinear3, norm3 } from './vec3';
+import { XUNIT3, YUNIT3, ZUNIT3, vec3, isVec3, eq3, norm3 } from './vec3';
 import { floatEq, FLOAT_EQ_EPS } from './float-eq';
 
 export class Plane3Impl implements Plane3 {
@@ -31,5 +31,5 @@ export function isPlane3(p: unknown): p is Plane3 {
 }
 
 export function plane3eq(a: Plane3, b: Plane3, eps: number = FLOAT_EQ_EPS): boolean {
-    return a === b || (floatEq(a.distance, b.distance, eps) && collinear3(a.normal, b.normal, eps));
+    return a === b || (floatEq(a.distance, b.distance, eps) && eq3(a.normal, b.normal, eps));
 }
