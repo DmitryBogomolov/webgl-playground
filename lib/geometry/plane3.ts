6210ce99e77156089465b668343382c011860470
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2022-10-30 21:16:36 +0300

    Add stubs

--- lib/geometry/plane3.ts
+++ lib/geometry/plane3.ts
@@ -1,6 +1,8 @@
 import type { Plane3 } from './types/plane3';
+import type { Line3 } from './types/line3';
 import type { Vec3 } from './types/vec3';
-import { XUNIT3, YUNIT3, ZUNIT3, isVec3, eq3, norm3, neg3 } from './vec3';
+import { XUNIT3, YUNIT3, ZUNIT3, isVec3, isZero3, eq3, norm3, neg3, cross3 } from './vec3';
+import { line3 } from './line3';
 import { floatEq, FLOAT_EQ_EPS } from './float-eq';
 
 export class Plane3Impl implements Plane3 {
@@ -32,4 +34,18 @@ export function isPlane3(p: unknown): p is Plane3 {
 
 export function plane3eq(a: Plane3, b: Plane3, eps: number = FLOAT_EQ_EPS): boolean {
     return a === b || (floatEq(a.distance, b.distance, eps) && eq3(a.normal, b.normal, eps));
+}
+
+export function plane3plane3intersect(a: Plane3, b: Plane3): Line3 | number {
+    const direction = cross3(a.normal, b.normal);
+    if (isZero3(direction)) {
+        return Math.abs(a.distance - b.distance);
+    }
+    // TODO...
+    return line3(direction, direction);
+}
+
+export function plane3line3intersect(p: Plane3, l: Line3): Vec3 | number {
+    // TODO...
+    return 0;
 }
