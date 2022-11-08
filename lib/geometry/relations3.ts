110c54d2ac3aa6b924bd1836045b7ee0ed6ac351
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2022-11-08 13:09:40 +0300

    Add line-plane intersection

--- lib/geometry/relations3.ts
+++ lib/geometry/relations3.ts
@@ -1,7 +1,7 @@
 import type { Vec3 } from './types/vec3';
 import type { Line3 } from'./types/line3';
 import type { Plane3 } from'./types/plane3';
-import { add3, sub3, mul3, norm3, project3, dist3 } from './vec3';
+import { add3, sub3, mul3, norm3, project3, dist3, dot3 } from './vec3';
 
 // Take direction from line point to targer point, project it onto line direction.
 // Apply that offset to line point.
@@ -29,9 +29,22 @@ export function point3plane3distance(point: Vec3, plane: Plane3): number {
     return dist3(projection, point);
 }
 
-export function line3plane3intersection(line: Line3, plane: Plane3): Vec3 | null {
-    // TODO...
-    return null;
+// Line is defined as v = anchor + direction * t. Plane is defined as (normal, v) = distance.
+// Substitution of v gives (anchor, normal) + (direction, normal) * t = distance
+// and then t = (distance - (anchor, normal)) / (direction, normal).
+// Returns intersection point or (if line is parallel to plane) distance between line and plane.
+export function line3plane3intersection(line: Line3, plane: Plane3): Vec3 | number {
+    const normal = norm3(plane.normal);
+    const num = plane.distance - dot3(line.anchor, normal);
+    const den = dot3(line.direction, normal);
+    const t = num / den;
+    if (Number.isFinite(t)) {
+        return add3(mul3(line.direction, t), line.anchor);
+    }
+    if (num === 0) {
+        return 0;
+    }
+    return point3plane3distance(line.anchor, plane);
 }
 
 export function line3line3intersection(line1: Line3, line2: Line3): Vec3 | null {
