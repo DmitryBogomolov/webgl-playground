b57770063005f50bec3590d7296c800aecd87d73
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2022-12-27 10:40:48 +0300

    Add query flag check

--- templates/screenshot-button.ts
+++ templates/screenshot-button.ts
@@ -1,14 +1,23 @@
 import { Runtime, takeCanvasSnapshot, downloadBlob } from 'lib';
 
-// @ts-ignore Take method closest to constructor.
-// eslint-disable-next-line @typescript-eslint/unbound-method
-const { _getContext } = Runtime.prototype;
-// @ts-ignore Hook into constructor.
-Runtime.prototype._getContext = function (...args) {
-    setTimeout(() => createButton(this));
-    // @ts-ignore Call original method.
-    return _getContext.apply(this, args);
-};
+const url = new URL(window.location.href);
+const flag = url.searchParams.get('screenshot');
+
+if (flag !== '0' && flag !== 'no') {
+    patch();
+}
+
+function patch(): void {
+    // @ts-ignore Take method closest to constructor.
+    // eslint-disable-next-line @typescript-eslint/unbound-method
+    const { _getContext } = Runtime.prototype;
+    // @ts-ignore Hook into constructor.
+    Runtime.prototype._getContext = function (...args) {
+        setTimeout(() => createButton(this));
+        // @ts-ignore Call original method.
+        return _getContext.apply(this, args);
+    };
+}
 
 function createButton(runtime: Runtime): void {
     const button = document.createElement('button');
