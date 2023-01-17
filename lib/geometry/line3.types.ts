1a8c27701d07896a35bba781858fade75183c5c0
Dmitry Bogomolov <Dmitry.Bogomolov.mail@gmail.com>
2023-01-17 10:28:56 +0300

    Remove not used (#137)
    * Remove line2
    
    * Remove line3
    
    * Remove plane3
    
    * Remove relations2
    
    * Remove relations3
    
    * Remove sys-lin-eq
    
    * Remove dirnorm
    
    * Cleanup index

--- lib/geometry/line3.types.ts
+++
@@ -1,6 +0,0 @@
-import type { Vec3 } from './vec3.types';
-
-export interface Line3 {
-    readonly direction: Vec3;
-    readonly anchor: Vec3;
-}
--- lib/geometry/plane3.types.ts
+++
@@ -1,6 +0,0 @@
-import type { Vec3 } from './vec3.types';
-
-export interface Plane3 {
-    readonly normal: Vec3;
-    readonly distance: number;
-}
