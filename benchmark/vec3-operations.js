e81215483c258fc9ced604054e446e5f3a5479aa
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2023-05-09 14:17:11 +0300

    Update naming

--- benchmark/vec3-operations.js
+++ benchmark/vec3-operations.js
@@ -9,7 +9,7 @@ function measure(callback) {
     return end - start;
 }
 
-class Vec3Cls {
+class Vec3 {
     constructor(x, y, z) {
         this.x = x;
         this.y = y;
@@ -18,14 +18,14 @@ class Vec3Cls {
 }
 
 function v3(x, y, z) {
-    return new Vec3Cls(x, y, z);
+    return new Vec3(x, y, z);
 }
 
-function f1(v, k) {
+function f1_ret(v, k) {
     return v3(v.x * k, v.y * k, v.z * k);
 }
 
-function f1x(v, k, out) {
+function f1_out(v, k, out) {
     const x = v.x * k;
     const y = v.y * k;
     const z = v.z * k;
@@ -34,7 +34,7 @@ function f1x(v, k, out) {
     out.z = z;
 }
 
-function f1z(v, k, out = v3(0, 0, 0)) {
+function f1_both(v, k, out = v3(0, 0, 0)) {
     const x = v.x * k;
     const y = v.y * k;
     const z = v.z * k;
@@ -44,11 +44,11 @@ function f1z(v, k, out = v3(0, 0, 0)) {
     return out;
 }
 
-function f2(a, b) {
+function f2_ret(a, b) {
     return v3(a.x + b.x, a.y + b.y, a.z + b.z);
 }
 
-function f2x(a, b, out) {
+function f2_out(a, b, out) {
     const x = a.x + b.x;
     const y = a.y + b.y;
     const z = a.z + b.z;
@@ -57,7 +57,7 @@ function f2x(a, b, out) {
     out.z = z;
 }
 
-function f2z(a, b, out = v3(0, 0, 0)) {
+function f2_both(a, b, out = v3(0, 0, 0)) {
     const x = a.x + b.x;
     const y = a.y + b.y;
     const z = a.z + b.z;
@@ -67,23 +67,23 @@ function f2z(a, b, out = v3(0, 0, 0)) {
     return out;
 }
 
-function test1(a, b, storage, f1, f2) {
-    for (let i = 0; i < storage.length; ++i) {
-        const k = i / (storage.length - 1);
+function test_ret(a, b, arr, f1, f2) {
+    for (let i = 0; i < arr.length; ++i) {
+        const k = i / (arr.length - 1);
         const x1 = f2(f1(a, 1 - k), f1(b, k));
         const x2 = f2(f1(a, k / 2), f1(b, k / 2));
         const x3 = f2(f1(a, 1 - k * k), f1(b, k * k));
         const x4 = f2(f1(a, 0.2 + k), f1(b, 0.2 + k));
-        storage[i] = f2(f2(x1, x2), f2(x3, x4));
+        arr[i] = f2(f2(x1, x2), f2(x3, x4));
     }
-    let result = storage[0];
-    for (let i = 1; i < storage.length; ++i) {
-        result = f2(result, f1(storage[i], i / (storage.length - 1)));
+    let result = arr[0];
+    for (let i = 1; i < arr.length; ++i) {
+        result = f2(result, f1(arr[i], i / (arr.length - 1)));
     }
     return result;
 }
 
-function test2(a, b, storage, f1x, f2x) {
+function test_out(a, b, arr, f1, f2) {
     const scratch1 = v3(0, 0, 0);
     const scratch2 = v3(0, 0, 0);
 
@@ -92,35 +92,35 @@ function test2(a, b, storage, f1x, f2x) {
     const x3 = v3(0, 0, 0);
     const x4 = v3(0, 0, 0);
 
-    for (let i = 0; i < storage.length; ++i) {
-        const k = i / (storage.length - 1);
+    for (let i = 0; i < arr.length; ++i) {
+        const k = i / (arr.length - 1);
 
-        f1x(a, 1 - k, scratch1);
-        f1x(b, k, scratch2);
-        f2x(scratch1, scratch2, x1);
+        f1(a, 1 - k, scratch1);
+        f1(b, k, scratch2);
+        f2(scratch1, scratch2, x1);
 
-        f1x(a, k / 2, scratch1);
-        f1x(b, k / 2, scratch2);
-        f2x(scratch1, scratch2, x2);
+        f1(a, k / 2, scratch1);
+        f1(b, k / 2, scratch2);
+        f2(scratch1, scratch2, x2);
 
-        f1x(a, 1 - k * k, scratch1);
-        f1x(b, k * k, scratch2);
-        f2x(scratch1, scratch2, x3);
+        f1(a, 1 - k * k, scratch1);
+        f1(b, k * k, scratch2);
+        f2(scratch1, scratch2, x3);
 
-        f1x(a, 0.2 + k, scratch1);
-        f1x(b, 0.2 + k, scratch2);
-        f2x(scratch1, scratch2, x4);
+        f1(a, 0.2 + k, scratch1);
+        f1(b, 0.2 + k, scratch2);
+        f2(scratch1, scratch2, x4);
 
-        f2x(x1, x2, scratch1);
-        f2x(x3, x4, scratch2);
+        f2(x1, x2, scratch1);
+        f2(x3, x4, scratch2);
         const z = v3(0, 0, 0);
-        f2x(scratch1, scratch2, z);
-        storage[i] = z;
+        f2(scratch1, scratch2, z);
+        arr[i] = z;
     }
-    const result = v3(storage[0].x, storage[0].y, storage[0].z);
-    for (let i = 1; i < storage.length; ++i) {
-        f1x(storage[i], i / (storage.length - 1), scratch1);
-        f2x(result, scratch1, result);
+    const result = v3(arr[0].x, arr[0].y, arr[0].z);
+    for (let i = 1; i < arr.length; ++i) {
+        f1(arr[i], i / (arr.length - 1), scratch1);
+        f2(result, scratch1, result);
     }
     return result;
 }
@@ -129,8 +129,8 @@ const a = v3(1, 4, 8);
 const b = v3(3, 2, 9);
 const storage = new Array(512);
 
-console.log(measure(() => test1(a, b, storage, f1, f2)));
-console.log(measure(() => test2(a, b, storage, f1x, f2x)));
+console.log(measure(() => test_ret(a, b, storage, f1_ret, f2_ret)));
+console.log(measure(() => test_out(a, b, storage, f1_out, f2_out)));
 
-console.log(measure(() => test1(a, b, storage, f1z, f2z)));
-console.log(measure(() => test2(a, b, storage, f1z, f2z)));
+console.log(measure(() => test_ret(a, b, storage, f1_both, f2_both)));
+console.log(measure(() => test_out(a, b, storage, f1_both, f2_both)));
