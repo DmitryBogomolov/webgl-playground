87d566561e057773ea86184b165b15ce114558a9
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2023-05-11 11:16:18 +0300

    Isolate benchmarks in clusters

--- benchmark/util/index.js
+++ benchmark/util/index.js
@@ -1,9 +1,11 @@
+const cluster = require('node:cluster');
+
 function now() {
     return performance.now();
 }
 
-function measure(callback, samples) {
-    samples.fill(0);
+function measure(name, callback, sampleCount) {
+    const samples = Array(sampleCount).fill(0);
     const start = now();
     for (let i = 0; i < samples.length; ++i) {
         const sampleStart = now();
@@ -18,6 +20,7 @@ function measure(callback, samples) {
         sum += sample;
     });
     return {
+        name,
         total: end - start,
         min: samples[0],
         max: samples[samples.length - 1],
@@ -30,19 +33,57 @@ function fmt(value) {
     return value .toPrecision(4);
 }
 
-function runBenchmarks(targets, sampleCount) {
-    const samples = new Array(sampleCount);
-    const reports = targets.map(({ name, action }) => {
-        const report = measure(action, samples);
-        return { name, report };
+function runWorker(index) {
+    return new Promise((resolve, reject) => {
+        const worker = cluster.fork();
+        worker.on('online', () => {
+            worker.send({ index });
+        });
+        worker.on('message', (payload) => {
+            resolve(payload);
+        });
+        worker.on('error', (err) => {
+            reject(err);
+        })
     });
-    reports.forEach(({ name, report }) => {
-        console.log(name, ' // ', fmt(report.total));
+}
+
+function runBenchmarksMaster(targets, sampleCount) {
+    const tasks = [];
+    for (let i = 0; i < targets.length; ++i) {
+        tasks.push(runWorker(i));
+    }
+    return Promise.all(tasks).then((reports) => {
+        printReports(reports);
+    });
+}
+
+function runBenchmarksWorker(targets, sampleCount) {
+    process.on('message', (payload) => {
+        const target = targets[payload.index];
+        const report = measure(target.name, target.action, sampleCount);
+        process.send(report);
+        process.exit(0);
+    });
+}
+
+function printReports(reports) {
+    reports.forEach((report) => {
+        console.log(report.name, ' // ', fmt(report.total));
         console.log('  avg: ', fmt(report.avg));
         console.log('  med: ', fmt(report.med));
         console.log('  min: ', fmt(report.min));
         console.log('  max: ', fmt(report.max));
     });
+}
+
+function runBenchmarks(targets, sampleCount) {
+    if (cluster.isMaster) {
+        return runBenchmarksMaster(targets, sampleCount);
+    } else {
+        runBenchmarksWorker(targets, sampleCount);
+        return Promise.resolve();
+    }
 }
 
 module.exports = { runBenchmarks };
