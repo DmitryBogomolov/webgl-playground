61416f6dc76a85df3f29843d810ba14ce7c08dcf
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2023-05-16 13:17:47 +0300

    Add Mat2Mut

--- lib/geometry/mat2.ts
+++ lib/geometry/mat2.ts
@@ -1,5 +1,5 @@
-import type { Mat2 } from './mat2.types';
-import type { Vec2 } from './vec2.types';
+import type { Mat2, Mat2Mut } from './mat2.types';
+import type { Vec2, Vec2Mut } from './vec2.types';
 import { upd2 } from './vec2.helper';
 import { vec2 } from './vec2';
 import { floatEq as eq, FLOAT_EQ_EPS } from './float-eq';
@@ -27,84 +27,88 @@ export function eq2x2(lhs: Mat2, rhs: Mat2, eps: number = FLOAT_EQ_EPS): boolean
     return true;
 }
 
-export function zero2x2(out: Mat2 = mat2()): Mat2 {
-    (out as number[]).fill(0);
+function m2(): Mat2Mut {
+    return mat2() as Mat2Mut;
+}
+
+export function zero2x2(out: Mat2Mut = m2()): Mat2 {
+    out.fill(0);
     return out;
 }
 
-export function identity2x2(out: Mat2 = mat2()): Mat2 {
-    (out as number[])[0] = 1;
-    (out as number[])[1] = 0;
-    (out as number[])[2] = 0;
-    (out as number[])[3] = 1;
+export function identity2x2(out: Mat2Mut = m2()): Mat2 {
+    out[0] = 1;
+    out[1] = 0;
+    out[2] = 0;
+    out[3] = 1;
     return out;
 }
 
-export function update2x2(values: ReadonlyArray<number>, out: Mat2 = mat2()): Mat2 {
-    (out as number[])[0] = values[0];
-    (out as number[])[1] = values[2];
-    (out as number[])[2] = values[1];
-    (out as number[])[3] = values[3];
+export function update2x2(values: ReadonlyArray<number>, out: Mat2Mut = m2()): Mat2 {
+    out[0] = values[0];
+    out[1] = values[2];
+    out[2] = values[1];
+    out[3] = values[3];
     return out;
 }
 
-export function clone2x2(mat: Mat2, out: Mat2 = mat2()): Mat2 {
+export function clone2x2(mat: Mat2, out: Mat2Mut = m2()): Mat2 {
     if (mat === out) {
         return out;
     }
     for (let i = 0; i < MAT_SIZE; ++i) {
-        (out as number[])[i] = mat[i];
+        out[i] = mat[i];
     }
     return out;
 }
 
-export function transpose2x2(mat: Mat2, out: Mat2 = mat2()): Mat2 {
+export function transpose2x2(mat: Mat2, out: Mat2Mut = m2()): Mat2 {
     const [
         m11, m21,
         m12, m22,
-    ] = mat as number[];
-    (out as number[])[0] = m11;
-    (out as number[])[1] = m12;
-    (out as number[])[2] = m21;
-    (out as number[])[3] = m22;
+    ] = mat;
+    out[0] = m11;
+    out[1] = m12;
+    out[2] = m21;
+    out[3] = m22;
     return out;
 }
 
-export function add2x2(lhs: Mat2, rhs: Mat2, out: Mat2 = mat2()): Mat2 {
+export function add2x2(lhs: Mat2, rhs: Mat2, out: Mat2Mut = m2()): Mat2 {
     for (let i = 0; i < MAT_SIZE; ++i) {
-        (out as number[])[i] = lhs[i] + rhs[i];
+        out[i] = lhs[i] + rhs[i];
     }
     return out;
 }
 
-export function sub2x2(lhs: Mat2, rhs: Mat2, out: Mat2 = mat2()): Mat2 {
+export function sub2x2(lhs: Mat2, rhs: Mat2, out: Mat2Mut = m2()): Mat2 {
     for (let i = 0; i < MAT_SIZE; ++i) {
-        (out as number[])[i] = lhs[i] - rhs[i];
+        out[i] = lhs[i] - rhs[i];
     }
     return out;
 }
 
-export function mul2x2(lhs: Mat2, rhs: Mat2, out: Mat2 = mat2()): Mat2 {
+export function mul2x2(lhs: Mat2, rhs: Mat2, out: Mat2Mut = m2()): Mat2 {
     const [
         a11, a21,
         a12, a22,
-    ] = lhs as number[];
+    ] = lhs;
     const [
         b11, b21,
         b12, b22,
-    ] = rhs as number[];
-    (out as number[])[0] = a11 * b11 + a12 * b21;
-    (out as number[])[1] = a21 * b11 + a22 * b21;
-    (out as number[])[2] = a11 * b12 + a12 * b22;
-    (out as number[])[3] = a21 * b12 + a22 * b22;
+    ] = rhs;
+    out[0] = a11 * b11 + a12 * b21;
+    out[1] = a21 * b11 + a22 * b21;
+    out[2] = a11 * b12 + a12 * b22;
+    out[3] = a21 * b12 + a22 * b22;
     return out;
 }
 
-export function mul2v2(lhs: Mat2, rhs: Vec2, out: Vec2 = vec2(0, 0)): Vec2 {
+export function mul2v2(lhs: Mat2, rhs: Vec2, out: Vec2Mut = vec2(0, 0) as Vec2Mut): Vec2 {
     const [
         a11, a21,
         a12, a22,
-    ] = lhs as number[];
+    ] = lhs;
     const { x, y } = rhs;
     return upd2(out,
         a11 * x + a12 * y,
@@ -116,26 +120,26 @@ export function det2x2(mat: Mat2): number {
     return mat[0] * mat[3] - mat[2] * mat[1];
 }
 
-export function adjugate2x2(mat: Mat2, out: Mat2 = mat2()): Mat2 {
+export function adjugate2x2(mat: Mat2, out: Mat2Mut = m2()): Mat2 {
     const [
         m11, m21,
         m12, m22,
-    ] = mat as number[];
-    (out as number[])[0] = m22;
-    (out as number[])[1] = -m21;
-    (out as number[])[2] = -m12;
-    (out as number[])[3] = m11;
+    ] = mat;
+    out[0] = m22;
+    out[1] = -m21;
+    out[2] = -m12;
+    out[3] = m11;
     return out;
 }
 
-export function inverse2x2(mat: Mat2, out: Mat2 = mat2()): Mat2 {
+export function inverse2x2(mat: Mat2, out: Mat2Mut = m2()): Mat2 {
     let k = 1 / det2x2(mat);
     if (!Number.isFinite(k)) {
         k = 0;
     }
     adjugate2x2(mat, out);
     for (let i = 0; i < MAT_SIZE; ++i) {
-        (out as number[])[i] = out[i] * k;
+        out[i] = out[i] * k;
     }
     return out;
 }
--- lib/geometry/mat2.types.ts
+++ lib/geometry/mat2.types.ts
@@ -1,3 +1,8 @@
-export interface Mat2 {
+export interface Mat2 extends ReadonlyArray<number> {
     readonly [i: number]: number;
+}
+
+export interface Mat2Mut extends Array<number> {
+    [i: number]: number;
+    readonly _NO_CAST: null;
 }
