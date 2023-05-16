50beb4c9c1356f4c2aeee14e267226b5fb80b164
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2023-05-16 15:47:29 +0300

    Fix lint

--- lib/geometry/spherical.ts
+++ lib/geometry/spherical.ts
@@ -4,7 +4,7 @@ import { upd3 } from './vec3.helper';
 import { vec3 } from './vec3';
 
 export function spherical2zxy(
-    { azimuth, elevation }: Spherical, out: Vec3Mut = vec3(0, 0, 0) as Vec3Mut
+    { azimuth, elevation }: Spherical, out: Vec3Mut = vec3(0, 0, 0) as Vec3Mut,
 ): Vec3 {
     return upd3(out,
         Math.cos(elevation) * Math.sin(azimuth),
@@ -14,7 +14,7 @@ export function spherical2zxy(
 }
 
 export function zxy2spherical(
-    { x, y, z }: Vec3, out: SphericalMut = { azimuth: 0, elevation: 0 } as SphericalMut
+    { x, y, z }: Vec3, out: SphericalMut = { azimuth: 0, elevation: 0 } as SphericalMut,
 ): Spherical {
     out.azimuth = Math.atan2(x, z);
     out.elevation = Math.asin(y);
