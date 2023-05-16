da255225abdbc2e17799f840811858682aa15ebb
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2023-05-16 13:07:19 +0300

    Add Vec3Mut

--- lib/geometry/vec3.helper.ts
+++ lib/geometry/vec3.helper.ts
@@ -1,9 +1,8 @@
-import type { Vec3 } from './vec3.types';
+import type { Vec3, Vec3Mut } from './vec3.types';
 
-export function upd3(out: Vec3, x: number, y: number, z: number): Vec3 {
-    type V3 = { x: number; y: number; z: number; };
-    (out as V3).x = x;
-    (out as V3).y = y;
-    (out as V3).z = z;
+export function upd3(out: Vec3Mut, x: number, y: number, z: number): Vec3 {
+    out.x = x;
+    out.y = y;
+    out.z = z;
     return out;
 }
