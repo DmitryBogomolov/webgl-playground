d06b3980741805d056bdb20a8318fe7747b0d27f
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2023-06-20 11:17:21 +0300

    Stub sources combining

--- webpack.shader-loader.test.ts
+++ webpack.shader-loader.test.ts
@@ -1,19 +1,217 @@
-import {findIncludes} from './webpack.shader-loader';
+import {findIncludes, traverseSource, buildCombinedSource, SourceInfo, IncludeInfo} from './webpack.shader-loader';
+import fs from 'fs/promises';
+
+jest.mock('fs/promises');
 
 describe('shader-loader', () => {
+    afterEach(() => {
+        (fs.readFile as jest.Mock).mockReset();
+    });
+
+    function setDebugSources(sources: Readonly<Record<string, string>>): void {
+        (fs.readFile as jest.Mock).mockImplementation((name: string) => {
+            const source = sources[name];
+            return source ? Promise.resolve(source) : Promise.reject(new Error(`not found: ${name}`));
+        });
+    }
+
     it('find includes', () => {
         expect(findIncludes('')).toEqual([]);
         expect(findIncludes('Hello')).toEqual([]);
-        expect(findIncludes('//#include test.txt')).toEqual([{ line: 0, path: 'test.txt' }]);
-        expect(findIncludes('//#include test.txt  ')).toEqual([{ line: 0, path: 'test.txt' }]);
-        expect(findIncludes('//#include test.txt  // comment ')).toEqual([{ line: 0, path: 'test.txt' }]);
+        expect(findIncludes('//#include test.txt')).toEqual([{ line: 0, path: 'test.txt', start: 0, end: 19 }]);
+        expect(findIncludes('//#include test.txt\n')).toEqual([{ line: 0, path: 'test.txt', start: 0, end: 20 }]);
+        expect(findIncludes('//#include test.txt  ')).toEqual([{ line: 0, path: 'test.txt', start: 0, end: 21 }]);
+        expect(findIncludes('//#include test.txt  \n')).toEqual([{ line: 0, path: 'test.txt', start: 0, end: 22 }]);
+        expect(findIncludes('//#include test.txt  // comment ')).toEqual([{ line: 0, path: 'test.txt', start: 0, end: 32 }]);
 
-        expect(findIncludes('Hello\n\n//#include test.txt\n')).toEqual([
-            { line: 2, path: 'test.txt' },
+        expect(findIncludes('Hello\n\n//#include test.txt\n')).toEqual<IncludeInfo[]>([
+            { line: 2, path: 'test.txt', start: 7, end: 27 },
         ]);
-        expect(findIncludes('Hello\n//#include test-1.txt\n//#include test-2.txt\nWorld')).toEqual([
-            { line: 1, path: 'test-1.txt' },
-            { line: 2, path: 'test-2.txt' },
+        expect(findIncludes('Hello\n//#include test-1.txt\n//#include test-2.txt\nWorld')).toEqual<IncludeInfo[]>([
+            { line: 1, path: 'test-1.txt', start: 6, end: 28 },
+            { line: 2, path: 'test-2.txt', start: 28, end: 50 },
         ]);
+    });
+
+    it('traverseSource', async () => {
+        {
+            const result = new Map<string, SourceInfo>();
+            await traverseSource('Hello', '/some/dir/file-1.txt', result);
+
+            expect(result).toEqual(new Map<string, SourceInfo>([
+                [
+                    '/some/dir/file-1.txt',
+                    { path: '/some/dir/file-1.txt', source: 'Hello\n', includes: [] },
+                ]
+            ]));
+        }
+        {
+            const result = new Map<string, SourceInfo>();
+            await traverseSource('Hello World\n', '/some/dir/file-1.txt', result);
+
+            expect(result).toEqual(new Map<string, SourceInfo>([
+                [
+                    '/some/dir/file-1.txt',
+                    { path: '/some/dir/file-1.txt', source: 'Hello World\n', includes: [] },
+                ]
+            ]));
+        }
+    });
+
+    it('traverseSource complex', async () => {
+        const path1 = '/some/dir/file-1.txt';
+        const path2 = '/some/dir/file-2.txt';
+        const path3 = '/some/dir/sub/file-3.txt';
+        const path4 = '/some/dir/sub/file-4.txt';
+        const source1 = 'File 1\n\n//#include ./file-2.txt\n\nHello World 1\n';
+        const source2 = 'File 2\n//#include ./sub/file-3.txt\n//#include ./sub/file-4.txt\nHello World 2\n';
+        const source3 = 'File 3\nHello World 3\n';
+        const source4 = 'File 4\nHello World 4\n';
+        setDebugSources({
+            [path2]: source2,
+            [path3]: source3,
+            [path4]: source4,
+        });
+
+        {
+            const result = new Map<string, SourceInfo>();
+            await traverseSource(source3, path3, result);
+            expect(result).toEqual(new Map<string, SourceInfo>([
+                [
+                    path3,
+                    { path: path3, source: source3, includes: [] },
+                ]
+            ]));
+        }
+        {
+            const result = new Map<string, SourceInfo>();
+            await traverseSource(source4, path4, result);
+            expect(result).toEqual(new Map<string, SourceInfo>([
+                [
+                    path4,
+                    { path: path4, source: source4, includes: [] },
+                ]
+            ]));
+        }
+        {
+            const result = new Map<string, SourceInfo>();
+            await traverseSource(source2, path2, result);
+            expect(result).toEqual(new Map<string, SourceInfo>([
+                [
+                    path2,
+                    {
+                        path: path2, source: source2,
+                        includes: [
+                            { line: 1, path: path3, start: 7, end: 35 },
+                            { line: 2, path: path4, start: 35, end: 63 },
+                        ]
+                    },
+                ],
+                [
+                    path3,
+                    { path: path3, source: source3, includes: [] },
+                ],
+                [
+                    path4,
+                    { path: path4, source: source4, includes: [] },
+                ]
+            ]));
+        }
+        {
+            const result = new Map<string, SourceInfo>();
+            await traverseSource(source1, path1, result);
+            expect(result).toEqual(new Map<string, SourceInfo>([
+                [
+                    path1,
+                    {
+                        path: path1, source: source1,
+                        includes: [
+                            { line: 2, path: path2, start: 8, end: 32 },
+                        ]
+                    }
+                ],
+                [
+                    path2,
+                    {
+                        path: path2, source: source2,
+                        includes: [
+                            { line: 1, path: path3, start: 7, end: 35 },
+                            { line: 2, path: path4, start: 35, end: 63 },
+                        ]
+                    },
+                ],
+                [
+                    path3,
+                    { path: path3, source: source3, includes: [] },
+                ],
+                [
+                    path4,
+                    { path: path4, source: source4, includes: [] },
+                ]
+            ]));
+        }
+    });
+
+    it('buildCombinedSource', () => {
+        const path1 = '/some/dir/file-1.txt';
+        const path2 = '/some/dir/file-2.txt';
+        const path3 = '/some/dir/sub/file-3.txt';
+        const path4 = '/some/dir/sub/file-4.txt';
+        const source1 = 'File 1\n\n//#include ./file-2.txt\n\nHello World 1\n';
+        const source2 = 'File 2\n//#include ./sub/file-3.txt\n//#include ./sub/file-4.txt\nHello World 2\n';
+        const source3 = 'File 3\nHello World 3\n';
+        const source4 = 'File 4\nHello World 4\n';
+
+        const sources = new Map<string, SourceInfo>([
+            [
+                path1,
+                {
+                    path: path1, source: source1,
+                    includes: [{ line: 2, path: path2, start: 8, end: 31 }]
+                }
+            ],
+            [
+                path2,
+                {
+                    path: path2, source: source2,
+                    includes: [
+                        { line: 1, path: path3, start: 7, end: 34 },
+                        { line: 2, path: path4, start: 35, end: 62 },
+                    ]
+                }
+            ],
+            [   path3,
+                { path: path3, source: source3, includes: [] }
+            ],
+            [
+                path4,
+                { path: path4, source: source4, includes: [] }
+            ],
+        ]);
+
+        expect(buildCombinedSource(path3, sources)).toEqual(source3);
+        expect(buildCombinedSource(path4, sources)).toEqual(source4);
+        expect(buildCombinedSource(path2, sources)).toEqual([
+            'File 2',
+            'File 3',
+            'Hello World 3',
+            'File 4',
+            'Hello World 4',
+            'Hello World 2',
+            '',
+        ].join('\n'));
+        expect(buildCombinedSource(path1, sources)).toEqual([
+            'File 1',
+            '',
+            'File 2',
+            'File 3',
+            'Hello World 3',
+            'File 4',
+            'Hello World 4',
+            'Hello World 2',
+            '',
+            'Hello World 1',
+            '',
+        ].join('\n'));
     });
 });
--- webpack.shader-loader.ts
+++ webpack.shader-loader.ts
@@ -1,9 +1,19 @@
 import type {LoaderDefinitionFunction} from 'webpack';
+import path from 'path';
+import fs from 'fs/promises';
+
+export interface SourceInfo {
+    readonly path: string;
+    readonly source: string;
+    readonly includes: ReadonlyArray<IncludeInfo>;
+}
 
 export default <LoaderDefinitionFunction> async function (source: string): Promise<string> {
-    console.log('@@@@@@@@@@@@@', this.context, this.currentRequest, this.loadModule);
-
-    return `export default ${JSON.stringify(source)}`;
+    const rootPath = this.resourcePath;
+    const sources = new Map<string, SourceInfo>();
+    await traverseSource(source, rootPath, sources);
+    const combinedSource = buildCombinedSource(rootPath, sources);
+    return `export default ${JSON.stringify(source || combinedSource)}`;
 }
 
 function findLineStarts(source: string): number[] {
@@ -37,21 +47,76 @@ function findLineIndex(idx: number, lineStarts: ReadonlyArray<number>): number {
 export interface IncludeInfo {
     readonly line: number;
     readonly path: string;
+    readonly start: number;
+    readonly end: number;
 }
+
+const INCLUDE_PREFIX = '//#include ';
 
 export function findIncludes(source: string): IncludeInfo[] {
     const lineStarts = findLineStarts(source);
     const result: IncludeInfo[] = [];
     let k = 0;
     while (k < source.length) {
-        const i = source.indexOf('//#include ', k);
+        const i = source.indexOf(INCLUDE_PREFIX, k);
         if (i < 0) {
             break;
         }
+        const startIdx = i;
         const lineIdx = findLineIndex(i, lineStarts);
-        k = lineStarts[lineIdx + 1] || source.length;
-        const part = source.substring(i + '//#include'.length, k);
-        result.push({ line: lineIdx, path: part.trim().split(' ')[0] });
+        const endIdx = lineIdx + 1 < lineStarts.length ? lineStarts[lineIdx + 1] : source.length;
+        const part = source.substring(i + INCLUDE_PREFIX.length, endIdx);
+        k = endIdx;
+        result.push({ line: lineIdx, path: part.trim().split(' ')[0], start: startIdx, end: endIdx });
     }
     return result;
+}
+
+function normalizeLastNewline(source: string): string {
+    return source[source.length - 1] === '\n' ? source : source + '\n';
+}
+
+export async function traverseSource(
+    source: string, filePath: string, sources: Map<string, SourceInfo>,
+): Promise<void> {
+    if (sources.has(filePath)) {
+        return;
+    }
+    const fileDir = path.dirname(filePath);
+    const includes = findIncludes(source).map((include) => {
+        const itemPath = path.resolve(fileDir, include.path);
+        return {
+            ...include,
+            path: itemPath,
+        };
+    });
+    sources.set(filePath, {
+        path: filePath,
+        source: normalizeLastNewline(source),
+        includes,
+    });
+    const itemTasks: Promise<void>[] = [];
+    for (const include of includes) {
+        const itemTask = fs.readFile(include.path, 'utf8').then((itemSource) => {
+            return traverseSource(itemSource, include.path, sources);
+        });
+        itemTasks.push(itemTask);
+    }
+    await Promise.all(itemTasks);
+}
+
+export function buildCombinedSource(filePath: string, sources: ReadonlyMap<string, SourceInfo>): string {
+    const { source, includes } = sources.get(filePath)!;
+    if (includes.length === 0) {
+        return source;
+    }
+    const parts: string[] = [];
+    let k = 0;
+    for (const include of includes) {
+        parts.push(source.substring(k, include.start));
+        parts.push(buildCombinedSource(include.path, sources));
+        k = include.end + 1;
+    }
+    parts.push(source.substring(k));
+    return normalizeLastNewline(parts.join(''));
 }
