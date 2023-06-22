f6fc5535e97c70d48c4697de4296ff72aa1bcdad
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2023-06-22 14:57:55 +0300

    Move jest files to 'tools' folder

--- jest.config.js
+++ jest.config.js
@@ -169,7 +169,7 @@ module.exports = {
 
     // A list of paths to modules that run some code to configure or set up the testing framework before each test
     setupFilesAfterEnv: [
-        './jest-setup.ts',
+        '<rootDir>/tools/jest-setup.ts',
     ],
 
     // A list of paths to snapshot serializer modules Jest should use for snapshot testing
@@ -213,7 +213,7 @@ module.exports = {
 
     // A map from regular expressions to paths to transformers
     transform: {
-        '\.vert$|\.frag$|\.glsl$': '<rootDir>/jest-transform.js',
+        '\.vert$|\.frag$|\.glsl$': '<rootDir>/tools/jest-transform.js',
     },
 
     moduleNameMapper: {
