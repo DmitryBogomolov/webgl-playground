1ca63d318aaf157e9671d341f4d85f9c433afa43
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2023-06-27 00:15:58 +0300

    Update test

--- lib/wrappers/worker-channel/background-channel.test.ts
+++ lib/wrappers/worker-channel/background-channel.test.ts
@@ -4,26 +4,31 @@ describe('background-channel', () => {
     describe('BackgroundChannel', () => {
         function noop(): void { /* empty */ }
 
-        afterEach(() => {
-            jest.resetAllMocks();
-        });
-
         it('pass "self" as carrier', () => {
-            const addEventListener = jest.spyOn(self, 'addEventListener').mockImplementation(noop);
-            const removeEventListener = jest.spyOn(self, 'removeEventListener').mockImplementation(noop);
-            const postMessage = jest.spyOn(self, 'postMessage').mockImplementation(noop);
+            let addEventListener: jest.SpyInstance;
+            let removeEventListener: jest.SpyInstance;
+            let postMessage: jest.SpyInstance;
+            try {
+                addEventListener = jest.spyOn(self, 'addEventListener').mockImplementation(noop);
+                removeEventListener = jest.spyOn(self, 'removeEventListener').mockImplementation(noop);
+                postMessage = jest.spyOn(self, 'postMessage').mockImplementation(noop);
 
-            const channel = new BackgroundChannel({
-                connectionId: 1,
-                handler: noop,
-            });
-            channel.send({ text: 'Hello World' });
-            channel.flush();
-            channel.dispose();
+                const channel = new BackgroundChannel({
+                    connectionId: 1,
+                    handler: noop,
+                });
+                channel.send({ text: 'Hello World' });
+                channel.flush();
+                channel.dispose();
 
-            expect(addEventListener).toBeCalled();
-            expect(removeEventListener).toBeCalled();
-            expect(postMessage).toBeCalled();
+                expect(addEventListener).toBeCalled();
+                expect(removeEventListener).toBeCalled();
+                expect(postMessage).toBeCalled();
+            } finally {
+                addEventListener!.mockReset();
+                removeEventListener!.mockReset();
+                postMessage!.mockReset();
+            }
         });
     });
 });
