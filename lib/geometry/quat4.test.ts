1aa3851b46a1554eaafc9cd0f3e56de21570dec7
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2023-06-29 01:19:24 +0300

    Add tests

--- lib/geometry/quat4.test.ts
+++ lib/geometry/quat4.test.ts
@@ -1,27 +1,83 @@
 import {
     QUAT4_UNIT,
-    quat4fromAxisAngle, quat4Angle, quat4Axis, quat4mul,
+    quat4apply, quat4mul, quat4slerp, quat4fromAxisAngle, quat4Angle, quat4Axis,
 } from './quat4';
-import { eq4 } from './vec4';
 
 describe('quat4', () => {
+    const c_PI4 = Math.cos(Math.PI / 4);
+    const s_PI4 = Math.sin(Math.PI / 4);
+
     it('constants', () => {
-        expect(eq4(QUAT4_UNIT, { x: 0, y: 0, z: 0, w: 1 })).toEqual(true);
+        expect(QUAT4_UNIT).toBeVec4({ x: 0, y: 0, z: 0, w: 1 });
     });
 
-    it('quat4fromAxisAngle', () => {
-
-    });
-
-    it('quat4Angle', () => {
-
-    });
-
-    it('quat4Axis', () => {
-
+    it('quat4apply', () => {
+        expect(
+            quat4apply({ x: 0, y: 0, z: 0, w: 1 }, { x: 1, y: 2, z: 3 })
+        ).toBeVec3({ x: 1, y: 2, z: 3 });
+        expect(
+            quat4apply({ x: +s_PI4, y: 0, z: 0, w: c_PI4 }, { x: 1, y: 2, z: 3 })
+        ).toBeVec3({ x: 1, y: -3, z: 2 });
+        expect(
+            quat4apply({ x: 0, y: -s_PI4, z: 0, w: c_PI4 }, { x: 1, y: 2, z: 3 })
+        ).toBeVec3({ x: -3, y: 2, z: 1 });
+        expect(
+            quat4apply({ x: 0, y: 0, z: +s_PI4, w: c_PI4 }, { x: 1, y: 2, z: 3 })
+        ).toBeVec3({ x: -2, y: 1, z: 3 });
     });
 
     it('quat4mul', () => {
-
+        expect(
+            quat4mul({ x: 0, y: 0, z: 0, w: 1 }, { x: 0, y: 0, z: 0, w: 1 })
+        ).toBeVec4({ x: 0, y: 0, z: 0, w: 1 });
+        expect(
+            quat4mul({ x: -s_PI4, y: 0, z: 0, w: c_PI4 }, { x: 0, y: -s_PI4, z: 0, w: c_PI4 })
+        ).toBeVec4({ x: -0.5, y: -0.5, z: 0.5, w: 0.5 });
     });
+
+    it('quat4slerp', () => {
+        expect(
+            quat4slerp({ x: -0.5, y: -0.5, z: 0.5, w: 0.5 }, { x: -0.5, y: -0.5, z: 0.5, w: 0.5 }, 0)
+        ).toBeVec4({ x: -0.5, y: -0.5, z: 0.5, w: 0.5 });
+        expect(
+            quat4slerp({ x: -0.5, y: -0.5, z: 0.5, w: 0.5 }, { x: -0.5, y: -0.5, z: 0.5, w: 0.5 }, 1)
+        ).toBeVec4({ x: -0.5, y: -0.5, z: 0.5, w: 0.5 });
+        expect(
+            quat4slerp({ x: -0.5, y: -0.5, z: 0.5, w: 0.5 }, { x: -0.5, y: -0.5, z: 0.5, w: 0.5 }, 0.5)
+        ).toBeVec4({ x: -0.5, y: -0.5, z: 0.5, w: 0.5 });
+
+        expect(
+            quat4slerp({ x: 0, y: +s_PI4, z: 0, w: c_PI4 }, { x: 0, y: -s_PI4, z: 0, w: c_PI4 }, 0.5)
+        ).toBeVec4({ x: 0, y: 0, z: 0, w: 1 });
+        expect(
+            quat4slerp({ x: 0, y: +s_PI4, z: 0, w: c_PI4 }, { x: 0, y: -s_PI4, z: 0, w: c_PI4 }, -0.1)
+        ).toBeVec4({ x: 0, y: +s_PI4, z: 0, w: c_PI4 });
+        expect(
+            quat4slerp({ x: 0, y: +s_PI4, z: 0, w: c_PI4 }, { x: 0, y: -s_PI4, z: 0, w: c_PI4 }, +1.2)
+        ).toBeVec4({ x: 0, y: -s_PI4, z: 0, w: c_PI4 });
+        const c = Math.cos(Math.PI / 8);
+        const s = Math.sin(Math.PI / 8);
+        expect(
+            quat4slerp({ x: 0, y: +s_PI4, z: 0, w: c_PI4 }, { x: 0, y: -s_PI4, z: 0, w: c_PI4 }, 0.25)
+        ).toBeVec4({ x: 0, y: +s, z: 0, w: c });
+        expect(
+            quat4slerp({ x: 0, y: +s_PI4, z: 0, w: c_PI4 }, { x: 0, y: -s_PI4, z: 0, w: c_PI4 }, 0.75)
+        ).toBeVec4({ x: 0, y: -s, z: 0, w: c });
+    });
+
+    // it('quat4fromAxisAngle', () => {
+
+    // });
+
+    // it('quat4Angle', () => {
+
+    // });
+
+    // it('quat4Axis', () => {
+
+    // });
+
+    // it('quat4mul', () => {
+
+    // });
 });
--- lib/geometry/quat4.ts
+++ lib/geometry/quat4.ts
@@ -11,17 +11,16 @@ function v4(): Vec4Mut {
     return vec4(0, 0, 0, 0) as Vec4Mut;
 }
 
-export function quat4Angle(q: Vec4): number {
-    return Math.acos(q.w) * 2;
-}
-
-export function quat4Axis(q: Vec4, out: Vec3Mut = vec3(0, 0, 0) as Vec3Mut, eps: number = FLOAT_EQ_EPS): Vec3 {
-    const t = Math.sin(quat4Angle(q) / 2);
-    if (eq(t, 0, eps)) {
-        clone3(ZERO3, out);
-    } else {
-        mul3(q, 1 / t, out);
-    }
+export function quat4apply(q: Vec4, v: Vec3, out: Vec3Mut = vec3(0, 0, 0) as Vec3Mut): Vec3 {
+    const { x, y, z } = v;
+    const { x: qx, y: qy, z: qz, w: qw } = q;
+    const ix = qw * x + qy * z - qz * y;
+    const iy = qw * y + qz * x - qx * z;
+    const iz = qw * z + qx * y - qy * x;
+    const iw = - qx * x - qy * y - qz * z;
+    out.x = ix * qw + iw * -qx + iy * -qz - iz * -qy;
+	out.y = iy * qw + iw * -qy + iz * -qx - ix * -qz;
+	out.z = iz * qw + iw * -qz + ix * -qy - iy * -qx;
     return out;
 }
 
@@ -33,6 +32,20 @@ export function quat4mul(a: Vec4, b: Vec4, out: Vec4Mut = v4()): Vec4 {
     out.y = ay * bw + aw * by + az * bx - ax * bz;
     out.z = az * bw + aw * bz + ax * by - ay * bx;
     out.w = aw * bw - ax * bx - ay * by - az * bz;
+    return out;
+}
+
+export function quat4Angle(q: Vec4): number {
+    return Math.acos(q.w) * 2;
+}
+
+export function quat4Axis(q: Vec4, out: Vec3Mut = vec3(0, 0, 0) as Vec3Mut, eps: number = FLOAT_EQ_EPS): Vec3 {
+    const t = Math.sin(quat4Angle(q) / 2);
+    if (eq(t, 0, eps)) {
+        clone3(ZERO3, out);
+    } else {
+        mul3(q, 1 / t, out);
+    }
     return out;
 }
 
@@ -121,6 +134,12 @@ export function quat4fromVecs(from: Vec3, to: Vec3, out: Vec4Mut = v4(), eps: nu
 
 // http://www.euclideanspace.com/maths/algebra/realNormedAlgebra/quaternions/slerp/
 export function quat4slerp(a: Vec4, b: Vec4, t: number, out: Vec4Mut = v4(), eps: number = FLOAT_EQ_EPS): Vec4 {
+    if (t <= 0) {
+        return clone4(a, out);
+    }
+    if (t >= 1) {
+        return clone4(b, out);
+    }
     let k = dot4(a, b);
     let sign = 1;
     if (k < 0) {
@@ -136,24 +155,11 @@ export function quat4slerp(a: Vec4, b: Vec4, t: number, out: Vec4Mut = v4(), eps
         const angle = Math.acos(k);
         const s = Math.sin(angle);
         ta = Math.sin((1 - t) * angle) / s;
-        tb = Math.sin(t * angle) / t;
+        tb = Math.sin(t * angle) / s;
     }
     out.x = a.x * ta + b.x * tb * sign;
     out.y = a.y * ta + b.y * tb * sign;
     out.z = a.z * ta + b.z * tb * sign;
     out.w = a.w * ta + b.w * tb * sign;
-    return out;
-}
-
-export function quat4apply(q: Vec4, v: Vec3, out: Vec3Mut = vec3(0, 0, 0) as Vec3Mut): Vec3 {
-    const { x, y, z } = v;
-    const { x: qx, y: qy, z: qz, w: qw } = q;
-    const ix = qw * x + qy * z - qz * y;
-    const iy = qw * y + qz * x - qx * z;
-    const iz = qw * z + qx * y - qy * x;
-    const iw = - qx * x - qy * y - qz * z;
-    out.x = ix * qw + iw * -qx + iy * -qz - iz * -qy;
-	out.y = iy * qw + iw * -qy + iz * -qx - ix * -qz;
-	out.z = iz * qw + iw * -qz + ix * -qy - iy * -qx;
     return out;
 }
