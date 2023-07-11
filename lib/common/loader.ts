878983892f264a4b25f39c1459a6504a15cbf988
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2023-07-12 01:15:03 +0300

    Update Loader

--- lib/common/loader.ts
+++ lib/common/loader.ts
@@ -1,20 +1,51 @@
 export class Loader {
-    constructor() {
-    }
+    private readonly _pending: Map<string, Promise<ArrayBuffer>> = new Map();
+    private readonly _tasks: Set<Promise<ArrayBuffer>> = new Set();
 
     dispose(): void {
-    }
-
-    async load(url: string): Promise<ArrayBuffer> {
-        const response = await fetch(url);
-        if (!response.ok) {
-            throw new Error(`${url}: ${response.statusText}`);
+        for (const task of Array.from(this._tasks)) {
+            this.cancel(task);
         }
-        const buffer = await response.arrayBuffer();
-        return buffer;
     }
 
-    cancel(_task: Promise<ArrayBuffer>): void {
-
+    load(url: string): Promise<ArrayBuffer> {
+        let pending = this._pending.get(url);
+        if (!pending) {
+            pending = execute(url).finally(() => {
+                this._pending.delete(url);
+            });
+            this._pending.set(url, pending);
+        }
+        const task = new Promise<ArrayBuffer>((resolve, reject) => {
+            pending!.then(
+                (res) => {
+                    if (this._tasks.has(task)) {
+                        this._tasks.delete(task);
+                        resolve(res);
+                    }
+                },
+                (err) => {
+                    if (this._tasks.has(task)) {
+                        this._tasks.delete(task);
+                        reject(err);
+                    }
+                },
+            );
+        });
+        this._tasks.add(task);
+        return task;
     }
+
+    cancel(task: Promise<ArrayBuffer>): void {
+        this._tasks.delete(task);
+    }
+}
+
+async function execute(url: string): Promise<ArrayBuffer> {
+    const response = await fetch(url);
+    if (!response.ok) {
+        throw new Error(`${url}: ${response.statusText}`);
+    }
+    const buffer = await response.arrayBuffer();
+    return buffer;
 }
