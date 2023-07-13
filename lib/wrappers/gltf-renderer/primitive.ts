3b5e2c21ae5c611e2becc0efe08ca6e902de1616
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2023-07-14 01:38:26 +0300

    Refactor

--- lib/wrappers/gltf-renderer/primitive.ts
+++ lib/wrappers/gltf-renderer/primitive.ts
@@ -46,90 +46,40 @@ export function createPrimitive(
     if (getPrimitiveMode(primitive) !== SUPPORTED_PRIMITIVE_MODE) {
         throw new Error(`not supported primitive mode: ${getPrimitiveMode(primitive)}`);
     }
-
     // Though specification allows to have no positions there is no sense in accepting such primitives.
     if (positionIdx === undefined) {
         throw new Error('no POSITION attribute');
     }
-    const positionAccessor = getAccessor(positionIdx, asset);
-    if (getAccessorType(positionAccessor) !== VALID_POSITION_TYPE) {
-        throw new Error(`bad POSITION type: ${getAccessorType(positionAccessor)}`);
-    }
-    const positionData = getAccessorBinaryData(asset, positionAccessor);
-    const positionStride = getAccessorStride(asset.gltf, positionAccessor);
 
-    let indicesType: GlTF_ACCESSOR_TYPE;
-    let indicesCount: number;
-    let indicesData: Uint8Array;
-    if (primitive.indices !== undefined) {
-        const indicesAccessor = getAccessor(primitive.indices, asset);
-        indicesType = getAccessorType(indicesAccessor);
-        if (!VALID_INDEX_TYPES.has(indicesType)) {
-            throw new Error(`bad index type: ${indicesType}`);
-        }
-        indicesCount = indicesAccessor.count;
-        indicesData = getAccessorBinaryData(asset, indicesAccessor);
-    } else {
-        indicesType = 'ushort1';
-        indicesCount = positionAccessor.count;
-        indicesData = generateIndices(positionAccessor.count);
-    }
+    const positionInfo = getAttributeInfo(asset, positionIdx, validatePositionType, noop);
+    const indexInfo = primitive.indices !== undefined
+        ? getAttributeInfo(asset, primitive.indices, validateIndexType, noop)
+        : generateIndexInfo(positionInfo.count);
+    const normalInfo = normalIdx !== undefined
+        ? getAttributeInfo(
+            asset, normalIdx,
+            validateNormalType, (count) => validateAccessorCount(count, positionInfo.count, 'NORMAL'),
+        )
+        : generateNormalInfo(positionInfo, indexInfo)
+    const colorInfo = colorIdx !== undefined
+        ? getAttributeInfo(
+            asset, colorIdx,
+            validateColorType, (count) => validateAccessorCount(count, positionInfo.count, 'COLOR_0'),
+        )
+        : null;
+    const texcoordInfo = texcoordIdx !== undefined
+        ? getAttributeInfo(
+            asset, texcoordIdx,
+            validateTexcoordType, (count) => validateAccessorCount(count, positionInfo.count, 'TEXCOORD_0'),
+        )
+        : null;
 
-    let normalData: Uint8Array;
-    let normalStride: number;
-    if (normalIdx !== undefined) {
-        const normalAccessor = getAccessor(normalIdx, asset);
-        if (getAccessorType(normalAccessor) !== VALID_NORMAL_TYPE) {
-            throw new Error(`bad NORMAL type: ${getAccessorType(normalAccessor)}`);
-        }
-        if (normalAccessor.count !== positionAccessor.count) {
-            throw new Error(`bad NORMAL count: ${normalAccessor.count}`);
-        }
-        normalData = getAccessorBinaryData(asset, normalAccessor);
-        normalStride = getAccessorStride(asset.gltf, normalAccessor);
-    } else {
-        normalData = generateNormals(positionData, indicesData, indicesType);
-        normalStride = 12;
+    let totalVertexDataSize = positionInfo.data.byteLength + normalInfo.data.byteLength;
+    if (colorInfo) {
+        totalVertexDataSize += colorInfo.data.byteLength;
     }
-
-    let colorType: GlTF_ACCESSOR_TYPE | undefined;
-    let colorData: Uint8Array | undefined;
-    let colorStride: number | undefined;
-    if (colorIdx !== undefined) {
-        const colorAccessor = getAccessor(colorIdx, asset);
-        colorType = getAccessorType(colorAccessor);
-        if (!VALID_COLOR_TYPES.has(colorType)) {
-            throw new Error(`bad COLOR_0 type: ${colorType}`);
-        }
-        if (colorAccessor.count !== positionAccessor.count) {
-            throw new Error(`bad COLOR_0 count: ${colorAccessor.count}`);
-        }
-        colorData = getAccessorBinaryData(asset, colorAccessor);
-        colorStride = getAccessorStride(asset.gltf, colorAccessor);
-    }
-
-    let texcoordType: GlTF_ACCESSOR_TYPE | undefined;
-    let texcoordData: Uint8Array | undefined;
-    let texcoordStride: number | undefined;
-    if (texcoordIdx !== undefined) {
-        const texcoordAccessor = getAccessor(texcoordIdx, asset);
-        texcoordType = getAccessorType(texcoordAccessor);
-        if (!VALID_TEXCOORD_TYPE.has(texcoordType)) {
-            throw new Error(`bad TEXCOORD_0 type: ${texcoordType}`);
-        }
-        if (texcoordAccessor.count !== positionAccessor.count) {
-            throw new Error(`bad TEXCOORD_0 count: ${texcoordAccessor.count}`);
-        }
-        texcoordData = getAccessorBinaryData(asset, texcoordAccessor);
-        texcoordStride = getAccessorStride(asset.gltf, texcoordAccessor);
-    }
-
-    let totalVertexDataSize = positionData.byteLength + normalData.byteLength;
-    if (colorData) {
-        totalVertexDataSize += colorData.byteLength;
-    }
-    if (texcoordData) {
-        totalVertexDataSize += texcoordData.byteLength;
+    if (texcoordInfo) {
+        totalVertexDataSize += texcoordInfo.data.byteLength;
     }
 
     const result = new Primitive(runtime);
@@ -139,52 +89,56 @@ export function createPrimitive(
     const vertexAttributes: AttributeOptions[] = [];
     let vertexDataOffset = 0;
 
-    result.updateVertexData(positionData, vertexDataOffset);
-    vertexAttributes.push(
-        { name: 'a_position', type: 'float3', offset: vertexDataOffset, stride: positionStride },
-    );
-    vertexDataOffset += positionData.byteLength;
+    result.updateVertexData(positionInfo.data, vertexDataOffset);
+    vertexAttributes.push({
+        name: 'a_position',
+        type: positionInfo.type as ATTRIBUTE_TYPE,
+        offset: vertexDataOffset,
+        stride: positionInfo.stride
+    });
+    vertexDataOffset += positionInfo.data.byteLength;
 
-    result.updateVertexData(normalData, vertexDataOffset);
-    vertexAttributes.push(
-        { name: 'a_normal', type: 'float3', offset: vertexDataOffset, stride: normalStride },
-    );
-    vertexDataOffset += normalData.byteLength;
+    result.updateVertexData(normalInfo.data, vertexDataOffset);
+    vertexAttributes.push({
+        name: 'a_normal',
+        type: normalInfo.type as ATTRIBUTE_TYPE,
+        offset: vertexDataOffset,
+        stride: normalInfo.stride
+    });
+    vertexDataOffset += normalInfo.data.byteLength;
 
-    if (colorData) {
-        result.updateVertexData(colorData, vertexDataOffset);
-        const attrType = colorType as ATTRIBUTE_TYPE;
-        const isNormalized = colorType !== 'float3' && colorType !== 'float4';
-        vertexAttributes.push(
-            {
-                name: 'a_color', type: attrType, normalized: isNormalized,
-                offset: vertexDataOffset, stride: colorStride,
-            },
-        );
-        vertexDataOffset += colorData.byteLength;
+    if (colorInfo) {
+        result.updateVertexData(colorInfo.data, vertexDataOffset);
+        vertexAttributes.push({
+            name: 'a_color',
+            type: colorInfo.type as ATTRIBUTE_TYPE,
+            normalized: colorInfo.type !== 'float3' && colorInfo.type !== 'float4',
+            offset: vertexDataOffset,
+            stride: colorInfo.stride,
+        });
+        vertexDataOffset += colorInfo.data.byteLength;
     }
 
-    if (texcoordData) {
-        result.updateVertexData(texcoordData, vertexDataOffset);
-        const attrType = texcoordType as ATTRIBUTE_TYPE;
-        const isNormalized = texcoordType !== 'float2';
-        vertexAttributes.push(
-            {
-                name: 'a_texcoord', type: attrType, normalized: isNormalized,
-                offset: vertexDataOffset, stride: texcoordStride,
-            },
-        );
-        vertexDataOffset += texcoordData.byteLength;
+    if (texcoordInfo) {
+        result.updateVertexData(texcoordInfo.data, vertexDataOffset);
+        vertexAttributes.push({
+            name: 'a_texcoord',
+            type: texcoordInfo.type as ATTRIBUTE_TYPE,
+            normalized: texcoordInfo.type !== 'float2',
+            offset: vertexDataOffset,
+            stride: texcoordInfo.stride,
+        });
+        vertexDataOffset += texcoordInfo.data.byteLength;
     }
 
     const schema = parseVertexSchema(vertexAttributes);
     result.setVertexSchema(schema);
 
-    result.allocateIndexBuffer(indicesData.byteLength);
-    result.updateIndexData(indicesData);
+    result.allocateIndexBuffer(indexInfo.data.byteLength);
+    result.updateIndexData(indexInfo.data);
     result.setIndexConfig({
-        indexCount: indicesCount,
-        indexType: INDEX_TYPE_TO_TYPE[indicesType as GLTF_INDEX_TYPE],
+        indexCount: indexInfo.count,
+        indexType: INDEX_TYPE_TO_TYPE[indexInfo.type as GLTF_INDEX_TYPE],
         primitiveMode: 'triangles',
     });
 
@@ -192,12 +146,12 @@ export function createPrimitive(
 
     // TODO: Share program between all primitives (some schema check should be updated?).
     const programDefinitions = {
-        HAS_COLOR_ATTR: colorData ? '1' : '0',
-        HAS_TEXCOORD_ATTR: texcoordData ? '1' : '0',
+        HAS_COLOR_ATTR: colorInfo ? '1' : '0',
+        HAS_TEXCOORD_ATTR: texcoordIdx ? '1' : '0',
         HAS_MATERIAL: material ? '1' : '0',
-        HAS_BASE_COLOR_TEXTURE: !!texcoordData
+        HAS_BASE_COLOR_TEXTURE: !!texcoordInfo
             && material?.baseColorTextureIndex !== undefined ? '1' : '0',
-        HAS_METALLIC_ROUGHNESS_TEXTURE: !!texcoordData
+        HAS_METALLIC_ROUGHNESS_TEXTURE: !!texcoordInfo
             && material?.metallicRoughnessTextureIndex !== undefined ? '1' : '0',
     };
     const program = new Program(runtime, {
@@ -216,6 +170,66 @@ export function createPrimitive(
     };
 }
 
+interface AttributeInfo {
+    readonly type: GlTF_ACCESSOR_TYPE;
+    readonly stride: number;
+    readonly count: number;
+    readonly data: Uint8Array;
+}
+
+function getAttributeInfo(
+    asset: GlTFAsset, idx: number,
+    validateType: (t: GlTF_ACCESSOR_TYPE) => void, validateCount: (c: number) => void,
+): AttributeInfo {
+    const accessor = getAccessor(idx, asset);
+    validateCount(accessor.count);
+    const type = getAccessorType(accessor);
+    validateType(type);
+    const data = getAccessorBinaryData(asset, accessor);
+    const stride = getAccessorStride(asset.gltf, accessor);
+    return { type, stride, count: accessor.count, data };
+}
+
+function noop(): void {
+    // Nothing.
+}
+
+function validatePositionType(type: GlTF_ACCESSOR_TYPE): void {
+    if (type !== VALID_POSITION_TYPE) {
+        throw new Error(`bad POSITION type: ${type}`);
+    }
+}
+
+function validateIndexType(type: GlTF_ACCESSOR_TYPE): void {
+    if (!VALID_INDEX_TYPES.has(type)) {
+        throw new Error(`bad index type: ${type}`);
+    }
+}
+
+function validateNormalType(type: GlTF_ACCESSOR_TYPE): void {
+    if (type !== VALID_NORMAL_TYPE) {
+        throw new Error(`bad NORMAL type: ${type}`);
+    }
+}
+
+function validateColorType(type: GlTF_ACCESSOR_TYPE): void {
+    if (!VALID_COLOR_TYPES.has(type)) {
+        throw new Error(`bad COLOR_0 type: ${type}`);
+    }
+}
+
+function validateTexcoordType(type: GlTF_ACCESSOR_TYPE): void {
+    if (!VALID_TEXCOORD_TYPE.has(type)) {
+        throw new Error(`bad TEXCOORD_0 type: ${type}`);
+    }
+}
+
+function validateAccessorCount(count: number, matchCount: number, name: string): void {
+    if (count !== matchCount) {
+        throw new Error(`bad ${name} count: ${count}`);
+    }
+}
+
 function getAccessor(idx: number, asset: GlTFAsset): GlTFSchema.Accessor {
     const accessor = asset.gltf.accessors?.[idx];
     if (!accessor) {
@@ -224,12 +238,26 @@ function getAccessor(idx: number, asset: GlTFAsset): GlTFSchema.Accessor {
     return accessor;
 }
 
-function generateIndices(count: number): Uint8Array {
+function generateIndexInfo(count: number): AttributeInfo {
     const arr = new Uint16Array(count);
     for (let i = 0; i < arr.length; ++i) {
         arr[i] = i;
     }
-    return new Uint8Array(arr.buffer);
+    return {
+        type: 'ushort1',
+        count,
+        stride: 0,
+        data: new Uint8Array(arr.buffer),
+    };
+}
+
+function generateNormalInfo(positionInfo: AttributeInfo, indexInfo: AttributeInfo): AttributeInfo {
+    return {
+        type: 'float3',
+        count: positionInfo.count,
+        stride: 12,
+        data: generateNormals(positionInfo.data, indexInfo.data, indexInfo.type),
+    };
 }
 
 type GLTF_INDEX_TYPE = Extract<GlTF_ACCESSOR_TYPE, 'ubyte1' | 'ushort1' | 'uint1'>;
