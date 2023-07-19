9ce61ed63deaca587742118ce4f65cdb515b3dbc
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2023-07-20 00:37:39 +0300

    Add debug check

--- lib/common/base-disposable.ts
+++ lib/common/base-disposable.ts
@@ -6,8 +6,13 @@ export abstract class BaseDisposable extends BaseIdentity {
     private readonly _disposed = new EventEmitter();
 
     protected _emitDisposed(): void {
+        if (!this._disposed) {
+            throw new Error('DEBUG: already disposed');
+        }
         this._disposed.emit();
         this._disposed.clear();
+        // @ts-ignore DEBUG
+        this._disposed = null;
     }
 
     disposed(): EventProxy {
