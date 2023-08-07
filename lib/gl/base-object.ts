625eb987564963d10068545e9fbf4f14322e7767
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2023-08-08 00:42:07 +0300

    Introduce BaseObject

---
+++ lib/gl/base-object.ts
@@ -0,0 +1,46 @@
+import type { BaseObjectParams } from './base-object.types';
+import type { Logger } from '../common/logger.types';
+import { EventProxy } from '../common/event-emitter.types';
+import { EventEmitter } from '../common/event-emitter';
+import { LoggerImpl } from 'lib/common/logger';
+
+let nextId = 1;
+
+export abstract class BaseObject {
+    protected readonly _name: string;
+    protected readonly _id: string;
+    protected readonly _logger: Logger;
+    private readonly _disposed = new EventEmitter();
+
+    constructor(params: BaseObjectParams) {
+        this._name = params.name || this.constructor.name;
+        this._id = params.tag || String(nextId++);
+        this._logger = new LoggerImpl(`${this._name}#${this._id}`, params.logger);
+    }
+
+    protected _dispose(): void {
+        if (!this._disposed) {
+            throw new Error('DEBUG: already disposed');
+        }
+        this._disposed.emit();
+        this._disposed.clear();
+        // @ts-ignore DEBUG
+        this._disposed = null;
+    }
+
+    name(): string {
+        return this._name;
+    }
+
+    id(): string {
+        return this._id;
+    }
+
+    logger(): Logger {
+        return this._logger;
+    }
+
+    disposed(): EventProxy {
+        return this._disposed.proxy();
+    }
+}
---
+++ lib/gl/base-object.types.ts
@@ -0,0 +1,7 @@
+import type { Logger } from '../common/logger.types';
+
+export interface BaseObjectParams {
+    readonly logger: Logger;
+    readonly name?: string;
+    readonly tag?: string;
+}
