d3eaf5fbc7fcd9460ab35aceb2782dd220a13f57
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2023-08-17 14:03:55 +0300

    Add new logger stub

---
+++ src/common/logger_ex.types.ts
@@ -0,0 +1,17 @@
+export interface Logging {
+    info(message: string): void;
+    warn(message: string): void;
+    error(message: string): void;
+}
+
+export type LOG_LEVEL = ('info' | 'warn' | 'error');
+
+export interface LogEntry {
+    readonly timestamp: Date;
+    readonly level: LOG_LEVEL;
+    readonly message: string;
+}
+
+export interface LogTransport {
+    log(entry: LogEntry): void;
+}
