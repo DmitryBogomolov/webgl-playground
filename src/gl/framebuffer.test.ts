3da3c27e0e47e6580534a6822a81e3d061094623
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2023-12-16 21:04:14 +0300

    Restore tests

--- src/gl/framebuffer.test.ts
+++ src/gl/framebuffer.test.ts
@@ -8,13 +8,21 @@ describe('framebuffer', () => {
     describe('Framebuffer', () => {
         const MockTexture = Texture as jest.Mock;
         let framebuffer: WebGLFramebuffer;
-        let texture: { setImageData: jest.Mock, glHandle: jest.Mock };
+        let texture: {
+            dispose: jest.Mock,
+            setFormat: jest.Mock,
+            setParameters: jest.Mock,
+            setImageData: jest.Mock,
+            glHandle: jest.Mock,
+        };
         let ctx: WebGLRenderingContext;
         let runtime: Runtime;
         let createFramebuffer: jest.Mock;
+        let deleteFramebuffer: jest.Mock;
         let bindFramebuffer: jest.Mock;
         let framebufferTexture2D: jest.Mock;
         let checkFramebufferStatus: jest.Mock;
+        let bindTexture: jest.Mock;
 
         const {
             FRAMEBUFFER,
@@ -25,16 +33,22 @@ describe('framebuffer', () => {
         beforeEach(() => {
             framebuffer = { tag: 'framebuffer' };
             texture = {
+                dispose: jest.fn(),
+                setFormat: jest.fn(),
+                setParameters: jest.fn(),
                 setImageData: jest.fn(),
                 glHandle: jest.fn().mockReturnValue({ tag: 'texture' }),
             };
             MockTexture.mockReturnValueOnce(texture);
             createFramebuffer = jest.fn().mockReturnValueOnce(framebuffer);
+            deleteFramebuffer = jest.fn();
             framebufferTexture2D = jest.fn();
             checkFramebufferStatus = jest.fn();
             bindFramebuffer = jest.fn();
+            bindTexture = jest.fn();
             ctx = {
                 createFramebuffer,
+                deleteFramebuffer,
                 framebufferTexture2D,
                 checkFramebufferStatus,
             } as unknown as WebGLRenderingContext;
@@ -42,6 +56,7 @@ describe('framebuffer', () => {
                 gl: () => ctx,
                 logger: () => null,
                 bindFramebuffer,
+                bindTexture,
             } as unknown as Runtime;
         });
 
@@ -58,8 +73,14 @@ describe('framebuffer', () => {
             expect(createFramebuffer.mock.calls).toEqual([
                 [],
             ]);
+            expect(texture.setFormat.mock.calls).toEqual([
+                ['rgba'],
+            ]);
+            expect(texture.setParameters.mock.calls).toEqual([
+                [{}],
+            ]);
             expect(texture.setImageData.mock.calls).toEqual([
-                [{ size: { x: 200, y: 150 }, data: null }, { format: 'rgba' }],
+                [{ size: { x: 200, y: 150 }, data: null }],
             ]);
             expect(framebufferTexture2D.mock.calls).toEqual([
                 [FRAMEBUFFER, COLOR_ATTACHMENT0, TEXTURE_2D, { tag: 'texture' }, 0],
