80768f659be7c9200a65592643834e6ebf345c34
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2024-12-08 20:36:06 +0300

    Add animation button

--- playground-utils/animation.ts
+++ playground-utils/animation.ts
@@ -9,12 +9,41 @@ export function animation(runtime: Runtime): Observable<boolean> {
     value.dispose = dispose;
     value.on(handleChange);
 
+    const button = document.createElement('button');
+    button.className = 'btn animation-button';
+    const inner = document.createElement('i');
+    inner.className = 'fa fa-solid fa-pause';
+    button.appendChild(inner);
+    setIconClass();
+
+    button.addEventListener('click', () => {
+        value(!value());
+        setIconClass();
+    });
+    runtime.canvas().parentElement!.appendChild(button);
+
     return value;
+
+    function setIconClass(): void {
+        const val = value();
+        let toAdd = '';
+        let toRemove = '';
+        if (val) {
+            toAdd = 'fa-play';
+            toRemove = 'fa-pause';
+        } else {
+            toAdd = 'fa-pause';
+            toRemove = 'fa-play';
+        }
+        inner.classList.add(toAdd);
+        inner.classList.remove(toRemove);
+    }
 
     function dispose(this: typeof value): void {
         this.off(handleChange);
         runtime.frameRequested().off(handleFrame);
         originalDispose.call(this);
+        button.remove();
     }
 
     function handleChange(val: boolean): void {
