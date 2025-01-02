fc156e986aaab8d4268eef75c5cdec49207a1b01
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2025-01-02 13:14:57 +0300

    Add eye pos calculation

--- src/common/orbit-camera.ts
+++ src/common/orbit-camera.ts
@@ -1,27 +1,32 @@
+import type { OrbitCameraEyePosition } from './orbit-camera.types';
 import type { Vec3, Vec3Mut } from '../geometry/vec3.types';
 import type { Vec4Mut } from '../geometry/vec4.types';
 import type { Mat4Mut } from '../geometry/mat4.types';
 import type { EventProxy } from './event-emitter.types';
-import { ZERO3, YUNIT3, ZUNIT3, vec3, isVec3, eq3, clone3, norm3, dist3, cross3 } from '../geometry/vec3';
+import { ZERO3, YUNIT3, ZUNIT3, vec3, isVec3, eq3, clone3, norm3, dist3, cross3, mul3 } from '../geometry/vec3';
 import { clone4 } from '../geometry/vec4';
 import { QUAT4_UNIT, quat4apply, quat4fromMat } from '../geometry/quat4';
 import { mat4, identity4x4, mul4v3 } from '../geometry/mat4';
+import { floatEq } from '../geometry/float-eq';
 import { ViewProj } from './view-proj';
 import { toArgStr } from '../utils/string-formatter';
 import { EventEmitter } from './event-emitter';
 import { rowcol2idxRank } from '../geometry/helpers';
 
 export class OrbitCamera {
-    private readonly _changed = new EventEmitter();
     private readonly _viewProj = new ViewProj();
     private readonly _rotationMat = mat4();
     private readonly _rotationQuat = clone4(QUAT4_UNIT);
     private _originDir: Vec3 = clone3(ZUNIT3);
     private _upDir: Vec3 = clone3(YUNIT3);
     private _centerPos: Vec3 = clone3(ZERO3);
+    private _eyePos: Vec3 = clone3(ZUNIT3);
+    private _lon = 0;
+    private _lat = 0;
+    private _dist = 1;
 
     changed(): EventProxy {
-        return this._changed.proxy();
+        return this._viewProj.changed();
     }
 
     getOriginDir(): Vec3 {
@@ -42,6 +47,7 @@ export class OrbitCamera {
         if (eq3(this._originDir, zDir) && eq3(this._upDir, yDir)) {
             return;
         }
+        // this._viewProj.setUpDir(yDir);
         this._originDir = clone3(zDir);
         this._upDir = clone3(yDir);
         makeMat(xDir, yDir, zDir, this._rotationMat as Mat4Mut);
@@ -58,11 +64,29 @@ export class OrbitCamera {
     }
 
     setCenterPos(value: Vec3): void {
-        check(isVec3(value), 'center_pos', value);
+        check(isVec3(value), 'center_pos', toArgStr(value));
         if (eq3(this._centerPos, value)) {
             return;
         }
+        // this._viewProj.setCenterPos(value);
         this._centerPos = clone3(value);
+    }
+
+    setEyePos(value: OrbitCameraEyePosition): void {
+        const lon = value.lon ?? this._lon;
+        const lat = value.lat ?? this._lat;
+        const dist = value.dist ?? this._dist;
+        if (floatEq(lon, this._lon) && floatEq(lat, this._lat) && floatEq(dist, this._dist)) {
+            return;
+        }
+        const eyePos = _eyePos_scratch;
+        makeEyePos(lon, lat, dist, eyePos);
+        quat4apply(this._rotationQuat, eyePos, eyePos);
+        // this._viewProj.setEyePos(eyePos);
+        this._eyePos = clone3(eyePos);
+        this._lon = lon;
+        this._lat = lat;
+        this._dist = dist;
     }
 }
 
@@ -87,9 +111,17 @@ function makeMat(xDir: Vec3, yDir: Vec3, zDir: Vec3, mat: Mat4Mut): void {
     mat[rowcol2idxRank(4, 2, 2)] = zDir.z;
 }
 
+function makeEyePos(lon: number, lat: number, dist: number, eye: Vec3Mut): void {
+    eye.x = Math.cos(lon) * Math.sin(lat);
+    eye.y = Math.sin(lon),
+    eye.z = Math.cos(lon) * Math.cos(lat),
+    mul3(eye, dist, eye);
+}
+
 const _x_scratch = vec3(0, 0, 0) as Vec3Mut;
 const _y_scratch = vec3(0, 0, 0) as Vec3Mut;
 const _z_scratch = vec3(0, 0, 0) as Vec3Mut;
+const _eyePos_scratch = vec3(0, 0, 0) as Vec3Mut;
 
 function check(condition: boolean, name: string, value: unknown): void {
     if (!condition) {
