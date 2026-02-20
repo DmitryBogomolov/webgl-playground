c9331ce3ee7314b32692047dc313a8d9b3e79284
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2026-02-20 11:17:26 +0300

    Remove excessive logic from demo

--- playground/21_contour_line/index.ts
+++ playground/21_contour_line/index.ts
@@ -1,15 +1,13 @@
-import type { Runtime, Primitive, Vec3, Mat4, Mat4Mut, Color } from 'lib';
+import type { Runtime, Primitive, Vec3, Color } from 'lib';
 import type { Observable } from 'playground-utils/observable';
 import {
     createRenderState,
     OrbitCamera,
     vec3,
-    identity4x4, apply4x4, xrotation4x4, yrotation4x4,
     color,
-    deg2rad,
 } from 'lib';
 import { setup, disposeAll, renderOnChange } from 'playground-utils/setup';
-import { observable, computed } from 'playground-utils/observable';
+import { observable } from 'playground-utils/observable';
 import { createControls } from 'playground-utils/controls';
 import { makePrimitive, makeContourPrimitive, updateContourData } from './primitive';
 import { findContour } from './contour';
@@ -29,7 +27,6 @@ interface State {
     readonly camera: OrbitCamera;
     readonly primitive: Primitive;
     readonly contourPrimitive: Primitive;
-    readonly modelMat: Observable<Mat4>;
     readonly modelClr: Color;
     readonly modelPoints: ReadonlyArray<Vec3>;
     readonly contourEnabled: Observable<boolean>;
@@ -40,17 +37,6 @@ export function main(): () => void {
     const { runtime, container } = setup();
     runtime.setClearColor(color(0.8, 0.8, 0.8));
     const camera = new OrbitCamera();
-
-    const xRotation = observable(0);
-    const yRotation = observable(0);
-    const _modelMat = identity4x4() as Mat4Mut;
-    const modelMat = computed(([xRotation, yRotation]) => {
-        const mat = _modelMat;
-        identity4x4(mat);
-        apply4x4(mat, xrotation4x4, deg2rad(xRotation));
-        apply4x4(mat, yrotation4x4, deg2rad(yRotation));
-        return mat as Mat4;
-    }, [xRotation, yRotation]);
 
     const contourEnabled = observable(true);
     const contourThickness = observable(16);
@@ -63,7 +49,6 @@ export function main(): () => void {
         camera,
         primitive,
         contourPrimitive,
-        modelMat,
         modelClr: color(0.5, 0.1, 0.5),
         modelPoints: [
             vec3(-0.5, -0.5, -0.5),
@@ -85,10 +70,8 @@ export function main(): () => void {
     runtime.frameRequested().on(() => {
         renderScene(state);
     });
-    [camera.changed(), modelMat].forEach((emitter) => {
-        emitter.on(() => {
-            updateContourPrimitive(state);
-        });
+    camera.changed().on(() => {
+        updateContourPrimitive(state);
     });
     const disposeTrackBall = trackBall({
         element: runtime.canvas(),
@@ -98,11 +81,9 @@ export function main(): () => void {
             camera.setEyePos(v);
         },
     });
-    const cancelRender = renderOnChange(runtime, [camera, modelMat, contourEnabled, contourThickness]);
+    const cancelRender = renderOnChange(runtime, [camera, contourEnabled, contourThickness]);
 
     const controlRoot = createControls(container, [
-        { label: 'x rotation', value: xRotation, min: -180, max: +180 },
-        { label: 'y rotation', value: yRotation, min: -180, max: +180 },
         { label: 'enabled', checked: contourEnabled },
         { label: 'thickness', value: contourThickness, min: 0, max: 32 },
     ]);
@@ -124,7 +105,6 @@ function renderScene({
     camera,
     primitive,
     contourPrimitive,
-    modelMat,
     modelClr,
     contourEnabled,
     contourThickness,
@@ -133,7 +113,6 @@ function renderScene({
 
     runtime.setRenderState(objectRenderState);
     primitive.program().setUniform('u_view_proj', camera.getTransformMat());
-    primitive.program().setUniform('u_model', modelMat());
     primitive.program().setUniform('u_color', modelClr);
     primitive.render();
 
@@ -145,7 +124,7 @@ function renderScene({
     }
 }
 
-function updateContourPrimitive({ contourPrimitive, camera, modelMat, modelPoints }: State): void {
-    const points = findContour(modelPoints, modelMat(), camera.getTransformMat());
+function updateContourPrimitive({ contourPrimitive, camera, modelPoints }: State): void {
+    const points = findContour(modelPoints, camera.getTransformMat());
     updateContourData(contourPrimitive, points);
 }
--- playground/21_contour_line/shaders/object.vert
+++ playground/21_contour_line/shaders/object.vert
@@ -1,7 +1,6 @@
 #version 300 es
 
 uniform mat4 u_view_proj;
-uniform mat4 u_model;
 
 in vec3 a_position;
 in vec3 a_normal;
@@ -9,7 +8,6 @@ in vec3 a_normal;
 out vec3 v_normal;
 
 void main() {
-    vec4 position = u_model * vec4(a_position, 1.0);
-    gl_Position = u_view_proj * position;
-    v_normal = mat3(u_model) * a_normal;
+    gl_Position = u_view_proj * vec4(a_position, 1.0);
+    v_normal = a_normal;
 }
