0cef85117763d469cb519ac89725718e29c84a15
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2026-03-17 22:48:43 +0300

    Remove channel util

--- src/wrappers/worker-channel/background-channel.types.ts
+++
@@ -1,3 +0,0 @@
-import type { BaseChannelParams } from './base-channel.types';
-
-export type BackgroundChannelParams<T> = Omit<BaseChannelParams<T>, 'carrier'>;
--- src/wrappers/worker-channel/foreground-channel.types.ts
+++
@@ -1,5 +0,0 @@
-import type { BaseChannelParams } from './base-channel.types';
-
-export type ForegroundChannelParams<T> = Omit<BaseChannelParams<T>, 'carrier'> & {
-    readonly worker: Worker | string;
-};
