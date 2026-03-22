bf949a378ca316c8e19b770d5ce5facf6fcfc356
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2026-03-22 15:38:31 +0300

    Tune server scripts

--- tools/registry-builder.ts
+++ tools/registry-builder.ts
@@ -1,11 +1,19 @@
-import path from 'path';
-import fs from 'fs';
 import type { Playground } from './playground.types';
+import path from 'node:path';
+import fs from 'node:fs/promises';
 
-export async function buildRegistry(playgroundRoot: string): Promise<Playground[]> {
-    const names = await fs.promises.readdir(playgroundRoot);
-    const objects = await Promise.all(names.map((item) => checkDir(path.join(playgroundRoot, item))));
-    return objects.filter(Boolean) as Playground[];
+export function buildRegistry(playgroundRoot: string): Promise<Playground[]> {
+    return fs.readdir(playgroundRoot)
+        .then((names) => {
+            const tasks = names.map((item) => {
+                const dirPath = path.join(playgroundRoot, item);
+                return checkDir(dirPath);
+            });
+            return Promise.all(tasks);
+        })
+        .then((playgrounds) =>
+            playgrounds.filter(Boolean) as Playground[],
+        );
 }
 
 function capitalizeWord(word: string): string {
@@ -16,37 +24,30 @@ function capitalizeName(name: string): string {
     return name.replace(/_/g, ' ').replace(/\w\S*/g, capitalizeWord);
 }
 
-async function fileExists(path: string): Promise<boolean> {
-    try {
-        await fs.promises.access(path, fs.constants.R_OK);
-        return true;
-    } catch {
-        return false;
-    }
+function fileExists(path: string): Promise<boolean> {
+    return fs.access(path).then(() => true, () => false);
 }
 
 const INDEX_NAME = 'index.ts';
 const WORKER_NAME = 'worker.ts';
 const MARKUP_NAME = 'markup.html';
 
-async function checkDir(dirPath: string): Promise<Playground | null> {
+function checkDir(dirPath: string): Promise<Playground | null> {
     const name = path.basename(dirPath);
-    const indexPath = path.join(dirPath, INDEX_NAME);
-    const workerPath = path.join(dirPath, WORKER_NAME);
-    const markupPath = path.join(dirPath, MARKUP_NAME);
-    const [indexExists, workerExists, markupExists] = await Promise.all([
-        fileExists(indexPath),
-        fileExists(workerPath),
-        fileExists(markupPath),
-    ]);
-    if (!indexExists) {
-        return null;
-    }
-    return {
-        name,
-        title: capitalizeName(name),
-        index: INDEX_NAME,
-        worker: workerExists ? WORKER_NAME : null,
-        markup: markupExists ? MARKUP_NAME : null,
-    };
+    const tasks = [INDEX_NAME, WORKER_NAME, MARKUP_NAME].map((name) => {
+        const filePath = path.join(dirPath, name);
+        return fileExists(filePath);
+    });
+    return Promise.all(tasks).then(([indexExists, workerExists, markupExists]) => {
+        if (!indexExists) {
+            return null;
+        }
+        return {
+            name,
+            title: capitalizeName(name),
+            index: INDEX_NAME,
+            worker: workerExists ? WORKER_NAME : null,
+            markup: markupExists ? MARKUP_NAME : null,
+        };
+    });
 }
