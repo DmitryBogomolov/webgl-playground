d7aba7e3ebd2683841f95c0b398fcc7f56444214
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2026-04-16 22:53:30 +0300

    Tune eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,81 +3,67 @@ import js from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import stylistic from '@stylistic/eslint-plugin';
 
-const stylisticRules = {
-    '@stylistic/indent': ['error', 4],
-    '@stylistic/indent-binary-ops': ['error', 4],
-    '@stylistic/semi': ['error', 'always'],
-    '@stylistic/quote-props': ['error', 'consistent'],
-    '@stylistic/arrow-parens': ['error', 'always'],
-    '@stylistic/member-delimiter-style': ['error', {
-        multiline: {
-            delimiter: 'semi',
-            requireLast: true,
-        },
-        singleline: {
-            delimiter: 'semi',
-            requireLast: false,
-        },
-        multilineDetection: 'brackets',
-    }],
-    '@stylistic/brace-style': ['error', '1tbs', { allowSingleLine: true }],
-    '@stylistic/max-len': ['error', { code: 120 }],
-};
-
-const baseConfig = defineConfig({
-    name: 'sources',
-    extends: [
-        js.configs.recommended,
-        tseslint.configs.recommendedTypeChecked,
-        tseslint.configs.stylisticTypeChecked,
-        stylistic.configs.recommended,
-    ],
-    languageOptions: {
-        ecmaVersion: 2018,
-        sourceType: 'module',
-        parserOptions: {
-            projectService: true,
-        },
-    },
-    rules: {
-        '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-        '@typescript-eslint/array-type': 'off',
-        '@typescript-eslint/dot-notation': 'off',
-        '@typescript-eslint/no-inferrable-types': ['error', { ignoreParameters: true, ignoreProperties: true }],
-        '@typescript-eslint/explicit-function-return-type': ['error', { allowExpressions: true }],
-        ...stylisticRules,
-    },
-});
-
-const TESTS_PATTERN = '**/*.test.ts';
-
 export default defineConfig([
     globalIgnores(['./node_modules/', './dist/', './build/']),
     {
+        name: 'base',
+        extends: [
+            js.configs.recommended,
+            stylistic.configs.recommended,
+        ],
+        languageOptions: {
+            ecmaVersion: 2018,
+            sourceType: 'module',
+            parserOptions: {
+                projectService: true,
+            },
+        },
+        rules: {
+            '@stylistic/indent': ['error', 4],
+            '@stylistic/indent-binary-ops': ['error', 4],
+            '@stylistic/semi': ['error', 'always'],
+            '@stylistic/quote-props': ['error', 'consistent'],
+            '@stylistic/arrow-parens': ['error', 'always'],
+            '@stylistic/member-delimiter-style': ['error', {
+                multiline: {
+                    delimiter: 'semi',
+                    requireLast: true,
+                },
+                singleline: {
+                    delimiter: 'semi',
+                    requireLast: false,
+                },
+                multilineDetection: 'brackets',
+            }],
+            '@stylistic/brace-style': ['error', '1tbs', { allowSingleLine: true }],
+            '@stylistic/max-len': ['error', { code: 120 }],
+        },
+    },
+    {
         name: 'sources',
         files: ['**/*.ts'],
-        ignores: [TESTS_PATTERN],
         extends: [
-            baseConfig,
+            tseslint.configs.recommendedTypeChecked,
+            tseslint.configs.stylisticTypeChecked,
         ],
+        rules: {
+            '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+            '@typescript-eslint/array-type': 'off',
+            '@typescript-eslint/dot-notation': 'off',
+            '@typescript-eslint/no-inferrable-types': ['error', { ignoreParameters: true, ignoreProperties: true }],
+            '@typescript-eslint/explicit-function-return-type': ['error', { allowExpressions: true }],
+        },
     },
     {
         name: 'tests',
-        files: [TESTS_PATTERN],
-        extends: [
-            baseConfig,
-        ],
+        files: ['**/*.test.ts'],
         rules: {
             '@typescript-eslint/unbound-method': 'off',
         },
     },
     {
-        name: 'configs',
-        files: ['**/*.mjs', '**/*.js'],
-        extends: [
-            js.configs.recommended,
-            stylistic.configs.recommended,
-        ],
+        name: 'scripts',
+        files: ['**/*.{js,mjs}'],
         languageOptions: {
             globals: {
                 module: false,
@@ -88,7 +74,6 @@ export default defineConfig([
             },
         },
         rules: {
-            ...stylisticRules,
             'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
         },
     },
