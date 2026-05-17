6e477d4510df0a9a7faeaeae8cbf1b96ef95abc6
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2026-05-17 20:19:41 +0300

    Tune script

--- tools/remove-outdated-folders.sh
+++ tools/remove-outdated-folders.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 
-if [ $# -lt 2 ]; then
-    echo "Usage: $0 <bucket> <file_name> <threshold || 3>"
+if [ "$#" -lt 2 ]; then
+    echo "Usage: $0 <bucket> <file_name> <age || 3>"
     exit 1
 fi
 
 bucket=$1
 file_name=$2
-threshold=${3:-3}
+age=${3:-3}
+
+if ! [ "$age" -gt 0 ]; then
+    echo "Not valid age: $age"
+    exit 1
+fi
 
 tags=$(aws s3api list-objects-v2 \
     --bucket $bucket \
@@ -27,7 +32,7 @@ for tag_path in ./tags/*.tag; do
     closed_time=$(date -u -d "$pr_tag" +%s)
     diff=$(( (now_time - closed_time) / 86400 ))
     echo "$tag_path $pr_tag $closed_time $diff"
-    if [ "$diff" -gt "$threshold" ]; then
+    if [ "$diff" -gt "$age" ]; then
         pr=$(basename "$tag_path" .tag)
         # aws s3 rm --recursive s3://$bucket/$pr/
         echo "TODO: aws s3 rm --recursive s3://$bucket/$pr/"
